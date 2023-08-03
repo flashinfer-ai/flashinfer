24d92d4356dffe803fab5584097d3344f9c4e242
yzh119 <expye@outlook.com>
2023-08-03 06:31:20 +0000

    rename

--- src/bench.cu
+++ src/bench.cu
@@ -4,7 +4,7 @@
 #include <nvbench/nvbench.cuh>
 
 template <typename dtype_in, typename dtype_out, int seq_len, int num_heads, int head_dim>
-void bench_kv_parallel_flashattn_decode(nvbench::state &state) {
+void bench_flashinfer_decode(nvbench::state &state) {
   // Allocate input data:
   thrust::device_vector<dtype_in> Q(num_heads * head_dim);
   thrust::device_vector<dtype_in> K(seq_len * num_heads * head_dim);
@@ -28,9 +28,9 @@ void bench_kv_parallel_flashattn_decode(nvbench::state &state) {
   });
 }
 
-#define CREATE_BENCH_F16F16(SEQLEN, NUMHEADS, HEADDIM)                               \
-  auto bench_kv_parallel_flashattn_decode_f16f16_##SEQLEN##_##NUMHEADS##_##HEADDIM = \
-      bench_kv_parallel_flashattn_decode<half, half, SEQLEN, NUMHEADS, HEADDIM>;
+#define CREATE_BENCH_F16F16(SEQLEN, NUMHEADS, HEADDIM)                    \
+  auto bench_flashinfer_decode_f16f16_##SEQLEN##_##NUMHEADS##_##HEADDIM = \
+      bench_flashinfer_decode<half, half, SEQLEN, NUMHEADS, HEADDIM>;
 
 CREATE_BENCH_F16F16(32, 32, 128);
 CREATE_BENCH_F16F16(64, 32, 128);
@@ -44,14 +44,14 @@ CREATE_BENCH_F16F16(8192, 32, 128);
 CREATE_BENCH_F16F16(16384, 32, 128);
 CREATE_BENCH_F16F16(32768, 32, 128);
 
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_32_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_64_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_128_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_256_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_512_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_1024_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_2048_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_4096_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_8192_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_16384_32_128);
-NVBENCH_BENCH(bench_kv_parallel_flashattn_decode_f16f16_32768_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_32_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_64_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_128_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_256_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_512_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_1024_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_2048_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_4096_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_8192_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_16384_32_128);
+NVBENCH_BENCH(bench_flashinfer_decode_f16f16_32768_32_128);
