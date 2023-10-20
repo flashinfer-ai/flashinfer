3f7c2f633d5da610ea8777508caaa0af47f8f578
Zihao <expye@outlook.com>
2023-10-20 04:51:44 +0000

    update config.cmake

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -6,6 +6,8 @@ set(FLASHINFER_TVM_BINDING ON)
 set(FLASHINFER_PREFILL ON)
 # Whether to compile decode kernel tests/benchmarks or not.
 set(FLASHINFER_DECODE ON)
+# Whether to compile page kernel tests/benchmarks or not.
+set(FLASHINFER_PAGE ON)
 # Set target cuda architectures for tests/benchmarks, defaults to native (which is new in CMake 3.24, you can configure it manually if you are using older CMake).
 # Example: "80;86;90;90"
 set(FLASHINFER_CUDA_ARCHITECTURES native)
