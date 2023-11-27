a44bcfa34efd4b1026fb28f6d86e86bf72699258
Zihao <expye@outlook.com>
2023-11-27 00:24:30 +0000

    python format

--- python/flashinfer/ops/utils.py
+++ python/flashinfer/ops/utils.py
@@ -2,17 +2,11 @@ class RotaryMode:
     NONE = 0
     LLAMA = 1
 
-    FORMAT2STR = {
-        0: "NONE",
-        1: "LLAMA"
-    }
+    FORMAT2STR = {0: "NONE", 1: "LLAMA"}
 
 
 class TensorLayout:
     NHD = 0
     HND = 1
 
-    FORMAT2STR = {
-        0: "NHD",
-        1: "HND"
-    }
+    FORMAT2STR = {0: "NHD", 1: "HND"}
