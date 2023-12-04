b460406240907864d5431f21516d9a33e7c63ed5
Zihao <expye@outlook.com>
2023-12-04 06:03:47 +0000

    aexpose more functions

--- python/flashinfer/__init__.py
+++ python/flashinfer/__init__.py
@@ -2,8 +2,12 @@ import torch
 from .ops import (
     single_decode_with_kv_cache,
     single_prefill_with_kv_cache,
+    single_prefill_with_kv_cache_return_lse,
     merge_state,
     merge_states,
+    batch_decode_with_padded_kv_cache,
+    batch_decode_with_padded_kv_cache_return_lse,
+    batch_decode_with_shared_prefix_padded_kv_cache,
 )
 
 __version__ = "0.0.1"
