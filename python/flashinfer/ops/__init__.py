dd4f0a9a9abc34108551b3912896a7ec4f9bb9e2
yzh119 <expye@outlook.com>
2023-12-10 22:45:48 -0500

    some more docs

--- python/flashinfer/ops/__init__.py
+++ python/flashinfer/ops/__init__.py
@@ -27,6 +27,30 @@ def single_decode_with_kv_cache(
     rope_scale: Optional[float] = None,
     rope_theta: Optional[float] = None,
 ):
+    r"""Single request decode with KV cache.
+
+    Parameters
+    ----------
+    q : torch.Tensor
+        Shape: [num_qo_heads, head_dim]
+    k : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    v : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    rotary_mode : str
+        Whether to apply rotary embeddings inside attention kernels, could be
+        "NONE" or "LLAMA".
+    tensor_layout : str
+        The layout of the input k/v tensors, could be either "NHD" or "HND".
+    sm_scale : Optional[float]
+        The scale of softmax, if not provided, will be set to 1 / sqrt(head_dim)
+    rope_scale : Optional[float]
+        The scale used in RoPE interpolation, if not provided, will be set to 1.0.
+    rope_theta : Optional[float]
+        The theta used in RoPE, if not provided, will be set to 1e4.
+    """
     tmp = _get_cache_buf("single_decode_with_kv_cache_tmp", 8 * 1024 * 1024, q.device)
     if sm_scale is None:
         head_dim = q.shape[-1]
@@ -59,6 +83,34 @@ def single_prefill_with_kv_cache(
     rope_scale: Optional[float] = None,
     rope_theta: Optional[float] = None,
 ):
+    r"""Single request prefill with KV cache kernel.
+
+    Parameters
+    ----------
+    q : torch.Tensor
+        Shape: [qo_len, num_qo_heads, head_dim] if NHD
+               [num_qo_heads, qo_len, head_dim] if HND
+    k : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    v : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    causal : bool
+        Whether to apply causal mask to the attention matrix.
+    rotary_mode : str
+        Whether to apply rotary embeddings inside attention kernels, could be
+        "NONE" or "LLAMA".
+    tensor_layout : str
+        The layout of the input k/v tensors, could be either "NHD" or "HND".
+    allow_fp16_qk_reduction : bool
+        Whether to use f16 for qk reduction (could be significantly faster for GeForce cards, at
+        the cost of precision loss).
+    rope_scale : Optional[float]
+        The scale used in RoPE interpolation, if not provided, will be set to 1.0.
+    rope_theta : Optional[float]
+        The theta used in RoPE, if not provided, will be set to 1e4.
+    """
     tmp = _get_cache_buf("single_prefill_with_kv_cache_tmp", 8 * 1024 * 1024, q.device)
     if rope_scale is None:
         rope_scale = 1.0
@@ -89,6 +141,44 @@ def single_prefill_with_kv_cache_return_lse(
     rope_scale: Optional[float] = None,
     rope_theta: Optional[float] = None,
 ):
+    r"""Single request prefill with KV cache kernel, return logsumexp value.
+
+    Parameters
+    ----------
+    q : torch.Tensor
+        Shape: [qo_len, num_qo_heads, head_dim] if NHD
+               [num_qo_heads, qo_len, head_dim] if HND
+    k : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    v : torch.Tensor
+        Shape: [kv_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, kv_len, head_dim] if HND
+    causal : bool
+        Whether to apply causal mask to the attention matrix.
+    rotary_mode : str
+        Whether to apply rotary embeddings inside attention kernels, could be
+        "NONE" or "LLAMA".
+    tensor_layout : str
+        The layout of the input k/v tensors, could be either "NHD" or "HND".
+    allow_fp16_qk_reduction : bool
+        Whether to use f16 for qk reduction (could be significantly faster for GeForce cards, at
+        the cost of precision loss).
+    rope_scale : Optional[float]
+        The scale used in RoPE interpolation, if not provided, will be set to 1.0.
+    rope_theta : Optional[float]
+        The theta used in RoPE, if not provided, will be set to 1e4.
+
+    Returns
+    -------
+    V : torch.Tensor
+        The attention output.
+        Shape: [qo_len, num_qo_heads, head_dim] if NHD
+               [num_qo_heads, qo_len, head_dim] if HND
+    S : torch.Tensor
+        The logsumexp value.
+        Shape: [qo_len, num_qo_heads]
+    """
     tmp = _get_cache_buf(
         "single_prefill_with_kv_cache_return_lse_tmp", 8 * 1024 * 1024, q.device
     )
@@ -113,10 +203,50 @@ def single_prefill_with_kv_cache_return_lse(
 def merge_state(
     v_a: torch.Tensor, s_a: torch.Tensor, v_b: torch.Tensor, s_b: torch.Tensor
 ):
+    r"""Merge the attention output (V) and the logsumexp value (S) from the two KV-segments.
+
+    Parameters
+    ----------
+    v_a : torch.Tensor
+        The attention output from the KV segment A.
+        Shape: [batch_size, num_heads, head_dim]
+    s_a : torch.Tensor
+        The logsumexp value from the KV segment A.
+        Shape: [batch_size, num_heads]
+    v_b : torch.Tensor
+        The attention output from the KV segment B.
+        Shape: [batch_size, num_heads, head_dim]
+    s_b : torch.Tensor
+        The logsumexp value from the KV segment B.
+        Shape: [batch_size, num_heads]
+
+    Returns
+    -------
+    V : torch.Tensor
+        The merged attention output (equivalent to attention with merged KV-segment [A: B]).
+        Shape: [batch_size, num_heads, head_dim]
+    """
     return _kernels.merge_state(v_a, s_a, v_b, s_b)
 
 
 def merge_states(v: torch.Tensor, s: torch.Tensor):
+    r"""Merge the attention output (V) and the logsumexp value (S) from multiple KV-segments.
+
+    Parameters
+    ----------
+    v : torch.Tensor
+        The attention output from the KV segments.
+        Shape: [num_kv_segments, batch_size, num_heads, head_dim]
+    s : torch.Tensor
+        The logsumexp value from the KV segments.
+        Shape: [num_kv_segments, batch_size, num_heads]
+
+    Returns
+    -------
+    V : torch.Tensor
+        The merged attention output.
+        Shape: [batch_size, num_heads, head_dim]
+    """
     return _kernels.merge_states(v, s)
 
 
@@ -130,19 +260,22 @@ def batch_decode_with_padded_kv_cache(
     rope_scale: Optional[float] = None,
     rope_theta: Optional[float] = None,
 ):
-    r"""
+    r"""Batch decode with padded KV cache.
+
     Parameters
     ----------
     q : torch.Tensor
         Shape: [batch_size, num_qo_heads, head_dim]
     k_padded : torch.Tensor
-        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim]
+        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, padded_seq_len, head_dim] if HND
     v_padded : torch.Tensor
-        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim]
+        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, padded_seq_len, head_dim] if HND
 
     Returns
     -------
-    torch.Tensor
+    V : torch.Tensor
         Shape: [batch_size, num_heads, head_dim]
     """
     if sm_scale is None:
@@ -180,15 +313,30 @@ def batch_decode_with_padded_kv_cache_return_lse(
     q : torch.Tensor
         Shape: [batch_size, num_qo_heads, head_dim]
     k_padded : torch.Tensor
-        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD else [batch_size, num_kv_heads, padded_seq_len, head_dim]
+        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, padded_seq_len, head_dim] if HND
     v_padded : torch.Tensor
-        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD else [batch_size, num_kv_heads, padded_seq_len, head_dim]
+        Shape: [batch_size, padded_seq_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, padded_seq_len, head_dim] if HND
+    tensor_layout: str
+        The layout of the input k_padded/v_padded tensors, could be either
+        "NHD" or "HND"
+    rotary_mode: str
+        Whether to apply rotary embeddings inside attention kernels, could be
+        "NONE" or "LLAMA".
+    sm_scale: Optional[float]
+        The scale of softmax, if not provided, will be set to 1 / sqrt(head_dim)
+    rope_scale: Optional[float]
+        The scale used in RoPE interpolation, if not provided, will be set to 1.0.
+    rope_theta: Optional[float]
+        The theta used in RoPE, if not provided, will be set to 1e4.
 
     Returns
     -------
-    Tuple[torch.Tensor, torch.Tensor]
-        o shape: [batch_size, num_heads, head_dim]
-        lse shape: [batch_size, num_heads]
+    V : torch.Tensor
+        Shape: [batch_size, num_heads, head_dim]
+    S : torch.Tensor
+        Shape: [batch_size, num_heads]
     """
     if sm_scale is None:
         head_dim = q.shape[-1]
@@ -220,7 +368,39 @@ def batch_decode_with_shared_prefix_padded_kv_cache(
     rope_scale: Optional[float] = None,
     rope_theta: Optional[float] = None,
 ):
-    o_shared, lse_shared = single_prefill_with_kv_cache_return_lse(
+    r"""Batch decode with shared prefix padded KV cache.
+
+    Parameters
+    ----------
+    q : torch.Tensor
+        Shape: [batch_size, num_qo_heads, head_dim]
+    k_shared : torch.Tensor
+        Shape: [shared_prefix_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, shared_prefix_len, head_dim] if HND
+    v_shared : torch.Tensor
+        Shape: [shared_prefix_len, num_kv_heads, head_dim] if NHD
+               [num_kv_heads, shared_prefix_len, head_dim] if HND
+    k_unique : torch.Tensor
+        Shape: [batch_size, unique_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, unique_len, head_dim] if HND
+    v_unique : torch.Tensor
+        Shape: [batch_size, unique_len, num_kv_heads, head_dim] if NHD
+               [batch_size, num_kv_heads, unique_len, head_dim] if HND
+    tensor_layout : str
+        The layout of the input k/v tensors, could be either "NHD" or "HND".
+    sm_scale : Optional[float]
+        The scale of softmax, if not provided, will be set to 1 / sqrt(head_dim)
+    rope_scale : Optional[float]
+        The scale used in RoPE interpolation, if not provided, will be set to 1.0.
+    rope_theta : Optional[float]
+        The theta used in RoPE, if not provided, will be set to 1e4.
+
+    Returns
+    -------
+    V : torch.Tensor
+        Shape: [batch_size, num_heads, head_dim]
+    """
+    V_shared, S_shared = single_prefill_with_kv_cache_return_lse(
         q,
         k_shared,
         v_shared,
@@ -231,7 +411,7 @@ def batch_decode_with_shared_prefix_padded_kv_cache(
         rope_scale=rope_scale,
         rope_theta=rope_theta,
     )
-    o_unique, lse_unique = batch_decode_with_padded_kv_cache_return_lse(
+    V_unique, S_unique = batch_decode_with_padded_kv_cache_return_lse(
         q,
         k_unique,
         v_unique,
@@ -241,5 +421,4 @@ def batch_decode_with_shared_prefix_padded_kv_cache(
         rope_scale=rope_scale,
         rope_theta=rope_theta,
     )
-    o_merged = merge_state(o_shared, lse_shared, o_unique, lse_unique)
-    return o_merged
+    return merge_state(V_shared, S_shared, V_unique, S_unique)
