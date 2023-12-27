c3dafa4318b81e7cad8d9cb97555da383a724bd0
yzh119 <expye@outlook.com>
2023-12-27 12:47:20 -0500

    add formatter script

---
+++ scripts/formatter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Formatting CUDA files"
+find include/ -regex '.*\.\(h\|cuh\|cu\|cc\)' | xargs clang-format -i
+find src/ -regex '.*\.\(h\|cuh\|cu\|cc\)' | xargs clang-format -i
+find python/ -regex '.*\.\(h\|cuh\|cu\|cc\)' | xargs clang-format -i
+echo "Formatting Python files"
+black python/flashinfer/*.py python/flashinfer/ops/*.py python/tests/*.py
