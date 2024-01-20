34b0e69ee9075234a0790c509eb7fea3e7d04d3e
Zihao Ye <expye@outlook.com>
2024-01-20 18:06:01 +0800

    bugfix

--- scripts/gh_deploy_doc.sh
+++ scripts/gh_deploy_doc.sh
@@ -10,12 +10,14 @@ mv _build/html/* .
 rm -rf _build/
 cd ..
 
-git ls-file | xargs rm
+git grep --cached -l '' | xargs rm
 git fetch
 git checkout -B gh-pages origin/gh-pages
+echo "3rdparty/" >> .gitignore
 
 DATE=`date`
-git add html/ && git commit -am "Build at ${DATE}"
+git add . && git commit -am "Build at ${DATE}"
 git push origin gh-pages
 git checkout main && git submodule update
 echo "Finish deployment at ${DATE}"
+
