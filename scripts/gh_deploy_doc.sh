090d7aaaf71581d93fa097fb9647feff6ae08673
Zihao Ye <expye@outlook.com>
2024-01-20 19:30:24 +0800

    bugfix

--- scripts/gh_deploy_doc.sh
+++ scripts/gh_deploy_doc.sh
@@ -8,14 +8,14 @@ mkdir -p tmp
 cd docs
 make html
 mv _build/html/* ../tmp/
-rm -rf _build/
 cd ..
 
 git grep --cached -l '' | xargs rm
 git fetch
 git checkout -B gh-pages origin/gh-pages
 echo "3rdparty/" >> .gitignore
-mv tmp/* docs/
+rm -rf docs/
+mv tmp docs
 
 DATE=`date`
 git add . && git commit -am "Build at ${DATE}"
