807d197be772e77104013aabdffe257d676171eb
Zihao Ye <expye@outlook.com>
2024-01-20 03:27:32 -0800

    [Doc] Bugfix for `gh_deploy_doc.sh` (#78)

--- scripts/gh_deploy_doc.sh
+++ scripts/gh_deploy_doc.sh
@@ -4,9 +4,10 @@
 
 set -euxo pipefail
 
+mkdir -p tmp
 cd docs
 make html
-mv _build/html/* .
+mv _build/html/* ../tmp/
 rm -rf _build/
 cd ..
 
@@ -14,6 +15,7 @@ git grep --cached -l '' | xargs rm
 git fetch
 git checkout -B gh-pages origin/gh-pages
 echo "3rdparty/" >> .gitignore
+mv tmp/* docs/
 
 DATE=`date`
 git add . && git commit -am "Build at ${DATE}"
