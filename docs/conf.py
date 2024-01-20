c3f093a089026605c90d2f40840c5ca08f3f6704
Zihao Ye <expye@outlook.com>
2024-01-20 21:56:01 +0800

    [Doc] Use tlcpack theme (#81)

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+import tlcpack_sphinx_addon
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -8,7 +12,7 @@
 
 project = 'FlashInfer'
 author = "FlashInfer Contributors"
-copyright = '2023-2024, {}'.format(author)
+footer_copyright = '2023-2024, {}'.format(author)
 
 version = "0.0.1"
 release = "0.0.1"
@@ -16,15 +20,67 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_tabs.tabs",
+    "sphinx_toolbox.collapse",
+    "sphinxcontrib.httpdomain",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_reredirects",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = [".rst"]
 
+language = "en"
 
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
-html_theme = 'sphinxdoc'
-html_static_path = ['_static']
+# A list of ignored prefixes for module index sorting.
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme is set by the make target
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+templates_path = []
+
+html_static_path = []
+
+footer_note = " "
+
+html_theme_options = {
+    "logo_only": True,
+}
+
+header_links = [
+    ("Home", "https://flashinfer.ai"),
+    ("Github", "https://github.com/flashinfer-ai/flashinfer"),
+    ("Discussions", "https://github.com/orgs/flashinfer-ai/discussions"),
+]
+
+html_context = {
+    "footer_copyright": footer_copyright,
+    "footer_note": footer_note,
+    "header_links": header_links,
+    "display_github": True,
+    "github_user": "flashinfer-ai",
+    "github_repo": "flashinfer",
+    "github_version": "main/docs/",
+    "theme_vcs_pageview_mode": "edit",
+    # "header_logo": "/path/to/logo",
+    # "header_logo_link": "",
+    # "version_selecter": "",
+}
+
+# add additional overrides
+templates_path += [tlcpack_sphinx_addon.get_templates_path()]
+html_static_path += [tlcpack_sphinx_addon.get_static_path()]
+
