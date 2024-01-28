9611d42139e14733f6b8f3b0ca487ce2f0c99138
Zihao Ye <expye@outlook.com>
2024-01-28 23:36:56 +0800

    debugging

--- python/setup.py
+++ python/setup.py
@@ -38,6 +38,7 @@ def get_local_version_suffix() -> str:
 
 
 def get_version():
+    assert False, root.resolve()
     with open(root / "version.txt") as f:
         version = f.read().strip()
     version += get_local_version_suffix()
