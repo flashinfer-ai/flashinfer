502a9d747a27e58c19785acdb880d9ad2cdf74e5
yzh119 <expye@outlook.com>
2024-03-11 06:34:19 +0000

    upd

---
+++ include/flashinfer/sampling/topp.cuh
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 by FlashInfer team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef FLASHINFER_SAMPLING_TOP_P_CUH_
+#define FLASHINFER_SAMPLING_TOP_P_CUH_
+
+#include "../vec_dtypes.cuh"
+
+namespace flashinfer {
+
+namespace sampling {
+
+/*!
+ * \brief Top-P sampling kernel.
+ * \param x The input tensor of shape (N, D)
+ * \param indices The output indices tensor of shape (N,)
+ * \param p The probability threshold
+ * \param d The 
+ */
+template <typename DType, typename IdType>
+__global__ void TopPSamplingKernel(
+  DType* __restrict__ x,
+  IdType* __restrict__ indices,
+  const float p,
+  const uint32_t d
+) {
+  const uint32_t i = blockIdx.x;
+  constexpr uint32_t vec_size = 16 / sizeof(DType);
+
+}
+
+} // namespace sampling
+
+} // namespace flashinfer
+
+#endif  // FLASHINFER_SAMPLING_TOP_P_CUH_
