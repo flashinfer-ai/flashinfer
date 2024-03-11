ae3dbd1d55777f1e7fca88fd1699219d1bc5c948
Zihao Ye <expye@outlook.com>
2024-03-11 10:30:20 +0000

    upd

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@ project = 'FlashInfer'
 author = "FlashInfer Contributors"
 copyright = '2023-2024, {}'.format(author)
 
-version = "0.0.2"
-release = "0.0.2"
+version = "0.0.3"
+release = "0.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
