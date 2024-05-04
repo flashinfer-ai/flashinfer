62343e66039a25cd49e89c8fe11fac153dd4bfee
Zihao Ye <expye@outlook.com>
2024-05-03 17:13:14 -0700

    fix: remove 8 from default page size (#233)
    To reduce pip wheel size.

--- python/setup.py
+++ python/setup.py
@@ -64,7 +64,7 @@ def get_instantiation_cu() -> List[str]:
     (root / prefix).mkdir(parents=True, exist_ok=True)
 
     group_sizes = os.environ.get("FLASHINFER_GROUP_SIZES", "1,4,6,8").split(",")
-    page_sizes = os.environ.get("FLASHINFER_PAGE_SIZES", "1,8,16,32").split(",")
+    page_sizes = os.environ.get("FLASHINFER_PAGE_SIZES", "1,16,32").split(",")
     head_dims = os.environ.get("FLASHINFER_HEAD_DIMS", "64,128,256").split(",")
     kv_layouts = os.environ.get("FLASHINFER_KV_LAYOUTS", "0,1").split(",")
     pos_encoding_modes = os.environ.get("FLASHINFER_POS_ENCODING_MODES", "0,1,2").split(
