9cb28deff8493583586804725f0c180372197d5d
Zihao Ye <expye@outlook.com>
2024-07-17 22:50:48 -0700

    doc: update documentation to v0.1.0 (#378)
    * bump doc version
    * add `top_k_top_p_sampling_from_probs` and `flashinfer.sparse` to
    documentation.

--- python/flashinfer/__init__.py
+++ python/flashinfer/__init__.py
@@ -38,6 +38,7 @@ from .sampling import (
     sampling_from_probs,
     top_p_sampling_from_probs,
     top_k_sampling_from_probs,
+    top_k_top_p_sampling_from_probs,
     top_p_renorm_prob,
     top_k_renorm_prob,
     chain_speculative_sampling,
