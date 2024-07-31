0dc287c57c8c7104c517e27f3d911fbf0f708818
Zihao Ye <expye@outlook.com>
2024-07-31 10:37:57 +0000

    reorder

--- python/csrc/batch_prefill.cu
+++ python/csrc/batch_prefill.cu
@@ -31,9 +31,9 @@ void BatchPrefillWithPagedKVCachePyTorchWrapper::BeginForward(
   CHECK_GQA_HEAD_DIVISIBLE(num_qo_heads, num_kv_heads);
   CHECK_DIM(1, qo_indptr);
   CHECK_DIM(1, paged_kv_indptr);
+  CHECK_DIM(1, workspace_buffer);
   CHECK_EQ(qo_indptr.size(0), batch_size + 1);
   CHECK_EQ(paged_kv_indptr.size(0), batch_size + 1);
-  CHECK_DIM(1, workspace_buffer);
   qo_indptr = qo_indptr.to(torch::dtype(torch::kInt32).device(torch::kCPU));
   paged_kv_indptr = paged_kv_indptr.to(torch::dtype(torch::kInt32).device(torch::kCPU));
   auto device = workspace_buffer.device();
