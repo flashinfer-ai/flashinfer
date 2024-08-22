28d21ed8587e0257882ee90064faf325c15ce5ab
Zihao Ye <expye@outlook.com>
2024-08-22 11:22:41 +0000

    upd

--- python/flashinfer/__init__.py
+++ python/flashinfer/__init__.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 from .cascade import (
+    MultiLevelCascadeAttentionWrapper,
     BatchDecodeWithSharedPrefixPagedKVCacheWrapper,
     BatchPrefillWithSharedPrefixPagedKVCacheWrapper,
     merge_state,
