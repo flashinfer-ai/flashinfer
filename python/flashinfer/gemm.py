19d5935a702e81ef5263a4917306e5bdfa2c02d8
zhyncs <me@zhyncs.com>
2024-09-02 01:29:59 +1000

    upd doc

--- python/flashinfer/gemm.py
+++ python/flashinfer/gemm.py
@@ -241,8 +241,9 @@ def bmm_fp8(
     >>> import flashinfer
     >>> def to_float8(x, dtype=torch.float8_e4m3fn):
     ...     finfo = torch.finfo(dtype)
-    ...     abs_max = x.abs().amax(dim=(1, 2), keepdim=True).clamp(min=1e-12)
-    ...     scale = finfo.max / abs_max
+    ...     min_val, max_val = x.aminmax()
+    ...     amax = torch.maximum(min_val.abs(), max_val.abs()).clamp(min=1e-12)
+    ...     scale = finfo.max / amax
     ...     x_scl_sat = (x * scale).clamp(min=finfo.min, max=finfo.max)
     ...     return x_scl_sat.to(dtype), scale.float().reciprocal()
     >>>
