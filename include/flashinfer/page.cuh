flashinfer-ai/flashinfer

commit 85b1878996a29814f674ee5000facb1e2e763d9a

Feature/non contiguous kv cache (#513)
This PR solves #506 

Custom strides to support non-contiguous kv cache.
Tests in `test_batch_prefill_kernels.py` and
`test_batch_decode_kernels.py` are modified to test input kv_data on
both contiguous and non-contiguous tensor.

---------

Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>
Co-authored-by: Zihao Ye <expye@outlook.com>

include/flashinfer/page.cuh
@@ -80,47 +80,6 @@ struct paged_kv_t {
    * \param head_dim The dimension of each head
    * \param batch_size The batch size
    * \param layout The layout of last 3 dimensions in KV-Cache.
-   * \param kv_data The flattened key-value cache
-   * \param k_data The flattened key cache
-   * \param v_data The flattened value cache
-   * \param indices The page indices array
-   * \param indptr The page indptr array
-   * \param last_page_len The offset of the last page for each request in the batch
-   * \param rope_pos_offset The start position of each request in the batch.
-   */
-  __host__ __forceinline__ paged_kv_t(uint32_t num_heads, uint32_t page_size, uint32_t head_dim,
-                                      uint32_t batch_size, QKVLayout layout, DType* kv_data,
-                                      DType* k_data, DType* v_data, IdType* indices, IdType* indptr,
-                                      IdType* last_page_len, IdType* rope_pos_offset = nullptr)
-      : num_heads(num_heads),
-        page_size(page_size),
-        head_dim(head_dim),
-        batch_size(batch_size),
-        indices(indices),
-        indptr(indptr),
-        last_page_len(last_page_len),
-        rope_pos_offset(rope_pos_offset) {
-    bool kv_defined = kv_data != nullptr;
-    if (kv_defined) {
-      stride_page = 2 * num_heads * page_size * head_dim;
-      this->k_data = kv_data;
-      this->v_data = kv_data + num_heads * page_size * head_dim;
-    } else {
-      stride_page = num_heads * page_size * head_dim;
-      this->k_data = k_data;
-      this->v_data = v_data;
-    }
-    stride_n = layout == QKVLayout::kHND ? head_dim : num_heads * head_dim;
-    stride_h = layout == QKVLayout::kHND ? page_size * head_dim : head_dim;
-  }
-
-  /*!
-   * \brief Construct a paged key-value cache
-   * \param num_heads The number of heads
-   * \param page_size The size of each page
-   * \param head_dim The dimension of each head
-   * \param batch_size The batch size
-   * \param layout The layout of last 3 dimensions in KV-Cache.
    * \param k_data The flattened key cache
    * \param v_data The flattened value cache
    * \param indices The page indices array
@@ -136,13 +95,13 @@ struct paged_kv_t {
         page_size(page_size),
         head_dim(head_dim),
         batch_size(batch_size),
-        k_data(k_data),
-        v_data(v_data),
         indices(indices),
         indptr(indptr),
         last_page_len(last_page_len),
         rope_pos_offset(rope_pos_offset) {
     stride_page = num_heads * page_size * head_dim;
+    this->k_data = k_data;
+    this->v_data = v_data;
     stride_n = layout == QKVLayout::kHND ? head_dim : num_heads * head_dim;
     stride_h = layout == QKVLayout::kHND ? page_size * head_dim : head_dim;
   }
@@ -154,33 +113,33 @@ struct paged_kv_t {
    * \param head_dim The dimension of each head
    * \param batch_size The batch size
    * \param layout The layout of last 3 dimensions in KV-Cache.
-   * \param kv_data The flattened key-value cache
+   * \param k_data The flattened key cache
+   * \param v_data The flattened value cache
+   * \param kv_strides custom strides of each dimensions of k_data and v_data
    * \param indices The page indices array
    * \param indptr The page indptr array
    * \param last_page_len The offset of the last page for each request in the batch
    * \param rope_pos_offset The start position of each request in the batch.
+   * \note This constructor should only be used when page_storage == kIndices
    */
   __host__ __forceinline__ paged_kv_t(uint32_t num_heads, uint32_t page_size, uint32_t head_dim,
-                                      uint32_t batch_size, QKVLayout layout, DType* kv_data,
-                                      IdType* indices, IdType* indptr, IdType* last_page_len,
+                                      uint32_t batch_size, QKVLayout layout, DType* k_data,
+                                      DType* v_data, const int64_t* kv_strides, IdType* indices,
+                                      IdType* indptr, IdType* last_page_len,
                                       IdType* rope_pos_offset = nullptr)
       : num_heads(num_heads),
         page_size(page_size),
         head_dim(head_dim),
         batch_size(batch_size),
-        k_data(kv_data),
-        v_data(kv_data + num_heads * page_size * head_dim),
         indices(indices),
         indptr(indptr),
         last_page_len(last_page_len),
         rope_pos_offset(rope_pos_offset) {
-    stride_page = 2 * num_heads * page_size * head_dim;
-    stride_n = layout == QKVLayout::kHND ? head_dim : num_heads * head_dim;
-    stride_h = layout == QKVLayout::kHND ? page_size * head_dim : head_dim;
-  }
-
-  __host__ __device__ __forceinline__ int64_t kv_ptr_delta() const {
-    return (int64_t(v_data) - int64_t(k_data)) / sizeof(DType);
+    stride_page = kv_strides[0];
+    this->k_data = k_data;
+    this->v_data = v_data;
+    stride_n = layout == QKVLayout::kHND ? kv_strides[2] : kv_strides[1];
+    stride_h = layout == QKVLayout::kHND ? kv_strides[1] : kv_strides[2];
   }
 
   __host__ __device__ __forceinline__ uint32_t get_length(uint32_t batch_idx) const {
@@ -220,14 +179,20 @@ struct paged_kv_t {
     return k_data + get_elem_offset(__ldg(indices + page_iter), head_idx, entry_idx, feat_idx);
   }
 
+  __device__ __forceinline__ size_t protective_get_kv_offset(IdType page_iter, uint32_t head_idx,
+                                                             uint32_t entry_idx, uint32_t feat_idx,
+                                                             IdType last_indptr) const {
+    if (page_iter < last_indptr) {
+      return get_elem_offset(__ldg(indices + page_iter), head_idx, entry_idx, feat_idx);
+    } else {
+      return 0;
+    }
+  }
+
   __device__ __forceinline__ DType* protective_get_k_ptr(IdType page_iter, uint32_t head_idx,
                                                          uint32_t entry_idx, uint32_t feat_idx,
                                                          IdType last_indptr) const {
-    if (page_iter < last_indptr) {
-      return k_data + get_elem_offset(__ldg(indices + page_iter), head_idx, entry_idx, feat_idx);
-    } else {
-      return k_data;
-    }
+    return k_data + protective_get_kv_offset(page_iter, head_idx, entry_idx, feat_idx, last_indptr);
   }
 
   __device__ __forceinline__ DType* get_v_ptr(IdType page_iter, uint32_t head_idx,
@@ -238,11 +203,7 @@ struct paged_kv_t {
   __device__ __forceinline__ DType* protective_get_v_ptr(IdType page_iter, uint32_t head_idx,
                                                          uint32_t entry_idx, uint32_t feat_idx,
                                                          IdType last_indptr) const {
-    if (page_iter < last_indptr) {
-      return v_data + get_elem_offset(__ldg(indices + page_iter), head_idx, entry_idx, feat_idx);
-    } else {
-      return v_data;
-    }
+    return v_data + protective_get_kv_offset(page_iter, head_idx, entry_idx, feat_idx, last_indptr);
   }
 };
 

python/csrc/page.cu
@@ -20,33 +20,24 @@
 using namespace flashinfer;
 
 void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
-                           torch::Tensor append_indptr, std::optional<torch::Tensor> paged_kv_cache,
-                           std::optional<torch::Tensor> paged_k_cache,
-                           std::optional<torch::Tensor> paged_v_cache, torch::Tensor kv_indices,
+                           torch::Tensor append_indptr, torch::Tensor paged_k_cache,
+                           torch::Tensor paged_v_cache, torch::Tensor kv_indices,
                            torch::Tensor kv_indptr, torch::Tensor kv_last_page_len,
                            unsigned int layout) {
-  bool paged_kv_defined = paged_kv_cache.has_value();
   CHECK_INPUT(append_key);
   CHECK_INPUT(append_value);
   CHECK_INPUT(append_indptr);
-  if (paged_kv_defined) {
-    CHECK_INPUT(paged_kv_cache.value());
-  } else {
-    CHECK_INPUT(paged_k_cache.value());
-    CHECK_INPUT(paged_v_cache.value());
-  }
+  // NOTE(Zihao): doesn't have to be contiguous
+  CHECK_LAST_DIM_CONTIGUOUS_INPUT(paged_k_cache);
+  CHECK_LAST_DIM_CONTIGUOUS_INPUT(paged_v_cache);
   CHECK_INPUT(kv_indices);
   CHECK_INPUT(kv_indptr);
   CHECK_INPUT(kv_last_page_len);
   CHECK_DIM(3, append_key);
   CHECK_DIM(3, append_value);
   CHECK_DIM(1, append_indptr);
-  if (paged_kv_defined) {
-    CHECK_DIM(5, paged_kv_cache.value());
-  } else {
-    CHECK_DIM(4, paged_k_cache.value());
-    CHECK_DIM(4, paged_v_cache.value());
-  }
+  CHECK_DIM(4, paged_k_cache);
+  CHECK_DIM(4, paged_v_cache);
   CHECK_DIM(1, kv_indices);
   CHECK_DIM(1, kv_indptr);
   CHECK_DIM(1, kv_last_page_len);
@@ -60,12 +51,8 @@ void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
   auto device = append_indptr.device();
   CHECK_EQ(append_key.device(), device);
   CHECK_EQ(append_value.device(), device);
-  if (paged_kv_defined) {
-    CHECK_EQ(paged_kv_cache->device(), device);
-  } else {
-    CHECK_EQ(paged_k_cache->device(), device);
-    CHECK_EQ(paged_v_cache->device(), device);
-  }
+  CHECK_EQ(paged_k_cache.device(), device);
+  CHECK_EQ(paged_v_cache.device(), device);
   CHECK_EQ(kv_indices.device(), device);
   CHECK_EQ(kv_indptr.device(), device);
   CHECK_EQ(kv_last_page_len.device(), device);
@@ -73,24 +60,13 @@ void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
   QKVLayout kv_layout = QKVLayout(layout);
 
   unsigned int num_heads, page_size, head_dim;
-  if (paged_kv_defined) {
-    head_dim = paged_kv_cache->size(4);
-    if (kv_layout == QKVLayout::kHND) {
-      num_heads = paged_kv_cache->size(2);
-      page_size = paged_kv_cache->size(3);
-    } else {
-      page_size = paged_kv_cache->size(2);
-      num_heads = paged_kv_cache->size(3);
-    }
+  head_dim = paged_k_cache.size(3);
+  if (kv_layout == QKVLayout::kHND) {
+    num_heads = paged_k_cache.size(1);
+    page_size = paged_k_cache.size(2);
   } else {
-    head_dim = paged_k_cache->size(3);
-    if (kv_layout == QKVLayout::kHND) {
-      num_heads = paged_k_cache->size(1);
-      page_size = paged_k_cache->size(2);
-    } else {
-      page_size = paged_k_cache->size(1);
-      num_heads = paged_k_cache->size(2);
-    }
+    page_size = paged_k_cache.size(1);
+    num_heads = paged_k_cache.size(2);
   }
 
   CHECK_EQ(append_key.size(1), num_heads);
@@ -100,17 +76,15 @@ void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
 
   cudaStream_t torch_current_stream = c10::cuda::getCurrentCUDAStream(device.index());
 
-  auto kv_scalar_dtype =
-      paged_kv_cache.has_value() ? paged_kv_cache->scalar_type() : paged_k_cache->scalar_type();
+  auto kv_scalar_dtype = paged_k_cache.scalar_type();
 
   bool success = DISPATCH_PYTORCH_DTYPE_TO_CTYPE(kv_scalar_dtype, c_type, [&] {
-    paged_kv_t<c_type, int32_t> paged_kv(
-        num_heads, page_size, head_dim, batch_size, kv_layout,
-        static_cast<c_type*>(paged_kv_cache.has_value() ? paged_kv_cache->data_ptr() : nullptr),
-        static_cast<c_type*>(paged_k_cache.has_value() ? paged_k_cache->data_ptr() : nullptr),
-        static_cast<c_type*>(paged_v_cache.has_value() ? paged_v_cache->data_ptr() : nullptr),
-        static_cast<int32_t*>(kv_indices.data_ptr()), static_cast<int32_t*>(kv_indptr.data_ptr()),
-        static_cast<int32_t*>(kv_last_page_len.data_ptr()));
+    paged_kv_t<c_type, int32_t> paged_kv(num_heads, page_size, head_dim, batch_size, kv_layout,
+                                         static_cast<c_type*>(paged_k_cache.data_ptr()),
+                                         static_cast<c_type*>(paged_v_cache.data_ptr()),
+                                         static_cast<int32_t*>(kv_indices.data_ptr()),
+                                         static_cast<int32_t*>(kv_indptr.data_ptr()),
+                                         static_cast<int32_t*>(kv_last_page_len.data_ptr()));
     cudaError_t status =
         AppendPagedKVCache(paged_kv, static_cast<c_type*>(append_key.data_ptr()),
                            static_cast<c_type*>(append_value.data_ptr()),

flashinfer-aot/csrc_aot/batch_decode.cu
@@ -87,36 +87,25 @@ std::vector<int64_t> BatchDecodeWithPagedKVCachePlan(
 
 std::vector<torch::Tensor> BatchDecodeWithPagedKVCacheRun(
     torch::Tensor float_workspace_buffer, torch::Tensor int_workspace_buffer,
-    std::vector<int64_t> plan_info_vec, torch::Tensor q,
-    std::optional<torch::Tensor> paged_kv_cache, std::optional<torch::Tensor> paged_k_cache,
-    std::optional<torch::Tensor> paged_v_cache, torch::Tensor paged_kv_indptr,
-    torch::Tensor paged_kv_indices, torch::Tensor paged_kv_last_page_len,
-    std::optional<torch::Tensor> alibi_slopes, unsigned int kv_layout_code, int window_left,
-    float logits_soft_cap, float sm_scale, float rope_scale, float rope_theta, bool return_lse) {
+    std::vector<int64_t> plan_info_vec, torch::Tensor q, torch::Tensor paged_k_cache,
+    torch::Tensor paged_v_cache, torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
+    torch::Tensor paged_kv_last_page_len, std::optional<torch::Tensor> alibi_slopes,
+    unsigned int kv_layout_code, int window_left, float logits_soft_cap, float sm_scale,
+    float rope_scale, float rope_theta, bool return_lse) {
   DecodePlanInfo plan_info;
   plan_info.FromVector(plan_info_vec);
   QKVLayout kv_layout = static_cast<QKVLayout>(kv_layout_code);
-  bool paged_kv_defined = paged_kv_cache.has_value();
   auto device = q.device();
   int64_t batch_size = q.size(0);
   int64_t num_qo_heads = q.size(1);
   int64_t num_kv_heads, page_size;
-  if (paged_kv_defined) {
-    if (kv_layout == QKVLayout::kHND) {
-      num_kv_heads = paged_kv_cache->size(2);
-      page_size = paged_kv_cache->size(3);
-    } else {
-      page_size = paged_kv_cache->size(2);
-      num_kv_heads = paged_kv_cache->size(3);
-    }
+
+  if (kv_layout == QKVLayout::kHND) {
+    num_kv_heads = paged_k_cache.size(1);
+    page_size = paged_k_cache.size(2);
   } else {
-    if (kv_layout == QKVLayout::kHND) {
-      num_kv_heads = paged_k_cache->size(1);
-      page_size = paged_k_cache->size(2);
-    } else {
-      page_size = paged_k_cache->size(1);
-      num_kv_heads = paged_k_cache->size(2);
-    }
+    page_size = paged_k_cache.size(1);
+    num_kv_heads = paged_k_cache.size(2);
   }
   uint32_t head_dim = q.size(2);
 
@@ -137,8 +126,14 @@ std::vector<torch::Tensor> BatchDecodeWithPagedKVCacheRun(
 
   // get q_scalar_type and kv_scalar_type
   auto q_scalar_type = q.scalar_type();
-  auto kv_scalar_type =
-      paged_kv_cache.has_value() ? paged_kv_cache->scalar_type() : paged_k_cache->scalar_type();
+  auto kv_scalar_type = paged_k_cache.scalar_type();
+
+  // get kv_cache_strides
+  const int64_t* kv_cache_strides = nullptr;
+  auto k_strides = paged_k_cache.strides();
+  auto v_strides = paged_v_cache.strides();
+  TORCH_CHECK(k_strides == v_strides, "k/v strides must be identical");
+  kv_cache_strides = k_strides.data();
 
   DISPATCH_PYTORCH_QKV_DTYPE_TO_CTYPE(q_scalar_type, kv_scalar_type, q_type, kv_type, [&] {
     using DTypeQ = q_type;
@@ -154,10 +149,9 @@ std::vector<torch::Tensor> BatchDecodeWithPagedKVCacheRun(
 
         paged_kv_t<DTypeKV, IdType> paged_kv(
             num_kv_heads, page_size, HEAD_DIM, batch_size, kv_layout,
-            static_cast<DTypeKV*>(paged_kv_cache.has_value() ? paged_kv_cache->data_ptr()
-                                                             : nullptr),
-            static_cast<DTypeKV*>(paged_k_cache.has_value() ? paged_k_cache->data_ptr() : nullptr),
-            static_cast<DTypeKV*>(paged_v_cache.has_value() ? paged_v_cache->data_ptr() : nullptr),
+            static_cast<DTypeKV*>(paged_k_cache.data_ptr()),
+            static_cast<DTypeKV*>(paged_v_cache.data_ptr()),
+            kv_cache_strides,
             static_cast<IdType*>(paged_kv_indices.data_ptr()),
             static_cast<IdType*>(paged_kv_indptr.data_ptr()),
             static_cast<IdType*>(paged_kv_last_page_len.data_ptr()));

flashinfer-aot/csrc_aot/batch_prefill.cu
@@ -198,38 +198,26 @@ std::vector<torch::Tensor> BatchPrefillWithRaggedKVCacheRun(
 std::vector<torch::Tensor> BatchPrefillWithPagedKVCacheRun(
     unsigned int mask_mode_code, torch::Tensor float_workspace_buffer,
     torch::Tensor int_workspace_buffer, std::vector<int64_t> plan_info_vec, torch::Tensor q,
-    std::optional<torch::Tensor> paged_kv_cache, std::optional<torch::Tensor> paged_k_cache,
-    std::optional<torch::Tensor> paged_v_cache, std::optional<torch::Tensor> maybe_custom_mask,
-    std::optional<torch::Tensor> maybe_alibi_slopes, torch::Tensor qo_indptr,
-    torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
+    torch::Tensor paged_k_cache, torch::Tensor paged_v_cache,
+    std::optional<torch::Tensor> maybe_custom_mask, std::optional<torch::Tensor> maybe_alibi_slopes,
+    torch::Tensor qo_indptr, torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
     torch::Tensor paged_kv_last_page_len, std::optional<torch::Tensor> maybe_qk_indptr,
     unsigned int layout, int32_t window_left, float logits_soft_cap, float sm_scale,
     float rope_scale, float rope_theta, bool return_lse) {
   PrefillPlanInfo plan_info;
   plan_info.FromVector(plan_info_vec);
   QKVLayout kv_layout = static_cast<QKVLayout>(layout);
-  bool paged_kv_defined = paged_kv_cache.has_value();
   auto device = q.device();
   int64_t batch_size = paged_kv_indptr.size(0) - 1;
   int64_t num_qo_heads = q.size(1);
   int64_t num_kv_heads, page_size;
   uint32_t head_dim = q.size(2);
-  if (paged_kv_defined) {
-    if (kv_layout == QKVLayout::kHND) {
-      num_kv_heads = paged_kv_cache->size(2);
-      page_size = paged_kv_cache->size(3);
-    } else {
-      page_size = paged_kv_cache->size(2);
-      num_kv_heads = paged_kv_cache->size(3);
-    }
+  if (kv_layout == QKVLayout::kHND) {
+    num_kv_heads = paged_k_cache.size(1);
+    page_size = paged_k_cache.size(2);
   } else {
-    if (kv_layout == QKVLayout::kHND) {
-      num_kv_heads = paged_k_cache->size(1);
-      page_size = paged_k_cache->size(2);
-    } else {
-      page_size = paged_k_cache->size(1);
-      num_kv_heads = paged_k_cache->size(2);
-    }
+    page_size = paged_k_cache.size(1);
+    num_kv_heads = paged_k_cache.size(2);
   }
 
   cudaStream_t torch_current_stream = c10::cuda::getCurrentCUDAStream(device.index());
@@ -248,8 +236,14 @@ std::vector<torch::Tensor> BatchPrefillWithPagedKVCacheRun(
   using IdType = int32_t;
   bool use_logits_soft_cap = logits_soft_cap > 0.f;
   auto q_scalar_type = q.scalar_type();
-  auto kv_scalar_type =
-      paged_kv_cache.has_value() ? paged_kv_cache->scalar_type() : paged_k_cache->scalar_type();
+  auto kv_scalar_type = paged_k_cache.scalar_type();
+
+  // get kv_cache_strides
+  const int64_t* kv_cache_strides = nullptr;
+  auto k_strides = paged_k_cache.strides();
+  auto v_strides = paged_v_cache.strides();
+  TORCH_CHECK(k_strides == v_strides, "k/v strides must be identical");
+  kv_cache_strides = k_strides.data();
 
   DISPATCH_PYTORCH_QKV_DTYPE_TO_CTYPE(q_scalar_type, kv_scalar_type, q_type, kv_type, [&] {
     using DTypeQ = q_type;
@@ -260,12 +254,9 @@ std::vector<torch::Tensor> BatchPrefillWithPagedKVCacheRun(
         return DISPATCH_LOGITS_SOFT_CAP(use_logits_soft_cap, USE_LOGITS_SOFT_CAP, [&] {
           paged_kv_t<DTypeKV, IdType> paged_kv(
               num_kv_heads, page_size, HEAD_DIM, batch_size, kv_layout,
-              static_cast<DTypeKV*>(paged_kv_cache.has_value() ? paged_kv_cache->data_ptr()
-                                                               : nullptr),
-              static_cast<DTypeKV*>(paged_k_cache.has_value() ? paged_k_cache->data_ptr()
-                                                              : nullptr),
-              static_cast<DTypeKV*>(paged_v_cache.has_value() ? paged_v_cache->data_ptr()
-                                                              : nullptr),
+              static_cast<DTypeKV*>(paged_k_cache.data_ptr()),
+              static_cast<DTypeKV*>(paged_v_cache.data_ptr()),
+              kv_cache_strides,
               static_cast<IdType*>(paged_kv_indices.data_ptr()),
               static_cast<IdType*>(paged_kv_indptr.data_ptr()),
               static_cast<IdType*>(paged_kv_last_page_len.data_ptr()));

flashinfer-aot/csrc_aot/flashinfer_ops.cu
@@ -16,9 +16,8 @@
 #include <torch/extension.h>
 
 void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
-                           torch::Tensor append_indptr, std::optional<torch::Tensor> paged_kv_cache,
-                           std::optional<torch::Tensor> paged_k_cache,
-                           std::optional<torch::Tensor> paged_v_cache, torch::Tensor kv_indices,
+                           torch::Tensor append_indptr, torch::Tensor paged_k_cache,
+                           torch::Tensor paged_v_cache, torch::Tensor kv_indices,
                            torch::Tensor kv_indptr, torch::Tensor kv_last_page_len,
                            unsigned int layout);
 

flashinfer-aot/csrc_aot/flashinfer_ops_decode.cu
@@ -31,12 +31,11 @@ std::vector<int64_t> BatchDecodeWithPagedKVCachePlan(
 
 std::vector<torch::Tensor> BatchDecodeWithPagedKVCacheRun(
     torch::Tensor float_workspace_buffer, torch::Tensor int_workspace_buffer,
-    std::vector<int64_t> plan_info_vec, torch::Tensor q,
-    std::optional<torch::Tensor> paged_kv_cache, std::optional<torch::Tensor> paged_k_cache,
-    std::optional<torch::Tensor> paged_v_cache, torch::Tensor paged_kv_indptr,
-    torch::Tensor paged_kv_indices, torch::Tensor paged_kv_last_page_len,
-    std::optional<torch::Tensor> alibi_slopes, unsigned int kv_layout_code, int window_left,
-    float logits_soft_cap, float sm_scale, float rope_scale, float rope_theta, bool return_lse);
+    std::vector<int64_t> plan_info_vec, torch::Tensor q, torch::Tensor paged_k_cache,
+    torch::Tensor paged_v_cache, torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
+    torch::Tensor paged_kv_last_page_len, std::optional<torch::Tensor> alibi_slopes,
+    unsigned int kv_layout_code, int window_left, float logits_soft_cap, float sm_scale,
+    float rope_scale, float rope_theta, bool return_lse);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("single_decode_with_kv_cache", &single_decode_with_kv_cache,

flashinfer-aot/csrc_aot/flashinfer_ops_prefill.cu
@@ -39,10 +39,9 @@ std::vector<torch::Tensor> BatchPrefillWithRaggedKVCacheRun(
 std::vector<torch::Tensor> BatchPrefillWithPagedKVCacheRun(
     unsigned int mask_mode_code, torch::Tensor float_workspace_buffer,
     torch::Tensor int_workspace_buffer, std::vector<int64_t> plan_info_vec, torch::Tensor q,
-    std::optional<torch::Tensor> paged_kv_cache, std::optional<torch::Tensor> paged_k_cache,
-    std::optional<torch::Tensor> paged_v_cache, std::optional<torch::Tensor> maybe_custom_mask,
-    std::optional<torch::Tensor> maybe_alibi_slopes, torch::Tensor qo_indptr,
-    torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
+    torch::Tensor paged_k_cache, torch::Tensor paged_v_cache,
+    std::optional<torch::Tensor> maybe_custom_mask, std::optional<torch::Tensor> maybe_alibi_slopes,
+    torch::Tensor qo_indptr, torch::Tensor paged_kv_indptr, torch::Tensor paged_kv_indices,
     torch::Tensor paged_kv_last_page_len, std::optional<torch::Tensor> maybe_qk_indptr,
     unsigned int layout, int32_t window_left, float logits_soft_cap, float sm_scale,
     float rope_scale, float rope_theta, bool return_lse);

commit 93b5d4e1b35cef24936c906b37dec8f7d71cf43d

bugfix: fix the stride bug in page append (#527)
We introduced a bug in #513 because we didn't consider non-contiguous
kv-cache for page append operator, this PR fix the bug.

include/flashinfer/page.cuh
@@ -80,8 +80,8 @@ struct paged_kv_t {
    * \param head_dim The dimension of each head
    * \param batch_size The batch size
    * \param layout The layout of last 3 dimensions in KV-Cache.
-   * \param k_data The flattened key cache
-   * \param v_data The flattened value cache
+   * \param k_data The start pointer of key cache, k_cache should be contiguous
+   * \param v_data The start pointer of value cache, v_cache should be contiguous
    * \param indices The page indices array
    * \param indptr The page indptr array
    * \param last_page_len The offset of the last page for each request in the batch
@@ -107,20 +107,19 @@ struct paged_kv_t {
   }
 
   /*!
-   * \brief Construct a paged key-value cache
+   * \brief Construct a paged key-value cache with custom kv-cache strides
    * \param num_heads The number of heads
    * \param page_size The size of each page
    * \param head_dim The dimension of each head
    * \param batch_size The batch size
    * \param layout The layout of last 3 dimensions in KV-Cache.
-   * \param k_data The flattened key cache
-   * \param v_data The flattened value cache
+   * \param k_data The start pointer of key cache, k_cache doesn't have to be contiguous
+   * \param v_data The start pointer of value cache, v_cache doesn't have to be contiguous
    * \param kv_strides custom strides of each dimensions of k_data and v_data
    * \param indices The page indices array
    * \param indptr The page indptr array
    * \param last_page_len The offset of the last page for each request in the batch
    * \param rope_pos_offset The start position of each request in the batch.
-   * \note This constructor should only be used when page_storage == kIndices
    */
   __host__ __forceinline__ paged_kv_t(uint32_t num_heads, uint32_t page_size, uint32_t head_dim,
                                       uint32_t batch_size, QKVLayout layout, DType* k_data,

python/csrc/page.cu
@@ -69,6 +69,13 @@ void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
     num_heads = paged_k_cache.size(2);
   }
 
+  // get kv_cache_strides
+  const int64_t* kv_cache_strides = nullptr;
+  auto k_strides = paged_k_cache.strides();
+  auto v_strides = paged_v_cache.strides();
+  TORCH_CHECK(k_strides == v_strides, "k/v strides must be identical");
+  kv_cache_strides = k_strides.data();
+
   CHECK_EQ(append_key.size(1), num_heads);
   CHECK_EQ(append_key.size(2), head_dim);
   CHECK_EQ(append_value.size(1), num_heads);
@@ -79,12 +86,12 @@ void append_paged_kv_cache(torch::Tensor append_key, torch::Tensor append_value,
   auto kv_scalar_dtype = paged_k_cache.scalar_type();
 
   bool success = DISPATCH_PYTORCH_DTYPE_TO_CTYPE(kv_scalar_dtype, c_type, [&] {
-    paged_kv_t<c_type, int32_t> paged_kv(num_heads, page_size, head_dim, batch_size, kv_layout,
-                                         static_cast<c_type*>(paged_k_cache.data_ptr()),
-                                         static_cast<c_type*>(paged_v_cache.data_ptr()),
-                                         static_cast<int32_t*>(kv_indices.data_ptr()),
-                                         static_cast<int32_t*>(kv_indptr.data_ptr()),
-                                         static_cast<int32_t*>(kv_last_page_len.data_ptr()));
+    paged_kv_t<c_type, int32_t> paged_kv(
+        num_heads, page_size, head_dim, batch_size, kv_layout,
+        static_cast<c_type*>(paged_k_cache.data_ptr()),
+        static_cast<c_type*>(paged_v_cache.data_ptr()), kv_cache_strides,
+        static_cast<int32_t*>(kv_indices.data_ptr()), static_cast<int32_t*>(kv_indptr.data_ptr()),
+        static_cast<int32_t*>(kv_last_page_len.data_ptr()));
     cudaError_t status =
         AppendPagedKVCache(paged_kv, static_cast<c_type*>(append_key.data_ptr()),
                            static_cast<c_type*>(append_value.data_ptr()),