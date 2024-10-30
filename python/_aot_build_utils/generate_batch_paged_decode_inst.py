flashinfer-ai/flashinfer

commit 7df90ddfbb839c9e5b3723f43ab0b48a906ad286

refactor: Refactor JIT and AOT build script (#567)
Previously, JIT and AOT packaging is a bit broken. This PR produces good
sdist for JIT mode, and wheel for AOT mode.

## Changes

Common changes:
1. Remove the symlinks. Symlinks causes lots of duplication when search
in VSCode.
2. In package distribution (sdist or wheel), add data files to
`python/flashinfer/data/`, i.e. inside the python package folder. This
is strongly recommended by setuptools.
* Data files include: `version.txt`, FlashInfer headers, Cutlass
headers.
* Symlinks will be created when building wheel, and will be removed when
finished unless it's using `develop` command.
3. Exclude unneeded cutlass docs and files from wheel and sdist.

AOT changes:
1. Remove `flashinfer-aot` dir. Contents are moved to `python/`.
2. Merge all kernels into one pybind. This is good for compilation
speed. (`_kernels_sm90` is preserved as a separated `.so` file.)
3. AOT wheel can now be built with the following command:
    ```bash
    cd flashinfer/python
TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0a" python3 aot_setup.py bdist_wheel
    ls -la dist/
    ```
4. AOT wheel can also be built for editable install (develop purpose)
    ```bash
    cd flashinfer/python
    TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0a" python3 aot_setup.py develop
    ```

JIT changes:
1. JIT mode can now be installed in various ways:
    ```bash
    cd flashinfer/python
    pip install -v .     # Regular install from source
    pip install -v -e .  # Editable install

    python -m build --sdist               # Build sdist
    pip install dist/flashinfer-*.tar.gz  # Install from sdist
    ```

## Directory structure of built package

See attached.

[dir-wheel.txt](https://github.com/user-attachments/files/17562193/dir-wheel.txt)

[dir-sdist.txt](https://github.com/user-attachments/files/17562194/dir-sdist.txt)

## Tests

I was able to pass `pytest -sv test_norm.py test_bmm_fp8.py` using

python/_aot_build_utils/generate_batch_paged_decode_inst.py
@@ -14,62 +14,58 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import sys
 import re
-from literal_map import (
-    pos_encoding_mode_literal,
-    dtype_literal,
-    mask_mode_literal,
-)
+import sys
 from pathlib import Path
+
+from .literal_map import (
+    dtype_literal,
+    idtype_literal,
+    pos_encoding_mode_literal,
+)
 
 
 def get_cu_file_str(
     head_dim,
     pos_encoding_mode,
-    allow_fp16_qk_reduction,
-    mask_mode,
     dtype_q,
     dtype_kv,
     dtype_out,
+    idtype,
 ):
-
     content = """#include <flashinfer/attention_impl.cuh>
 
 namespace flashinfer {{
 
-using ParamsT = SinglePrefillParams<{dtype_q}, {dtype_kv}, {dtype_out}>;
+using ParamsT = BatchDecodeParams<{dtype_q}, {dtype_kv}, {dtype_out}, {idtype}>;
 
-template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, {allow_fp16_qk_reduction}, {mask_mode}, ComposedAttention<ParamsT, get_variant_code(
-    {use_custom_mask}, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/false, /*use_alibi_bias=*/false)>>(
+template cudaError_t BatchDecodeWithPagedKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
+    /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/false, /*use_alibi_bias=*/false)>>(
     ParamsT params,
-    {dtype_out}* tmp,
+    {dtype_out}* tmp_v, float* tmp_s,
     cudaStream_t stream);
 
-template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, {allow_fp16_qk_reduction}, {mask_mode}, ComposedAttention<ParamsT, get_variant_code(
-    {use_custom_mask}, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/true, /*use_alibi_bias=*/false)>>(
+template cudaError_t BatchDecodeWithPagedKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
+    /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/true, /*use_alibi_bias=*/false)>>(
     ParamsT params,
-    {dtype_out}* tmp,
+    {dtype_out}* tmp_v, float* tmp_s,
     cudaStream_t stream);
-
 }}
     """.format(
         head_dim=head_dim,
         pos_encoding_mode=pos_encoding_mode_literal[int(pos_encoding_mode)],
-        allow_fp16_qk_reduction=allow_fp16_qk_reduction,
-        mask_mode=mask_mode_literal[int(mask_mode)],
         dtype_q=dtype_literal[dtype_q],
         dtype_kv=dtype_literal[dtype_kv],
         dtype_out=dtype_literal[dtype_out],
-        use_custom_mask="true" if int(mask_mode) == 2 else "false",
+        idtype=idtype_literal[idtype],
     )
     return content
 
 
 if __name__ == "__main__":
     pattern = (
-        r"single_prefill_head_([0-9]+)_posenc_([0-9]+)_"
-        r"fp16qkred_([a-z]+)_mask_([0-9]+)_dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)\.cu"
+        r"batch_paged_decode_head_([0-9]+)_posenc_([0-9]+)_"
+        r"dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)_idtype_([a-z0-9]+)\.cu"
     )
 
     compiled_pattern = re.compile(pattern)

python/_aot_build_utils/generate_batch_paged_prefill_inst.py
@@ -14,16 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import sys
 import re
-import itertools
-from literal_map import (
-    mask_mode_literal,
-    pos_encoding_mode_literal,
+import sys
+from pathlib import Path
+
+from .literal_map import (
     dtype_literal,
     idtype_literal,
+    mask_mode_literal,
+    pos_encoding_mode_literal,
 )
-from pathlib import Path
 
 
 def get_cu_file_str(

python/_aot_build_utils/generate_batch_ragged_prefill_inst.py
@@ -14,15 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import sys
 import re
-from literal_map import (
-    mask_mode_literal,
-    pos_encoding_mode_literal,
+import sys
+from pathlib import Path
+
+from .literal_map import (
     dtype_literal,
     idtype_literal,
+    mask_mode_literal,
+    pos_encoding_mode_literal,
 )
-from pathlib import Path
 
 
 def get_cu_file_str(

python/_aot_build_utils/generate_dispatch_inc.py
@@ -16,10 +16,11 @@ limitations under the License.
 
 import argparse
 from pathlib import Path
-from literal_map import (
-    pos_encoding_mode_literal,
+
+from .literal_map import (
     bool_literal,
     mask_mode_literal,
+    pos_encoding_mode_literal,
 )
 
 

python/_aot_build_utils/generate_single_decode_inst.py
@@ -14,14 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import sys
 import re
-from literal_map import (
-    pos_encoding_mode_literal,
-    dtype_literal,
-    idtype_literal,
-)
+import sys
 from pathlib import Path
+
+from .literal_map import (
+    dtype_literal,
+    pos_encoding_mode_literal,
+)
 
 
 def get_cu_file_str(
@@ -30,24 +30,23 @@ def get_cu_file_str(
     dtype_q,
     dtype_kv,
     dtype_out,
-    idtype,
 ):
     content = """#include <flashinfer/attention_impl.cuh>
 
 namespace flashinfer {{
 
-using ParamsT = BatchDecodeParams<{dtype_q}, {dtype_kv}, {dtype_out}, {idtype}>;
+using ParamsT = SingleDecodeParams<{dtype_q}, {dtype_kv}, {dtype_out}>;
 
-template cudaError_t BatchDecodeWithPagedKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
+template cudaError_t SingleDecodeWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
     /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/false, /*use_alibi_bias=*/false)>>(
     ParamsT params,
-    {dtype_out}* tmp_v, float* tmp_s,
+    {dtype_out}* tmp,
     cudaStream_t stream);
 
-template cudaError_t BatchDecodeWithPagedKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
+template cudaError_t SingleDecodeWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
     /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/true, /*use_alibi_bias=*/false)>>(
     ParamsT params,
-    {dtype_out}* tmp_v, float* tmp_s,
+    {dtype_out}* tmp,
     cudaStream_t stream);
 }}
     """.format(
@@ -56,20 +55,20 @@ template cudaError_t BatchDecodeWithPagedKVCacheDispatched<{head_dim}, {pos_enco
         dtype_q=dtype_literal[dtype_q],
         dtype_kv=dtype_literal[dtype_kv],
         dtype_out=dtype_literal[dtype_out],
-        idtype=idtype_literal[idtype],
     )
     return content
 
 
 if __name__ == "__main__":
     pattern = (
-        r"batch_paged_decode_head_([0-9]+)_posenc_([0-9]+)_"
-        r"dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)_idtype_([a-z0-9]+)\.cu"
+        r"single_decode_head_([0-9]+)_posenc_([0-9]+)_"
+        r"dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)\.cu"
     )
 
     compiled_pattern = re.compile(pattern)
     path = Path(sys.argv[1])
     fname = path.name
     match = compiled_pattern.match(fname)
+
     with open(path, "w") as f:
         f.write(get_cu_file_str(*match.groups()))

python/_aot_build_utils/generate_single_prefill_inst.py
@@ -14,60 +14,68 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import sys
 import re
-from literal_map import (
-    pos_encoding_mode_literal,
-    dtype_literal,
-)
+import sys
 from pathlib import Path
+
+from .literal_map import (
+    dtype_literal,
+    mask_mode_literal,
+    pos_encoding_mode_literal,
+)
 
 
 def get_cu_file_str(
     head_dim,
     pos_encoding_mode,
+    allow_fp16_qk_reduction,
+    mask_mode,
     dtype_q,
     dtype_kv,
     dtype_out,
 ):
+
     content = """#include <flashinfer/attention_impl.cuh>
 
 namespace flashinfer {{
 
-using ParamsT = SingleDecodeParams<{dtype_q}, {dtype_kv}, {dtype_out}>;
+using ParamsT = SinglePrefillParams<{dtype_q}, {dtype_kv}, {dtype_out}>;
 
-template cudaError_t SingleDecodeWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
-    /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/false, /*use_alibi_bias=*/false)>>(
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, {allow_fp16_qk_reduction}, {mask_mode}, ComposedAttention<ParamsT, get_variant_code(
+    {use_custom_mask}, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/false, /*use_alibi_bias=*/false)>>(
     ParamsT params,
     {dtype_out}* tmp,
     cudaStream_t stream);
 
-template cudaError_t SingleDecodeWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, ComposedAttention<ParamsT, get_variant_code(
-    /*use_custom_mask=*/false, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/true, /*use_alibi_bias=*/false)>>(
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {pos_encoding_mode}, {allow_fp16_qk_reduction}, {mask_mode}, ComposedAttention<ParamsT, get_variant_code(
+    {use_custom_mask}, /*use_sliding_window=*/true, /*use_logits_soft_cap=*/true, /*use_alibi_bias=*/false)>>(
     ParamsT params,
     {dtype_out}* tmp,
     cudaStream_t stream);
+
 }}
     """.format(
         head_dim=head_dim,
         pos_encoding_mode=pos_encoding_mode_literal[int(pos_encoding_mode)],
+        allow_fp16_qk_reduction=allow_fp16_qk_reduction,
+        mask_mode=mask_mode_literal[int(mask_mode)],
         dtype_q=dtype_literal[dtype_q],
         dtype_kv=dtype_literal[dtype_kv],
         dtype_out=dtype_literal[dtype_out],
+        use_custom_mask="true" if int(mask_mode) == 2 else "false",
     )
     return content
 
 
 if __name__ == "__main__":
     pattern = (
-        r"single_decode_head_([0-9]+)_posenc_([0-9]+)_"
-        r"dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)\.cu"
+        r"single_prefill_head_([0-9]+)_posenc_([0-9]+)_"
+        r"fp16qkred_([a-z]+)_mask_([0-9]+)_dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)\.cu"
     )
 
     compiled_pattern = re.compile(pattern)
     path = Path(sys.argv[1])
     fname = path.name
     match = compiled_pattern.match(fname)
-
     with open(path, "w") as f:
         f.write(get_cu_file_str(*match.groups()))

.gitignore
@@ -13,7 +13,12 @@ src/generated/
 python/csrc/generated/
 python/flashinfer/_build_meta.py
 python/flashinfer/jit/aot_config.py
-flashinfer-aot/csrc_aot/generated/
+python/csrc_aot/generated/
+
+# Package files
+python/flashinfer/data/
+python/flashinfer/version.txt
+python/MANIFEST.in
 
 # Generated documentation files
 docs/generated

flashinfer-aot/3rdparty
@@ -1,1 +1,0 @@
-../3rdparty

commit d5a585c2e029c21422cc9f614cf5df881edf55f9

fix broken cpp integration caused by #567

CMakeLists.txt
@@ -110,8 +110,8 @@ file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/src/generated)
 set(dispatch_inc_file ${PROJECT_SOURCE_DIR}/src/dispatch.inc)
 add_custom_command(
   OUTPUT ${dispatch_inc_file}
-  COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_dispatch_inc.py --path ${PROJECT_SOURCE_DIR}/src/dispatch.inc --head_dims ${HEAD_DIMS} --pos_encoding_modes ${POS_ENCODING_MODES} --allow_fp16_qk_reductions ${ALLOW_FP16_QK_REDUCTIONS} --mask_modes ${MASK_MODES}
-  DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_dispatch_inc.py
+  COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_dispatch_inc.py --path ${PROJECT_SOURCE_DIR}/src/dispatch.inc --head_dims ${HEAD_DIMS} --pos_encoding_modes ${POS_ENCODING_MODES} --allow_fp16_qk_reductions ${ALLOW_FP16_QK_REDUCTIONS} --mask_modes ${MASK_MODES}
+  DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_dispatch_inc.py
   COMMENT "Generating additional source file ${generated_dispatch_inc}"
   VERBATIM
 )
@@ -124,8 +124,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
       set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/single_decode_head_${head_dim}_posenc_${pos_encoding_mode}_dtypeq_${dtype}_dtypekv_${dtype}_dtypeout_${dtype}.cu)
       add_custom_command(
         OUTPUT ${generated_kernel_src}
-        COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_decode_inst.py ${generated_kernel_src}
-        DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_decode_inst.py
+        COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_decode_inst.py ${generated_kernel_src}
+        DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_decode_inst.py
         COMMENT "Generating additional source file ${generated_kernel_src}"
         VERBATIM
       )
@@ -137,8 +137,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
       set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/single_decode_head_${head_dim}_posenc_${pos_encoding_mode}_dtypeq_f16_dtypekv_${dtype_kv}_dtypeout_f16.cu)
       add_custom_command(
         OUTPUT ${generated_kernel_src}
-        COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_decode_inst.py ${generated_kernel_src}
-        DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_decode_inst.py
+        COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_decode_inst.py ${generated_kernel_src}
+        DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_decode_inst.py
         COMMENT "Generating additional source file ${generated_kernel_src}"
         VERBATIM
       )
@@ -156,8 +156,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
         set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_paged_decode_head_${head_dim}_posenc_${pos_encoding_mode}_dtypeq_${dtype}_dtypekv_${dtype}_dtypeout_${dtype}_idtype_${idtype}.cu)
         add_custom_command(
           OUTPUT ${generated_kernel_src}
-          COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_decode_inst.py ${generated_kernel_src}
-          DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_decode_inst.py
+          COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_decode_inst.py ${generated_kernel_src}
+          DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_decode_inst.py
           COMMENT "Generating additional source file ${generated_kernel_src}"
           VERBATIM
         )
@@ -169,8 +169,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
         set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_paged_decode_head_${head_dim}_posenc_${pos_encoding_mode}_dtypeq_f16_dtypekv_${dtype_kv}_dtypeout_f16_idtype_${idtype}.cu)
         add_custom_command(
           OUTPUT ${generated_kernel_src}
-          COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_decode_inst.py ${generated_kernel_src}
-          DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_decode_inst.py
+          COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_decode_inst.py ${generated_kernel_src}
+          DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_decode_inst.py
           COMMENT "Generating additional source file ${generated_kernel_src}"
           VERBATIM
         )
@@ -193,8 +193,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
           set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/single_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_${dtype}_dtypekv_${dtype}_dtypeout_${dtype}.cu)
           add_custom_command(
             OUTPUT ${generated_kernel_src}
-            COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_prefill_inst.py ${generated_kernel_src}
-            DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_prefill_inst.py
+            COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_prefill_inst.py ${generated_kernel_src}
+            DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_prefill_inst.py
             COMMENT "Generating additional source file ${generated_kernel_src}"
             VERBATIM
           )
@@ -205,8 +205,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
           set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/single_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_f16_dtypekv_${dtype_kv}_dtypeout_f16.cu)
           add_custom_command(
             OUTPUT ${generated_kernel_src}
-            COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_prefill_inst.py ${generated_kernel_src}
-            DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_single_prefill_inst.py
+            COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_prefill_inst.py ${generated_kernel_src}
+            DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_single_prefill_inst.py
             COMMENT "Generating additional source file ${generated_kernel_src}"
             VERBATIM
           )
@@ -227,8 +227,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
             set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_paged_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_${dtype}_dtypekv_${dtype}_dtypeout_${dtype}_idtype_${idtype}.cu)
             add_custom_command(
               OUTPUT ${generated_kernel_src}
-              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_prefill_inst.py ${generated_kernel_src}
-              DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_prefill_inst.py
+              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_prefill_inst.py ${generated_kernel_src}
+              DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_prefill_inst.py
               COMMENT "Generating additional source file ${generated_kernel_src}"
               VERBATIM
             )
@@ -239,8 +239,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
             set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_paged_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_f16_dtypekv_${dtype_kv}_dtypeout_f16_idtype_${idtype}.cu)
             add_custom_command(
               OUTPUT ${generated_kernel_src}
-              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_prefill_inst.py ${generated_kernel_src}
-              DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_paged_prefill_inst.py
+              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_prefill_inst.py ${generated_kernel_src}
+              DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_paged_prefill_inst.py
               COMMENT "Generating additional source file ${generated_kernel_src}"
               VERBATIM
             )
@@ -262,8 +262,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
             set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_ragged_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_${dtype}_dtypekv_${dtype}_dtypeout_${dtype}_idtype_${idtype}.cu)
             add_custom_command(
               OUTPUT ${generated_kernel_src}
-              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_ragged_prefill_inst.py ${generated_kernel_src}
-              DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_ragged_prefill_inst.py
+              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_ragged_prefill_inst.py ${generated_kernel_src}
+              DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_ragged_prefill_inst.py
               COMMENT "Generating additional source file ${generated_kernel_src}"
               VERBATIM
             )
@@ -274,8 +274,8 @@ foreach(head_dim IN LISTS HEAD_DIMS)
             set(generated_kernel_src ${PROJECT_SOURCE_DIR}/src/generated/batch_ragged_prefill_head_${head_dim}_posenc_${pos_encoding_mode}_fp16qkred_${allow_fp16_qk_reduction}_mask_${mask_mode}_dtypeq_f16_dtypekv_${dtype_kv}_dtypeout_f16_idtype_${idtype}.cu)
             add_custom_command(
               OUTPUT ${generated_kernel_src}
-              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_ragged_prefill_inst.py ${generated_kernel_src}
-              DEPENDS ${PROJECT_SOURCE_DIR}/flashinfer-aot/generate_batch_ragged_prefill_inst.py
+              COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_ragged_prefill_inst.py ${generated_kernel_src}
+              DEPENDS ${PROJECT_SOURCE_DIR}/python/_aot_build_utils/generate_batch_ragged_prefill_inst.py
               COMMENT "Generating additional source file ${generated_kernel_src}"
               VERBATIM
             )

python/_aot_build_utils/generate_batch_paged_decode_inst.py
@@ -18,11 +18,19 @@ import re
 import sys
 from pathlib import Path
 
-from .literal_map import (
-    dtype_literal,
-    idtype_literal,
-    pos_encoding_mode_literal,
-)
+if __package__:
+    from .literal_map import (
+        dtype_literal,
+        idtype_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        dtype_literal,
+        idtype_literal,
+        pos_encoding_mode_literal,
+    )
 
 
 def get_cu_file_str(

python/_aot_build_utils/generate_batch_paged_prefill_inst.py
@@ -18,12 +18,21 @@ import re
 import sys
 from pathlib import Path
 
-from .literal_map import (
-    dtype_literal,
-    idtype_literal,
-    mask_mode_literal,
-    pos_encoding_mode_literal,
-)
+if __package__:
+    from .literal_map import (
+        dtype_literal,
+        idtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        dtype_literal,
+        idtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
 
 
 def get_cu_file_str(

python/_aot_build_utils/generate_batch_ragged_prefill_inst.py
@@ -18,12 +18,21 @@ import re
 import sys
 from pathlib import Path
 
-from .literal_map import (
-    dtype_literal,
-    idtype_literal,
-    mask_mode_literal,
-    pos_encoding_mode_literal,
-)
+if __package__:
+    from .literal_map import (
+        dtype_literal,
+        idtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        dtype_literal,
+        idtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
 
 
 def get_cu_file_str(

python/_aot_build_utils/generate_dispatch_inc.py
@@ -14,14 +14,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import sys
 import argparse
 from pathlib import Path
 
-from .literal_map import (
-    bool_literal,
-    mask_mode_literal,
-    pos_encoding_mode_literal,
-)
+if __package__:
+    from .literal_map import (
+        bool_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        bool_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
 
 
 def get_dispatch_inc_str(args: argparse.Namespace) -> str:

python/_aot_build_utils/generate_single_decode_inst.py
@@ -18,11 +18,17 @@ import re
 import sys
 from pathlib import Path
 
-from .literal_map import (
-    dtype_literal,
-    pos_encoding_mode_literal,
-)
-
+if __package__:
+    from .literal_map import (
+        dtype_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        dtype_literal,
+        pos_encoding_mode_literal,
+    )
 
 def get_cu_file_str(
     head_dim,

python/_aot_build_utils/generate_single_prefill_inst.py
@@ -18,11 +18,19 @@ import re
 import sys
 from pathlib import Path
 
-from .literal_map import (
-    dtype_literal,
-    mask_mode_literal,
-    pos_encoding_mode_literal,
-)
+if __package__:
+    from .literal_map import (
+        dtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
+else:
+    sys.path.append(str(Path(__file__).resolve().parents[1] / "_aot_build_utils"))
+    from literal_map import (
+        dtype_literal,
+        mask_mode_literal,
+        pos_encoding_mode_literal,
+    )
 
 
 def get_cu_file_str(