32daeb6559ab2c2f3ceaadfb6e7f5a7f9abd3609
Zihao Ye <expye@outlook.com>
2024-11-20 11:41:54 +0000

    upd

--- python/csrc/flashinfer_ops.cu
+++ python/csrc/flashinfer_ops.cu
@@ -17,9 +17,9 @@
 
 //========== activation ==========
 
-void silu_and_mul(at::Tensor& out, at::Tensor& input, int cuda_stream);
-void gelu_tanh_and_mul(at::Tensor& out, at::Tensor& input, int cuda_stream);
-void gelu_and_mul(at::Tensor& out, at::Tensor& input, int cuda_stream);
+void silu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream);
+void gelu_tanh_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream);
+void gelu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream);
 
 //========== cascade ==========
 
