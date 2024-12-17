flashinfer-ai/flashinfer

commit 51236c913107f2f6098ac039a4aaa4841a443c25

perf: Dense and sparse customizable flashattention-3 template (#667)
This PR adds flashattention-3 template for improving prefill performance
on hopper. Block/Vector-sparse support in FlashInfer early version are
ported to FA-3 template with CustomStride abstraction in CuTE so that we
can support PageAttention with any page size. The programming interface
for FA3 template is exactly the same as our previous FA2 template while
we add an argument `backend` to allow user to select their own backend.

Functionalities that are missing in current template include custom mask
and we plan to support it using JIT instead of AOT.

H100 Reference performance on variable-length dense and sparse attention
kernels (exposed through
[BatchPrefillWithRaggedKVCacheWrapper](https://docs.flashinfer.ai/api/prefill.html#flashinfer.prefill.BatchPrefillWithRaggedKVCacheWrapper)
and
[BatchDecodeWithPagedKVCacheWrapper](https://docs.flashinfer.ai/api/decode.html#flashinfer.decode.BatchDecodeWithPagedKVCacheWrapper)
API correspondingly, for sparse attention workload, we use PageAttention
with `page_size=1`.

![image](https://github.com/user-attachments/assets/7e989f8c-8b0f-4c99-ad11-6102c2dc5090)

FlashInfer's vector sparse (page_size=1) attention implementation can
get 90% percent of the dense equivalent, reference benchmark:
https://github.com/flashinfer-ai/flashinfer/blob/04ee9bceb5ab0a66c612c1abaee8fa28de2b2349/benchmarks/bench_hopper_attention
.

JIT support is left to the next PR because this PR is already heavy. For
fp8 support, we will incorporate SageAttention-2 algorithm for numerical
stability, and it's left to v0.2.1.

Currently there is some discrepancy in attention variant interface for
our FA2 and FA3 template and we will gradually fix the gap.

cc @merrymercy @zhyncs @youkaichao @woosukkwon @jason-huang03

include/flashinfer/attention/hopper/mainloop_mma.cuh
@@ -1,0 +1,265 @@
+/*
+ * Copyright (c) 2024, Jay Shah, Ganesh Bikshandi, Ying Zhang, Vijay Thakkar, Pradeep Ramani, Tri
+ * Dao. Licensed under the BSD 3-Clause.
+ *
+ * Modified by the FlashInfer team.
+ */
+#ifndef FLASHINFER_ATTENTION_HOPPER_MAINLOOP_MMA_CUH_
+#define FLASHINFER_ATTENTION_HOPPER_MAINLOOP_MMA_CUH_
+
+#include <cutlass/array.h>
+#include <cutlass/cutlass.h>
+#include <cutlass/numeric_conversion.h>
+#include <cutlass/numeric_types.h>
+
+namespace flashinfer {
+
+template <typename Ktraits, bool LEFT_SLIDING_WINDOW, bool CAUSAL, typename WarpScheduler,
+          typename AttentionVariant, typename Params, typename MainloopPipeline,
+          typename PipelineState, typename SharedStorage, typename FrgTensorO,
+          typename AttentionUpdater>
+CUTLASS_DEVICE void mma_f16(const Params& mainloop_params, AttentionVariant& variant,
+                            MainloopPipeline pipeline_k, MainloopPipeline pipeline_v,
+                            PipelineState& smem_pipe_read_k, PipelineState& smem_pipe_read_v,
+                            FrgTensorO& tOrO, AttentionUpdater& attention_updater,
+                            int kv_tile_idx_count, int swa_begin_kv_tile_idx,
+                            int swa_end_kv_tile_idx, int thread_idx, int work_idx, int q_tile_idx,
+                            SharedStorage& shared_storage, const int32_t qo_len,
+                            const int32_t kv_len, const int32_t qo_head_idx,
+                            const int32_t kv_head_idx) {
+  using DTypeQ = typename Ktraits::DTypeQ;
+  using DTypeKV = typename Ktraits::DTypeKV;
+  using IdType = typename Ktraits::IdType;
+  using TileShape_QKD = typename Ktraits::TileShape_QKD;
+  static constexpr int NUM_MMA_THREADS = Ktraits::NUM_MMA_THREADS;
+  using SmemLayoutQ = typename Ktraits::SmemLayoutQ;
+  using SmemLayoutK = typename Ktraits::SmemLayoutK;
+  using SmemLayoutV = typename Ktraits::SmemLayoutV;
+  using SmemLayoutVt = typename Ktraits::SmemLayoutVt;
+  static_assert(is_rmem<FrgTensorO>::value, "O tensor must be rmem resident.");
+
+  static constexpr int CTA_Q = get<0>(TileShape_QKD{});
+  static constexpr int CTA_KV = get<1>(TileShape_QKD{});
+
+  Tensor sQ = make_tensor(make_smem_ptr(shared_storage.smem_q.data()), SmemLayoutQ{});
+  Tensor sK = make_tensor(make_smem_ptr(shared_storage.smem_k.data()), SmemLayoutK{});
+  Tensor sVt = make_tensor(make_smem_ptr(shared_storage.smem_v.data()), SmemLayoutVt{});
+
+  typename Ktraits::TiledMmaQK tiled_mma_qk;
+  typename Ktraits::TiledMmaPV tiled_mma_pv;
+  auto threadMmaQK = tiled_mma_qk.get_thread_slice(thread_idx);
+  auto threadMmaPV = tiled_mma_pv.get_thread_slice(thread_idx);
+
+  Tensor tSrQ = threadMmaQK.partition_fragment_A(sQ);
+  Tensor tSrK = threadMmaQK.partition_fragment_B(sK);
+  Tensor tOrV = threadMmaPV.partition_fragment_B(sVt);
+
+  auto consumer_wait = [](auto& pipeline, auto& smem_pipe_read) {
+    auto barrier_token = pipeline.consumer_try_wait(smem_pipe_read);
+    pipeline.consumer_wait(smem_pipe_read, barrier_token);
+  };
+
+  tiled_mma_pv.accumulate_ = GMMA::ScaleOut::Zero;
+  int kv_tile_idx = kv_tile_idx_count - 1;
+
+  cutlass::ConsumerToken barrier_token =
+      static_cast<cutlass::BarrierStatus>(shared_storage.barrier_Q.try_wait(work_idx % 2));
+  if (barrier_token == cutlass::BarrierStatus::WaitAgain) {
+    shared_storage.barrier_Q.wait(work_idx % 2);
+  }
+
+  Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0, 1>(TileShape_QKD{}));
+  consumer_wait(pipeline_k, smem_pipe_read_k);
+
+  WarpScheduler::barrier_sync();
+  gemm</*init=*/true, /*wg_wait=*/-1>(tiled_mma_qk, tSrQ, tSrK(_, _, _, smem_pipe_read_k.index()),
+                                      tSrS);
+  WarpScheduler::barrier_arrive();
+
+  if (work_idx != 0) {
+    int lane_predicate = cute::elect_one_sync();
+    if (cutlass::canonical_warp_idx_sync() == Ktraits::NUM_WARPS - 1 && lane_predicate) {
+#pragma unroll
+      for (uint32_t cta_id = 0; cta_id < 1; ++cta_id) {
+        shared_storage.barrier_O.arrive(cta_id, lane_predicate);
+      }
+    }
+  }
+  warpgroup_wait<0>();
+  pipeline_k.consumer_release(smem_pipe_read_k);
+  ++smem_pipe_read_k;
+
+  auto col_limit_right = [&](int qo_idx) { return qo_idx + 1 + kv_len - qo_len; };
+  auto col_limit_left = [&](int qo_idx) {
+    return qo_idx + kv_len - qo_len - mainloop_params.window_left;
+  };
+  {
+    Tensor cS = cute::make_identity_tensor(select<0, 1>(TileShape_QKD{}));
+    Tensor tScS = threadMmaQK.partition_C(cS);
+#pragma unroll
+    for (int i = 0; i < size(tSrS); ++i) {
+      int qo_idx = get<0>(tScS(i)) + q_tile_idx * CTA_Q;
+      int kv_idx = get<1>(tScS(i)) + kv_tile_idx * CTA_KV;
+      tSrS(i) = variant.LogitsTransform(mainloop_params, tSrS(i), /*batch_idx=*/0, qo_idx, kv_idx,
+                                        qo_head_idx, kv_head_idx);
+      if constexpr (!CAUSAL) {  // Just masking based on col
+        if (kv_idx >= kv_len) {
+          tSrS(i) = -math::inf;
+        }
+      } else {
+        if (kv_idx >= std::min(kv_len, col_limit_right(qo_idx))) {
+          tSrS(i) = -math::inf;
+        }
+      }
+      if constexpr (LEFT_SLIDING_WINDOW) {
+        if (kv_idx < col_limit_left(qo_idx)) {
+          tSrS(i) = -math::inf;
+        }
+      }
+    }
+  }
+
+  attention_updater.update</*init=*/true>(tSrS);
+  Tensor tOrP = make_tensor(convert_type<DTypeKV>(tSrS).data(),
+                            convert_layout_acc_Aregs<typename Ktraits::TiledMmaPV>(tSrS.layout()));
+
+  constexpr int n_masking_steps = CAUSAL ? cute::ceil_div(CTA_Q, CTA_KV) : 0;
+  // masking loops
+#pragma unroll
+  for (int masking_step = 0; masking_step < n_masking_steps && kv_tile_idx > swa_begin_kv_tile_idx;
+       ++masking_step, --kv_tile_idx) {
+    Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0, 1>(TileShape_QKD{}));
+    consumer_wait(pipeline_k, smem_pipe_read_k);
+    WarpScheduler::barrier_sync();
+    gemm</*init=*/true, /*wg_wait=*/-1>(tiled_mma_qk, tSrQ, tSrK(_, _, _, smem_pipe_read_k.index()),
+                                        tSrS);
+    if (masking_step > 0) {
+      attention_updater.rescale_o(tOrO);
+    }
+    consumer_wait(pipeline_v, smem_pipe_read_v);
+    gemm</*init=*/false, /*wg_wait=*/-1>(tiled_mma_pv, tOrP,
+                                         tOrV(_, _, _, smem_pipe_read_v.index()), tOrO);
+    WarpScheduler::barrier_arrive();
+    warpgroup_wait<1>();
+    pipeline_k.consumer_release(smem_pipe_read_k);  // release K
+    Tensor cS = cute::make_identity_tensor(select<0, 1>(TileShape_QKD{}));
+    Tensor tScS = threadMmaQK.partition_C(cS);
+#pragma unroll
+    for (int i = 0; i < size(tSrS); ++i) {
+      int qo_idx = get<0>(tScS(i)) + q_tile_idx * CTA_Q;
+      int kv_idx = get<1>(tScS(i)) + (kv_tile_idx - 1) * CTA_KV;
+      tSrS(i) = variant.LogitsTransform(mainloop_params, tSrS(i), /*batch_idx=*/0, qo_idx, kv_idx,
+                                        qo_head_idx, kv_head_idx);
+      if (kv_idx >= col_limit_right(qo_idx)) {
+        tSrS(i) = -math::inf;
+      }
+      if constexpr (LEFT_SLIDING_WINDOW) {
+        if (kv_idx < col_limit_left(qo_idx)) {
+          tSrS(i) = -math::inf;
+        }
+      }
+    }
+    attention_updater.update</*init=*/false>(tSrS);
+    warpgroup_wait<0>();
+    pipeline_v.consumer_release(smem_pipe_read_v);  // release V
+    ++smem_pipe_read_k;
+    ++smem_pipe_read_v;
+    cute::copy(make_tensor(convert_type<DTypeKV>(tSrS).data(),
+                           convert_layout_acc_Aregs<typename Ktraits::TiledMmaPV>(tSrS.layout())),
+               tOrP);
+  }
+
+#pragma unroll 1
+  for (; kv_tile_idx > swa_end_kv_tile_idx + 1; --kv_tile_idx) {
+    Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0, 1>(TileShape_QKD{}));
+    consumer_wait(pipeline_k, smem_pipe_read_k);
+    WarpScheduler::barrier_sync();
+    gemm</*init=*/true, /*wg_wait=*/-1>(tiled_mma_qk, tSrQ, tSrK(_, _, _, smem_pipe_read_k.index()),
+                                        tSrS);
+    attention_updater.rescale_o(tOrO);
+    consumer_wait(pipeline_v, smem_pipe_read_v);
+    gemm</*init=*/false, /*wg_wait=*/-1>(tiled_mma_pv, tOrP,
+                                         tOrV(_, _, _, smem_pipe_read_v.index()), tOrO);
+    WarpScheduler::barrier_arrive();
+    warpgroup_wait<1>();
+    pipeline_k.consumer_release(smem_pipe_read_k);  // release K
+                                                    // #pragma unroll
+    Tensor cS = cute::make_identity_tensor(select<0, 1>(TileShape_QKD{}));
+    Tensor tScS = threadMmaQK.partition_C(cS);
+#pragma unroll
+    for (int i = 0; i < size(tSrS); ++i) {
+      int qo_idx = get<0>(tScS(i)) + q_tile_idx * CTA_Q;
+      int kv_idx = get<1>(tScS(i)) + (kv_tile_idx - 1) * CTA_KV;
+      tSrS(i) = variant.LogitsTransform(mainloop_params, tSrS(i), /*batch_idx=*/0, qo_idx, kv_idx,
+                                        qo_head_idx, kv_head_idx);
+    }
+    attention_updater.update</*init=*/false>(tSrS);
+    warpgroup_wait<0>();
+    pipeline_v.consumer_release(smem_pipe_read_v);  // release V
+    ++smem_pipe_read_k;
+    ++smem_pipe_read_v;
+    cute::copy(make_tensor(convert_type<DTypeKV>(tSrS).data(),
+                           convert_layout_acc_Aregs<typename Ktraits::TiledMmaPV>(tSrS.layout())),
+               tOrP);
+  }
+
+  if constexpr (LEFT_SLIDING_WINDOW) {
+    constexpr int n_swa_masking_steps = cute::ceil_div(CTA_Q, CTA_KV) + 1;
+#pragma unroll
+    for (int masking_step = 0;
+         masking_step < n_swa_masking_steps && kv_tile_idx > swa_begin_kv_tile_idx;
+         ++masking_step, --kv_tile_idx) {
+      Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0, 1>(TileShape_QKD{}));
+      consumer_wait(pipeline_k, smem_pipe_read_k);
+      WarpScheduler::barrier_sync();
+      gemm</*init=*/true, /*wg_wait=*/-1>(tiled_mma_qk, tSrQ,
+                                          tSrK(_, _, _, smem_pipe_read_k.index()), tSrS);
+      attention_updater.rescale_o(tOrO);
+      consumer_wait(pipeline_v, smem_pipe_read_v);
+      gemm</*init=*/false, /*wg_wait=*/-1>(tiled_mma_pv, tOrP,
+                                           tOrV(_, _, _, smem_pipe_read_v.index()), tOrO);
+      WarpScheduler::barrier_arrive();
+      warpgroup_wait<1>();
+      pipeline_k.consumer_release(smem_pipe_read_k);  // release K
+      Tensor cS = cute::make_identity_tensor(select<0, 1>(TileShape_QKD{}));
+      Tensor tScS = threadMmaQK.partition_C(cS);
+#pragma unroll
+      for (int i = 0; i < size(tSrS); ++i) {
+        int qo_idx = get<0>(tScS(i)) + q_tile_idx * CTA_Q;
+        int kv_idx = get<1>(tScS(i)) + (kv_tile_idx - 1) * CTA_KV;
+        tSrS(i) = variant.LogitsTransform(mainloop_params, tSrS(i), /*batch_idx=*/0, qo_idx, kv_idx,
+                                          qo_head_idx, kv_head_idx);
+        if (kv_idx < col_limit_left(qo_idx)) {
+          tSrS(i) = -math::inf;
+        }
+      }
+      attention_updater.update</*init=*/false>(tSrS);
+      warpgroup_wait<0>();
+      pipeline_v.consumer_release(smem_pipe_read_v);  // release V
+      ++smem_pipe_read_k;
+      ++smem_pipe_read_v;
+      cute::copy(make_tensor(convert_type<DTypeKV>(tSrS).data(),
+                             convert_layout_acc_Aregs<typename Ktraits::TiledMmaPV>(tSrS.layout())),
+                 tOrP);
+    }
+  }
+
+  // Tell warp 0 that smem_q is ready
+  cutlass::arch::NamedBarrier::arrive(NUM_MMA_THREADS + Ktraits::NUM_PRODUCER_THREADS,
+                                      /*id=*/static_cast<int>(NamedBarriers::kQueryEmpty));
+  attention_updater.rescale_o(tOrO);
+  consumer_wait(pipeline_v, smem_pipe_read_v);
+  gemm</*init=*/false, /*wg_wait=*/-1>(tiled_mma_pv, tOrP, tOrV(_, _, _, smem_pipe_read_v.index()),
+                                       tOrO);
+  attention_updater.finalize(tSrS);
+  warpgroup_wait<0>();
+  pipeline_v.consumer_release(smem_pipe_read_v);  // release V, otherwise producers will hang
+  ++smem_pipe_read_v;
+
+  attention_updater.rescale_o(tOrO);
+  return;
+}
+
+}  // namespace flashinfer
+
+#endif  // FLASHINFER_ATTENTION_HOPPER_MAINLOOP_MMA_CUH_

LICENSE
@@ -199,3 +199,25 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+-------------------------------------------------------------------------------------------------
+Some of the code in this project are adapted from other open-source projects with different
+licenses. This product also bundles some third-party components under other open source licenses.
+This section summarizes those components and their licenses.
+See licenses/ for text of these licenses.
+
+BSD 3-Clause License
+--------------------
+
+include/flashinfer/attention/hopper/epilogue.cuh
+include/flashinfer/attention/hopper/mainloop.cuh
+include/flashinfer/attention/hopper/kernel_traits.cuh
+include/flashinfer/attention/hopper/named_barrier.cuh
+include/flashinfer/attention/hopper/tile_scheduler.cuh
+include/flashinfer/attention/hopper/utils.cuh
+
+BSD 3-Clause "New" License
+--------------------------
+
+3rdparty/cutlass
+include/flashinfer/attention/hopper/block_sparse_gather.cuh

aot_build_utils/generate_batch_paged_prefill_sm90_inst.py
@@ -1,0 +1,96 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import re
+import sys
+from pathlib import Path
+
+from .literal_map import (
+    dtype_literal,
+    idtype_literal,
+    mask_mode_literal,
+    pos_encoding_mode_literal,
+)
+
+
+def get_cu_file_str(
+    head_dim,
+    pos_encoding_mode,
+    allow_fp16_qk_reduction,
+    mask_mode,
+    dtype_q,
+    dtype_kv,
+    dtype_out,
+    idtype,
+):
+    def get_insts(attention_variant):
+        return "\n".join(
+            [
+                """template cudaError_t BatchPrefillWithPagedKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/true, {attention_variant}>(
+    Params& params,
+    cudaStream_t stream);
+
+template cudaError_t BatchPrefillWithPagedKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/false, {attention_variant}>(
+    Params& params,
+    cudaStream_t stream);
+    """.format(
+                    head_dim=head_dim,
+                    pos_encoding_mode=pos_encoding_mode_literal[int(pos_encoding_mode)],
+                    allow_fp16_qk_reduction=allow_fp16_qk_reduction,
+                    mask_mode=mask_mode_literal[int(mask_mode)],
+                    attention_variant=attention_variant,
+                )
+            ]
+        )
+
+    dtype_q = dtype_literal[dtype_q]
+    dtype_kv = dtype_literal[dtype_kv]
+    dtype_out = dtype_literal[dtype_out]
+    idtype = idtype_literal[idtype]
+
+    content = f"""#include <flashinfer/attention/hopper/prefill_sm90.cuh>
+#include <flashinfer/attention/hopper/variants.cuh>
+#include <flashinfer/cutlass_utils.cuh>
+
+
+namespace flashinfer {{
+
+using DTypeQ = cutlass_dtype_t<{dtype_q}>;
+using DTypeKV = cutlass_dtype_t<{dtype_kv}>;
+using DTypeO = cutlass_dtype_t<{dtype_out}>;
+
+using Params = BatchPrefillPagedParams<DTypeQ, DTypeKV, DTypeO, {idtype}>;
+
+{get_insts("LogitsSoftCap")}
+
+{get_insts("StandardAttention")}
+
+}}"""
+    return content
+
+
+if __name__ == "__main__":
+    pattern = (
+        r"batch_paged_prefill_head_([0-9]+)_posenc_([0-9]+)_"
+        r"fp16qkred_([a-z]+)_mask_([0-9]+)_dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)_idtype_([a-z0-9]+)_sm90\.cu"
+    )
+    compiled_pattern = re.compile(pattern)
+    path = Path(sys.argv[1])
+    fname = path.name
+    match = compiled_pattern.match(fname)
+
+    with open(path, "w") as f:
+        f.write(get_cu_file_str(*match.groups()))

aot_build_utils/generate_batch_ragged_prefill_sm90_inst.py
@@ -1,0 +1,97 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import re
+import sys
+from pathlib import Path
+
+from .literal_map import (
+    dtype_literal,
+    idtype_literal,
+    mask_mode_literal,
+    pos_encoding_mode_literal,
+)
+
+
+def get_cu_file_str(
+    head_dim,
+    pos_encoding_mode,
+    allow_fp16_qk_reduction,
+    mask_mode,
+    dtype_q,
+    dtype_kv,
+    dtype_out,
+    idtype,
+):
+
+    def get_insts(attention_variant):
+        return "\n".join(
+            [
+                """template cudaError_t BatchPrefillWithRaggedKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/true, {attention_variant}>(
+    Params& params,
+    cudaStream_t stream);
+
+template cudaError_t BatchPrefillWithRaggedKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/false, {attention_variant}>(
+    Params& params,
+    cudaStream_t stream);
+        """.format(
+                    head_dim=head_dim,
+                    pos_encoding_mode=pos_encoding_mode_literal[int(pos_encoding_mode)],
+                    allow_fp16_qk_reduction=allow_fp16_qk_reduction,
+                    mask_mode=mask_mode_literal[int(mask_mode)],
+                    attention_variant=attention_variant,
+                )
+            ]
+        )
+
+    dtype_q = dtype_literal[dtype_q]
+    dtype_kv = dtype_literal[dtype_kv]
+    dtype_out = dtype_literal[dtype_out]
+    idtype = idtype_literal[idtype]
+
+    content = f"""#include <flashinfer/attention/hopper/prefill_sm90.cuh>
+#include <flashinfer/attention/hopper/variants.cuh>
+#include <flashinfer/cutlass_utils.cuh>
+
+
+namespace flashinfer {{
+
+using DTypeQ = cutlass_dtype_t<{dtype_q}>;
+using DTypeKV = cutlass_dtype_t<{dtype_kv}>;
+using DTypeO = cutlass_dtype_t<{dtype_out}>;
+
+using Params = BatchPrefillRaggedParams<DTypeQ, DTypeKV, DTypeO, {idtype}>;
+
+{get_insts("LogitsSoftCap")}
+
+{get_insts("StandardAttention")}
+
+}}
+    """
+    return content
+
+
+if __name__ == "__main__":
+    pattern = (
+        r"batch_ragged_prefill_head_([0-9]+)_posenc_([0-9]+)_"
+        r"fp16qkred_([a-z]+)_mask_([0-9]+)_dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)_idtype_([a-z0-9]+)_sm90\.cu"
+    )
+    compiled_pattern = re.compile(pattern)
+    path = Path(sys.argv[1])
+    fname = path.name
+    match = compiled_pattern.match(fname)
+    with open(path, "w") as f:
+        f.write(get_cu_file_str(*match.groups()))

aot_build_utils/generate_single_prefill_sm90_inst.py
@@ -1,0 +1,85 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import re
+import sys
+from pathlib import Path
+
+from .literal_map import dtype_literal, mask_mode_literal, pos_encoding_mode_literal
+
+
+def get_cu_file_str(
+    head_dim,
+    pos_encoding_mode,
+    allow_fp16_qk_reduction,
+    mask_mode,
+    dtype_q,
+    dtype_kv,
+    dtype_out,
+):
+    content = """#include <flashinfer/attention/hopper/prefill_sm90.cuh>
+#include <flashinfer/attention/hopper/variants.cuh>
+#include <flashinfer/cutlass_utils.cuh>
+
+namespace flashinfer {{
+
+using DTypeQ = cutlass_dtype_t<{dtype_q}>;
+using DTypeKV = cutlass_dtype_t<{dtype_kv}>;
+using DTypeO = cutlass_dtype_t<{dtype_out}>;
+
+using Params = SinglePrefillParams<DTypeQ, DTypeKV, DTypeO>;
+
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/true, LogitsSoftCap>(
+    Params& params,
+    cudaStream_t stream);
+
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/false, LogitsSoftCap>(
+    Params& params,
+    cudaStream_t stream);
+
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/true, StandardAttention>(
+    Params& params,
+    cudaStream_t stream);
+
+template cudaError_t SinglePrefillWithKVCacheDispatched<{head_dim}, {mask_mode}, /*USE_SWA=*/false, StandardAttention>(
+    Params& params,
+    cudaStream_t stream);
+}}
+    """.format(
+        head_dim=head_dim,
+        pos_encoding_mode=pos_encoding_mode_literal[int(pos_encoding_mode)],
+        allow_fp16_qk_reduction=allow_fp16_qk_reduction,
+        mask_mode=mask_mode_literal[int(mask_mode)],
+        dtype_q=dtype_literal[dtype_q],
+        dtype_kv=dtype_literal[dtype_kv],
+        dtype_out=dtype_literal[dtype_out],
+        use_custom_mask="true" if int(mask_mode) == 2 else "false",
+    )
+    return content
+
+
+if __name__ == "__main__":
+    pattern = (
+        r"single_prefill_head_([0-9]+)_posenc_([0-9]+)_"
+        r"fp16qkred_([a-z]+)_mask_([0-9]+)_dtypeq_([a-z0-9]+)_dtypekv_([a-z0-9]+)_dtypeout_([a-z0-9]+)_sm90\.cu"
+    )
+
+    compiled_pattern = re.compile(pattern)
+    path = Path(sys.argv[1])
+    fname = path.name
+    match = compiled_pattern.match(fname)
+    with open(path, "w") as f:
+        f.write(get_cu_file_str(*match.groups()))

commit 124daea86fcdff4ba64e5b51337d81a46d6068cb

bugfix: Fix SWA Implementation Hanging Issue (#673)
This PR resolves an issue in the SWA implementation introduced in #667,
where the process would hang under specific conditions.

include/flashinfer/attention/hopper/mainloop_mma.cuh
@@ -204,11 +204,8 @@ CUTLASS_DEVICE void mma_f16(const Params& mainloop_params, AttentionVariant& var
   }
 
   if constexpr (LEFT_SLIDING_WINDOW) {
-    constexpr int n_swa_masking_steps = cute::ceil_div(CTA_Q, CTA_KV) + 1;
-#pragma unroll
-    for (int masking_step = 0;
-         masking_step < n_swa_masking_steps && kv_tile_idx > swa_begin_kv_tile_idx;
-         ++masking_step, --kv_tile_idx) {
+#pragma unroll 1
+    for (; kv_tile_idx > swa_begin_kv_tile_idx; --kv_tile_idx) {
       Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0, 1>(TileShape_QKD{}));
       consumer_wait(pipeline_k, smem_pipe_read_k);
       WarpScheduler::barrier_sync();