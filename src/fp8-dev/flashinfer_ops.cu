01f1b0a644c6dc18858cb0da659422e96d1675cc
happierpig <zhaoyilong217@sjtu.edu.cn>
2025-02-17 22:49:55 +0000

    [minor] add perf & correctness tests.

---
+++ src/fp8-dev/flashinfer_ops.cu
@@ -0,0 +1,157 @@
+#include <thrust/device_vector.h>
+
+#include <flashinfer/attention/hopper/default_params.cuh>
+#include <flashinfer/attention/hopper/prefill_sm90.cuh>
+#include <flashinfer/attention/hopper/quantization/prefill_sm90.cuh>
+#include <flashinfer/attention/hopper/variants.cuh>
+#include <flashinfer/attention/mask.cuh>
+#include <flashinfer/layout.cuh>
+#include <flashinfer/math.cuh>
+#include <flashinfer/utils.cuh>
+
+using namespace flashinfer;
+
+template <typename DTypeQ, typename DTypeKV, typename DTypeO>
+void run_fwd_flashinfer(thrust::device_vector<DTypeQ>& q_d, thrust::device_vector<DTypeKV>& k_d,
+                        thrust::device_vector<DTypeKV>& v_d, thrust::device_vector<DTypeO>& o_d,
+                        thrust::device_vector<float>& scale_q_d,
+                        thrust::device_vector<float>& scale_k_d,
+                        thrust::device_vector<float>& scale_v_d, int32_t qo_len, int32_t kv_len,
+                        int32_t num_qo_heads, int32_t num_kv_heads, int32_t head_dim,
+                        float sm_scale, MaskMode mask_mode, QKVLayout kv_layout) {
+  using IdType = int32_t;
+  using DTypeScale = float;
+
+  constexpr bool USE_SLIDING_WINDOW = false;
+  using Params = SinglePrefillParams<DTypeQ, DTypeKV, DTypeO, IdType>;
+  using AttentionVariant = DefaultFP8Attention;
+
+  Params params;
+  params.q_ptr = static_cast<DTypeQ*>(thrust::raw_pointer_cast(q_d.data()));
+  params.k_ptr = static_cast<DTypeKV*>(thrust::raw_pointer_cast(k_d.data()));
+  params.v_ptr = static_cast<DTypeKV*>(thrust::raw_pointer_cast(v_d.data()));
+  params.o_ptr = static_cast<DTypeO*>(thrust::raw_pointer_cast(o_d.data()));
+  params.lse_ptr = nullptr;
+  // q NHD
+  params.q_stride_n = num_qo_heads * head_dim;
+  params.q_stride_h = head_dim;
+  params.o_stride_n = num_qo_heads * head_dim;
+  params.o_stride_h = head_dim;
+  if (kv_layout == QKVLayout::kNHD) {
+    params.k_stride_n = num_kv_heads * head_dim;
+    params.k_stride_h = head_dim;
+    params.v_stride_n = num_kv_heads * head_dim;
+    params.v_stride_h = head_dim;
+  } else {
+    // k HND
+    params.k_stride_h = kv_len * head_dim;
+    params.k_stride_n = head_dim;
+    params.v_stride_h = kv_len * head_dim;
+    params.v_stride_n = head_dim;
+  }
+  params.qo_len = qo_len;
+  params.kv_len = kv_len;
+  params.head_dim = head_dim;
+  params.num_qo_heads = num_qo_heads;
+  params.num_kv_heads = num_kv_heads;
+  params.causal = (mask_mode == MaskMode::kCausal);
+  params.group_size = params.num_qo_heads / params.num_kv_heads;
+  params.window_left = 0;
+
+  params.additional_params.scale_q = thrust::raw_pointer_cast(scale_q_d.data());
+  params.additional_params.scale_k = thrust::raw_pointer_cast(scale_k_d.data());
+  params.additional_params.scale_v = thrust::raw_pointer_cast(scale_v_d.data());
+  params.additional_params.sm_scale = sm_scale;
+
+  cudaError_t status;
+  cudaStream_t stream = 0;
+  DISPATCH_HEAD_DIM(head_dim, HEAD_DIM, {
+    DISPATCH_MASK_MODE(mask_mode, MASK_MODE, {
+      status = SingleFP8PrefillWithKVCacheDispatched<HEAD_DIM, MASK_MODE, USE_SLIDING_WINDOW,
+                                                     AttentionVariant, Params>(params, stream);
+    });
+  });
+  if (status != cudaSuccess) {
+    throw std::runtime_error("Failed to run SingleFP8PrefillWithKVCacheDispatched");
+  }
+}
+
+void run_fwd(thrust::device_vector<cutlass::float_e4m3_t>& q_d,
+             thrust::device_vector<cutlass::float_e4m3_t>& k_d,
+             thrust::device_vector<cutlass::float_e4m3_t>& v_d,
+             thrust::device_vector<cutlass::half_t>& o_d, thrust::device_vector<float>& scale_q_d,
+             thrust::device_vector<float>& scale_k_d, thrust::device_vector<float>& scale_v_d,
+             int32_t qo_len, int32_t kv_len, int32_t num_qo_heads, int32_t num_kv_heads,
+             int32_t head_dim, float sm_scale, MaskMode mask_mode, QKVLayout kv_layout) {
+  run_fwd_flashinfer<cutlass::float_e4m3_t, cutlass::float_e4m3_t, cutlass::half_t>(
+      q_d, k_d, v_d, o_d, scale_q_d, scale_k_d, scale_v_d, qo_len, kv_len, num_qo_heads,
+      num_kv_heads, head_dim, sm_scale, mask_mode, kv_layout);
+}
+
+template <typename DTypeQ, typename DTypeKV, typename DTypeO>
+void run_fwd_flashinfer(thrust::device_vector<DTypeQ>& q_d, thrust::device_vector<DTypeKV>& k_d,
+                        thrust::device_vector<DTypeKV>& v_d, thrust::device_vector<DTypeO>& o_d,
+                        int32_t qo_len, int32_t kv_len, int32_t num_qo_heads, int32_t num_kv_heads,
+                        int32_t head_dim, float sm_scale, MaskMode mask_mode, QKVLayout kv_layout) {
+  using IdType = int32_t;
+
+  constexpr bool USE_SLIDING_WINDOW = false;
+  using Params = SinglePrefillParams<DTypeQ, DTypeKV, DTypeO, IdType>;
+  using AttentionVariant = StandardAttention;
+
+  Params params;
+  params.q_ptr = static_cast<DTypeQ*>(thrust::raw_pointer_cast(q_d.data()));
+  params.k_ptr = static_cast<DTypeKV*>(thrust::raw_pointer_cast(k_d.data()));
+  params.v_ptr = static_cast<DTypeKV*>(thrust::raw_pointer_cast(v_d.data()));
+  params.o_ptr = static_cast<DTypeO*>(thrust::raw_pointer_cast(o_d.data()));
+  params.lse_ptr = nullptr;
+  // q NHD
+  params.q_stride_n = num_qo_heads * head_dim;
+  params.q_stride_h = head_dim;
+  params.o_stride_n = num_qo_heads * head_dim;
+  params.o_stride_h = head_dim;
+  if (kv_layout == QKVLayout::kNHD) {
+    params.k_stride_n = num_kv_heads * head_dim;
+    params.k_stride_h = head_dim;
+    params.v_stride_n = num_kv_heads * head_dim;
+    params.v_stride_h = head_dim;
+  } else {
+    // k HND
+    params.k_stride_h = kv_len * head_dim;
+    params.k_stride_n = head_dim;
+    params.v_stride_h = kv_len * head_dim;
+    params.v_stride_n = head_dim;
+  }
+  params.qo_len = qo_len;
+  params.kv_len = kv_len;
+  params.head_dim = head_dim;
+  params.num_qo_heads = num_qo_heads;
+  params.num_kv_heads = num_kv_heads;
+  params.causal = (mask_mode == MaskMode::kCausal);
+  params.group_size = params.num_qo_heads / params.num_kv_heads;
+  params.window_left = 0;
+  params.additional_params.sm_scale = sm_scale;
+
+  cudaError_t status;
+  cudaStream_t stream = 0;
+  DISPATCH_HEAD_DIM(head_dim, HEAD_DIM, {
+    DISPATCH_MASK_MODE(mask_mode, MASK_MODE, {
+      status = SinglePrefillWithKVCacheDispatched<HEAD_DIM, MASK_MODE, USE_SLIDING_WINDOW,
+                                                  AttentionVariant, Params>(params, stream);
+    });
+  });
+  if (status != cudaSuccess) {
+    throw std::runtime_error("Failed to run SingleFP16PrefillWithKVCacheDispatched");
+  }
+}
+
+void run_fwd(thrust::device_vector<cutlass::half_t>& q_d,
+             thrust::device_vector<cutlass::half_t>& k_d,
+             thrust::device_vector<cutlass::half_t>& v_d,
+             thrust::device_vector<cutlass::half_t>& o_d, int32_t qo_len, int32_t kv_len,
+             int32_t num_qo_heads, int32_t num_kv_heads, int32_t head_dim, float sm_scale,
+             MaskMode mask_mode, QKVLayout kv_layout) {
+  run_fwd_flashinfer<cutlass::half_t, cutlass::half_t, cutlass::half_t>(
+      q_d, k_d, v_d, o_d, qo_len, kv_len, num_qo_heads, num_kv_heads, head_dim, sm_scale, mask_mode,
+      kv_layout);
+}
---
+++ src/fp8-dev/utils.h
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2023 by FlashInfer team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cuda_fp8.h>
+#include <cuda_runtime.h>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/host_vector.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/random.h>
+#include <thrust/transform.h>
+
+#include <random>
+
+namespace utils {
+
+template <typename DTypeO>
+float vec_cal_mse_(std::vector<DTypeO>& x, std::vector<DTypeO>& y) {
+  float mse = 0.0;
+  for (size_t i = 0; i < x.size(); ++i) {
+    float diff = float(x[i]) - float(y[i]);
+    mse += diff * diff;
+  }
+  return mse / x.size();
+}
+
+template <typename T>
+void vec_normal_(std::vector<T>& vec, float mean = 0.f, float std = 1.f, int seed = 327) {
+  std::mt19937 gen{seed};
+  // std::random_device rd{};
+  // std::mt19937 gen{rd()};
+
+  std::normal_distribution d{mean, std};
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec[i] = T(cutlass::float_e4m3_t(d(gen)));
+  }
+
+  if constexpr (sizeof(T) == 1) {
+    // view vec as uint8_t
+    uint8_t* data = reinterpret_cast<uint8_t*>(vec.data());
+    // random shuffle
+    std::shuffle(data, data + vec.size(), gen);
+  } else {
+    // random shuffle
+    std::shuffle(vec.begin(), vec.end(), gen);
+  }
+}
+
+template <typename T>
+void vec_uniform_(std::vector<T>& vec, float a = 0.f, float b = 1.f) {
+  std::random_device rd{};
+  std::mt19937 gen{rd()};
+  std::uniform_real_distribution d{a, b};
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec[i] = T(d(gen));
+  }
+}
+
+template <typename T>
+void vec_zero_(std::vector<T>& vec) {
+  std::fill(vec.begin(), vec.end(), T(0));
+}
+
+template <typename T>
+void vec_fill_(std::vector<T>& vec, T val) {
+  std::fill(vec.begin(), vec.end(), T(val));
+}
+
+template <typename T>
+void vec_randint_(std::vector<T>& vec, int low, int high) {
+  std::random_device rd{};
+  std::mt19937 gen{rd()};
+  std::uniform_int_distribution d{low, high};
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec[i] = T(d(gen));
+  }
+}
+
+template <typename T>
+void vec_randbin_(std::vector<T>& vec) {
+  std::random_device rd{};
+  std::mt19937 gen{rd()};
+  std::uniform_int_distribution d{0, 1024};
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec[i] = T(d(gen) % 2 ? -2 : 2);
+  }
+}
+
+template <typename T>
+size_t vec_bytes(const T& vec) {
+  return vec.size() * sizeof(typename T::value_type);
+}
+
+template <typename T>
+bool isclose(T a, T b, float rtol = 1e-5, float atol = 1e-8) {
+  return fabs(a - b) <= (atol + rtol * fabs(b));
+}
+
+template <typename T>
+std::tuple<std::vector<std::vector<T>>, std::vector<std::vector<int32_t>>>
+create_shared_prefix_testcase_data(size_t batch_size, size_t shared_prefix_length,
+                                   size_t unique_kv_length, size_t qo_append_length,
+                                   size_t num_qo_heads, size_t num_kv_heads, size_t head_dim,
+                                   size_t page_size) {
+  uint32_t num_pages = ((shared_prefix_length + unique_kv_length * batch_size) / page_size);
+  std::vector<T> shared_k_h(shared_prefix_length * num_kv_heads * head_dim);
+  std::vector<T> shared_v_h(shared_prefix_length * num_kv_heads * head_dim);
+  std::vector<T> q_h((batch_size * qo_append_length) * num_qo_heads * head_dim);
+
+  utils::vec_normal_(shared_k_h);
+  utils::vec_normal_(shared_v_h);
+  utils::vec_normal_(q_h);
+
+  std::vector<int32_t> qo_indptr{0};
+  std::vector<int32_t> kv_indptr_combined_h{0};
+  std::vector<int32_t> kv_indptr_unique_h{0};
+  std::vector<int32_t> kv_last_page_len_combined_h;
+  std::vector<int32_t> kv_last_page_len_unique_h;
+
+  for (uint32_t request_id = 0; request_id < batch_size; ++request_id) {
+    qo_indptr.push_back(qo_indptr.back() + qo_append_length);
+    kv_indptr_combined_h.push_back(kv_indptr_combined_h.back() +
+                                   (shared_prefix_length + unique_kv_length) / page_size);
+    kv_indptr_unique_h.push_back(kv_indptr_unique_h.back() + unique_kv_length / page_size);
+    kv_last_page_len_combined_h.push_back(page_size);
+    kv_last_page_len_unique_h.push_back(page_size);
+  }
+
+  std::vector<int32_t> kv_indices_combined_h(kv_indptr_combined_h.back());
+  std::vector<int32_t> kv_indices_unique_h(kv_indptr_unique_h.back());
+
+  std::vector<T> kv_data_h(num_pages * 2 * num_kv_heads * page_size * head_dim);
+  uint32_t page_id = 0;
+
+  for (; page_id < (shared_prefix_length / page_size); page_id++) {
+    for (uint32_t entry_idx = 0; entry_idx < page_size; entry_idx++) {
+      for (uint32_t head_idx = 0; head_idx < num_kv_heads; head_idx++) {
+        std::copy(
+            shared_k_h.begin() +
+                ((page_id * page_size + entry_idx) * num_kv_heads + head_idx) * head_dim,
+            shared_k_h.begin() +
+                ((page_id * page_size + entry_idx) * num_kv_heads + head_idx + 1) * head_dim,
+            kv_data_h.begin() +
+                (((page_id * 2 + 0) * num_kv_heads + head_idx) * page_size + entry_idx) * head_dim);
+        std::copy(
+            shared_v_h.begin() +
+                ((page_id * page_size + entry_idx) * num_kv_heads + head_idx) * head_dim,
+            shared_v_h.begin() +
+                ((page_id * page_size + entry_idx) * num_kv_heads + head_idx + 1) * head_dim,
+            kv_data_h.begin() +
+                (((page_id * 2 + 1) * num_kv_heads + head_idx) * page_size + entry_idx) * head_dim);
+      }
+    }
+    for (uint32_t request_id = 0; request_id < batch_size; ++request_id) {
+      kv_indices_combined_h[request_id * ((shared_prefix_length + unique_kv_length) / page_size) +
+                            page_id] = page_id;
+    }
+  }
+
+  for (uint32_t request_id = 0; request_id < batch_size; ++request_id) {
+    for (uint32_t page_iter = 0; page_iter < (unique_kv_length / page_size);
+         ++page_iter, ++page_id) {
+      for (uint32_t entry_idx = 0; entry_idx < page_size; entry_idx++) {
+        for (uint32_t head_idx = 0; head_idx < num_kv_heads; head_idx++) {
+          std::vector<T> k(head_dim), v(head_dim);
+          utils::vec_normal_(k);
+          utils::vec_normal_(v);
+          std::copy(k.begin(), k.end(),
+                    kv_data_h.begin() +
+                        (((page_id * 2 + 0) * num_kv_heads + head_idx) * page_size + entry_idx) *
+                            head_dim);
+          std::copy(v.begin(), v.end(),
+                    kv_data_h.begin() +
+                        (((page_id * 2 + 1) * num_kv_heads + head_idx) * page_size + entry_idx) *
+                            head_dim);
+        }
+      }
+      kv_indices_combined_h[request_id * ((shared_prefix_length + unique_kv_length) / page_size) +
+                            (shared_prefix_length / page_size) + page_iter] = page_id;
+      kv_indices_unique_h[request_id * (unique_kv_length / page_size) + page_iter] = page_id;
+    }
+  }
+  return std::make_tuple<std::vector<std::vector<T>>, std::vector<std::vector<int32_t>>>(
+      {std::move(q_h), std::move(shared_k_h), std::move(shared_v_h), std::move(kv_data_h)},
+      {std::move(qo_indptr), std::move(kv_indices_combined_h), std::move(kv_indices_unique_h),
+       std::move(kv_indptr_combined_h), std::move(kv_indptr_unique_h),
+       std::move(kv_last_page_len_combined_h), std::move(kv_last_page_len_unique_h)});
+}
+
+}  // namespace utils
