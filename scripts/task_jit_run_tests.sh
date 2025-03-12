16e2a93f7a98c0c4363ff98bcd85ad3305677bed
Zihao Ye <expye@outlook.com>
2025-03-12 15:12:33 +0000

    upd

--- scripts/task_jit_run_tests.sh
+++ scripts/task_jit_run_tests.sh
@@ -3,7 +3,11 @@
 set -eo pipefail
 set -x
 : ${NUM_THREADS:=$(nproc)}
+: ${CUDA_VISIBLE_DEVICES:=0}
 
 pip install -e . -v
 
-pytest -v tests/test_norm.py
+pytest -s tests/test_norm.py
+pytest -s tests/test_sampling.py
+pytest -s tests/test_jit_example.py
+pytest -s tests/test_jit_warmup.py
