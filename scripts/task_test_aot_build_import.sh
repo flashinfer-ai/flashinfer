53f66dbe311f8466142e9c1b29ddf99ab14ae6b5
Zihao Ye <expye@outlook.com>
2025-03-14 05:17:01 +0000

    upd

--- scripts/task_test_aot_build_import.sh
+++ scripts/task_test_aot_build_import.sh
@@ -4,8 +4,10 @@ set -eo pipefail
 set -x
 : ${MAX_JOBS:=$(nproc)}
 : ${CUDA_VISIBLE_DEVICES:=""}
+export TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0+PTX"
+export FLASHINFER_ENABLE_AOT=1
 
-FLASHINFER_ENABLE_AOT=1 python -m build --wheel
+python -m build --wheel
 pip install dist/*.whl
 
 # test import
