141a99fdcec3a64e74d034291aae6b076d9264a2
Zihao Ye <expye@outlook.com>
2025-03-17 15:11:29 +0000

    upd

--- scripts/task_jit_run_tests_part4.sh
+++ scripts/task_jit_run_tests_part4.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 set -x
 : ${MAX_JOBS:=$(nproc)}
 : ${CUDA_VISIBLE_DEVICES:=0}
+: ${PYTORCH_CUDA_ALLOC_CONF:=expandable_segments:True}  # avoid memory fragmentation
 
 pip install -e . -v
 
