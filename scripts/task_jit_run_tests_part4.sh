30b2838e625854ad7f394f6a4bf2aaf18af94a3c
Zihao Ye <expye@outlook.com>
2025-03-17 12:28:40 -0400

    bugfix: bugfix to #949 (#951)
    The sm86/sm89 version of mla kernel was not tests after change #942,
    this PR fixes the issue.
    
    This PR also make the following changes:
    1. adding the mla unittest to CI (on a10g node). 
    2. shrinking the unittest of mla so that CI can finish in reasonable
    time.
    3. change `is_sm90a_supported(torch.device("cuda"))` to `backend ==
    "fa3" and not is_sm90a_supported(torch.device("cuda")):` for non-hopper
    GPUs, as pointed by @Atream .

---
+++ scripts/task_jit_run_tests_part4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+set -x
+: ${MAX_JOBS:=$(nproc)}
+: ${CUDA_VISIBLE_DEVICES:=0}
+
+pip install -e . -v
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True  # avoid memory fragmentation
+pytest -s tests/test_deepseek_mla.py
