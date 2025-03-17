ba0ee1df96c0fd4a406f1bf429f86386dda0159c
Zihao Ye <expye@outlook.com>
2025-03-17 16:10:25 +0000

    upd

--- scripts/task_jit_run_tests_part4.sh
+++ scripts/task_jit_run_tests_part4.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 set -x
 : ${MAX_JOBS:=$(nproc)}
 : ${CUDA_VISIBLE_DEVICES:=0}
-: ${PYTORCH_CUDA_ALLOC_CONF:=expandable_segments:True}  # avoid memory fragmentation
 
 pip install -e . -v
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True  # avoid memory fragmentation
 pytest -s tests/test_deepseek_mla.py
