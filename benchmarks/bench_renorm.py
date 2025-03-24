7bf640f2d292d6132e5ab360e3a31364c5a777bd
xsling <me@xsl.sh>
2025-03-24 11:14:33 +0000

    add benchmark

---
+++ benchmarks/bench_renorm.py
@@ -0,0 +1,119 @@
+import torch
+from triton.testing import do_bench
+
+import flashinfer
+
+
+def normal_distribution(std):
+    def normal_noise(shape, device):
+        return torch.randn(shape, device=device) * std
+
+    normal_noise.__name__ = f"normal_distribution(std={std})"
+    return normal_noise
+
+
+def gumbel_distribution(beta):
+    def gumbel_noise(shape, device):
+        U = torch.rand(shape, device=device)
+        eps = 1e-20
+        return torch.log(-torch.log(U + eps) + eps) / beta
+
+    gumbel_noise.__name__ = f"gumbel_distribution(beta={beta})"
+    return gumbel_noise
+
+
+def init_seed_top_p_renorm(*args, **kwargs):
+    torch.manual_seed(42)
+    return flashinfer.sampling.top_p_renorm_probs(*args, **kwargs)
+
+
+def init_seed_top_k_renorm(*args, **kwargs):
+    torch.manual_seed(42)
+    return flashinfer.sampling.top_k_renorm_probs(*args, **kwargs)
+
+
+def init_seed_top_k_mask_logits(*args, **kwargs):
+    torch.manual_seed(42)
+    return flashinfer.sampling.top_k_mask_logits(*args, **kwargs)
+
+
+@torch.inference_mode()
+def main():
+    print("---")
+    print("top-p renorm")
+    for vocab_size in [128512]:
+        for batch_size in [1, 16, 32, 64, 128, 256, 512]:
+            for distrib in [
+                normal_distribution(1),
+                normal_distribution(5),
+                gumbel_distribution(0.1),
+                gumbel_distribution(1),
+            ]:
+                for p in [0.1, 0.5, 0.9]:
+                    logits = distrib((batch_size, vocab_size), device="cuda")
+                    probs = torch.softmax(logits, dim=-1)
+                    ms = do_bench(
+                        lambda: init_seed_top_p_renorm(probs, p),
+                        warmup=100,
+                        rep=1000,
+                    )
+
+                    io = (probs.numel() * probs.element_size()) * 2
+                    bandwidth = io * 1e-6 / ms
+                    print(
+                        f"vocab_size: {vocab_size}, batch_size: {batch_size}, distrib: {distrib.__name__}, p: {p}, duration: {ms*1e3:.2f} us, effective bandwidth: {bandwidth:.2f} GB/s"
+                    )
+
+    print("---")
+    print("top-k renorm")
+    for vocab_size in [128512]:
+        for batch_size in [1, 16, 32, 64, 128, 256, 512]:
+            for distrib in [
+                normal_distribution(1),
+                normal_distribution(5),
+                gumbel_distribution(0.1),
+                gumbel_distribution(1),
+            ]:
+                for k in [10, 100, 1000, 5000]:
+                    logits = distrib((batch_size, vocab_size), device="cuda")
+                    probs = torch.softmax(logits, dim=-1)
+                    ms = do_bench(
+                        lambda: init_seed_top_k_renorm(probs, k),
+                        warmup=100,
+                        rep=1000,
+                    )
+
+                    io = (probs.numel() * probs.element_size()) * 2
+                    bandwidth = io * 1e-6 / ms
+                    print(
+                        f"vocab_size: {vocab_size}, batch_size: {batch_size}, distrib: {distrib.__name__}, k: {k}, duration: {ms*1e3:.2f} us, effective bandwidth: {bandwidth:.2f} GB/s"
+                    )
+
+    print("---")
+    print("top-k mask logits")
+
+    for vocab_size in [128512]:
+        for batch_size in [1, 16, 32, 64, 128, 256, 512]:
+            for distrib in [
+                normal_distribution(1),
+                normal_distribution(5),
+                gumbel_distribution(0.1),
+                gumbel_distribution(1),
+            ]:
+                for k in [10, 100, 1000, 5000]:
+                    logits = distrib((batch_size, vocab_size), device="cuda")
+                    ms = do_bench(
+                        lambda: init_seed_top_k_mask_logits(logits, k),
+                        warmup=100,
+                        rep=1000,
+                    )
+
+                    io = (logits.numel() * logits.element_size()) * 2
+                    bandwidth = io * 1e-6 / ms
+                    print(
+                        f"vocab_size: {vocab_size}, batch_size: {batch_size}, distrib: {distrib.__name__}, k: {k}, duration: {ms*1e3:.2f} us, effective bandwidth: {bandwidth:.2f} GB/s"
+                    )
+
+
+if __name__ == "__main__":
+    main()
