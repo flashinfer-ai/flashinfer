1b7276f9d22c38879dbfb6d01ae9623f9a45a55a
yingyih <yingyihuang2000@outlook.com>
2025-03-29 17:39:14 +0000

    upd (todo: bf16, fp32, bench)

--- flashinfer/triton/__init__.py
+++ flashinfer/triton/__init__.py
@@ -1,1 +1,2 @@
 from . import cascade  # noqa: F401
+from . import sm_constraint_gemm  # Add this line
--- flashinfer/triton/sm_constraint_gemm.py
+++ flashinfer/triton/sm_constraint_gemm.py
@@ -10,18 +10,32 @@ from .utils import check_device, check_dim, check_input, check_shape
 
 
 def matmul_persistent(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
+    """
+    GEMM operation with SM constraint by Triton.
+    C = alpha * (a @ b.T) + beta * C
+
+    Args:
+        a: The first input matrix. Shape: (M, K)
+        b: The second input matrix. Shape: (K, N)
+        c: The output matrix. Shape: (M, N). In-place operation is supported.
+        alpha: The scaling factor for the product of a and b.
+        beta: The scaling factor for the output matrix c.
+        num_sms: The number of SMs to use for the computation.
+    """
+
     # Check constraints.
     check_input(a)
-    check_input(b)
+    # check_input(b) # b can be non-contiguous
     check_input(c)
     check_device([a, b, c])
     check_dim(2, a)
     check_dim(2, b)
     check_dim(2, c)
 
-    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
-    assert a.dtype == b.dtype, "Incompatible dtypes"
-    assert a.shape[1] == c.shape[1], "Incompatible dimensions"
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions between a and b"
+    assert a.dtype == b.dtype, "Incompatible dtypes between a and b"
+    assert a.shape[0] == c.shape[0], "Incompatible dimensions between a and c"
+    assert b.shape[1] == c.shape[1], "Incompatible dimensions between b and c"
 
     M, K = a.shape
     K, N = b.shape
