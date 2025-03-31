4d2cee0d7a7c5ad8bc18df8c79acf3266ac80660
yingyih <yingyihuang2000@outlook.com>
2025-03-31 18:54:17 +0000

    add param: out_dtype

--- flashinfer/triton/sm_constraint_gemm.py
+++ flashinfer/triton/sm_constraint_gemm.py
@@ -11,17 +11,18 @@ from .kernels.sm_constraint_gemm import (
 from .utils import check_device, check_dim, check_input
 
 
-def gemm_persistent(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
+def gemm_persistent(a, b, c=None, alpha=1.0, beta=0.0, out_dtype=None, num_sms=None):
     """
-    GEMM operation with SM constraint by Triton (Hopper).
+    GEMM operation with SM constraint by Triton.
     C = alpha * (a @ b.T) + beta * C
 
     Args:
         a: The first input matrix. Shape: (M, K)
         b: The second input matrix. Shape: (K, N)
-        c: The output matrix. Shape: (M, N). In-place operation is supported.
+        c: The output matrix. Shape: (M, N). In-place epilogue is supported. Expected to be out_dtype (if not specified, same as a.dtype, but fp8 --> bf16).
         alpha: The scaling factor for the product of a and b.
         beta: The scaling factor for the output matrix c.
+        out_dtype: The dtype of the output matrix. Default: fp8 --> bf16. Otherwise, same as a.dtype.
         num_sms: The number of SMs to use for the computation.
     """
 
@@ -42,9 +43,18 @@ def gemm_persistent(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
     M, K = a.shape
     K, N = b.shape
     dtype = a.dtype
+    out_dtype = (
+        out_dtype
+        if out_dtype
+        else dtype if dtype != torch.float8_e4m3fn else torch.bfloat16
+    )
+
+    assert (
+        c is None or c.dtype == out_dtype
+    ), "Incompatible dtypes between c and out_dtype"
 
     # Allocates output.
-    c = torch.empty((M, N), device=a.device, dtype=dtype) if c is None else c
+    c = torch.empty((M, N), device=a.device, dtype=out_dtype) if c is None else c
 
     # Set num_sms to be 100% of the available SMs
     NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
@@ -78,7 +88,7 @@ def gemm_persistent(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
     return c
 
 
-def gemm(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
+def gemm(a, b, c=None, alpha=1.0, beta=0.0, out_dtype=None):
     """
     GEMM operation without SM constraint by Triton.
     C = alpha * (a @ b.T) + beta * C
@@ -86,8 +96,11 @@ def gemm(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
     Args:
         a: The first input matrix. Shape: (M, K)
         b: The second input matrix. Shape: (K, N)
-        c: The output matrix. Shape: (M, N). In-place operation is supported.
+        c: The output matrix. Shape: (M, N). In-place epilogue is supported. Expected to be out_dtype (if not specified, same as a.dtype, but fp8 --> bf16).
         alpha: The scaling factor for the product of a and b.
+        beta: The scaling factor for the output matrix c.
+        out_dtype: The dtype of the output matrix. Default: fp8 --> bf16. Otherwise, same as a.dtype.
+        num_sms: The number of SMs to use for the computation.
     """
     # Check constraints.
     check_input(a)
@@ -105,9 +118,18 @@ def gemm(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
     M, K = a.shape
     K, N = b.shape
     dtype = a.dtype
+    out_dtype = (
+        out_dtype
+        if out_dtype
+        else dtype if dtype != torch.float8_e4m3fn else torch.bfloat16
+    )
+
+    assert (
+        c is None or c.dtype == out_dtype
+    ), "Incompatible dtypes between c and out_dtype"
 
     # Allocates output.
-    c = torch.empty((M, N), device=a.device, dtype=dtype) if c is None else c
+    c = torch.empty((M, N), device=a.device, dtype=out_dtype) if c is None else c
 
     # 1D launch kernel where each block gets its own program.
     grid = lambda META: (
@@ -134,22 +156,33 @@ def gemm(a, b, c=None, alpha=1.0, beta=0.0, num_sms=None):
 
 
 def gemm_descriptor_persistent(
-    a, b, c=None, alpha=1.0, beta=0.0, num_sms=None, EPILOGUE_SUBTILE=False
+    a,
+    b,
+    c=None,
+    alpha=1.0,
+    beta=0.0,
+    out_dtype=None,
+    num_sms=None,
+    EPILOGUE_SUBTILE=False,
 ):
     """
-    GEMM operation with SM constraint by Triton (Hopper & TMA), using TMA and device-side descriptor creation.
+    GEMM operation with SM constraint by Triton.
+    Requires TMA support and descriptor creation.
     C = alpha * (a @ b.T) + beta * C
 
     Note:
         - K and N must be greater than 16.
+        - Support float16, float8_e4m3fn, bfloat16.
 
     Args:
         a: The first input matrix. Shape: (M, K)
         b: The second input matrix. Shape: (K, N)
-        c: The output matrix. Shape: (M, N). In-place operation is supported.
+        c: The output matrix. Shape: (M, N). In-place epilogue is supported. Expected to be out_dtype (if not specified, same as a.dtype, but fp8 --> bf16).
         alpha: The scaling factor for the product of a and b.
         beta: The scaling factor for the output matrix c.
+        out_dtype: The dtype of the output matrix. Default: fp8 --> bf16. Otherwise, same as a.dtype.
         num_sms: The number of SMs to use for the computation.
+        EPILOGUE_SUBTILE: Whether to use the epilogue subtile optimization.
     """
     # Check constraints.
     check_input(a)
@@ -167,11 +200,21 @@ def gemm_descriptor_persistent(
     M, K = a.shape
     N, K = b.shape
     dtype = a.dtype
+    out_dtype = (
+        out_dtype
+        if out_dtype
+        else dtype if dtype != torch.float8_e4m3fn else torch.bfloat16
+    )
 
-    assert K >= 16, "K must be greater than 16"
-    assert N >= 16, "N must be greater than 16"
+    # check on TMA tensor map swizzling granularity
+    assert K >= 16, "K must be >= 16"
+    assert N >= 16, "N must be >= 16"
 
-    c = torch.empty((M, N), device=a.device, dtype=dtype) if c is None else c
+    assert (
+        c is None or c.dtype == out_dtype
+    ), "Incompatible dtypes between c and out_dtype"
+
+    c = torch.empty((M, N), device=a.device, dtype=out_dtype) if c is None else c
 
     NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
     num_sms = NUM_SMS if num_sms is None else min(NUM_SMS, num_sms)
