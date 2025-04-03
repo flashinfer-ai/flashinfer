fe27b7e6826a53a3651c0882126d416e42bd99f8
Zihao Ye <expye@outlook.com>
2025-04-03 23:52:22 +0000

    upd

---
+++ flashinfer/attention.py
@@ -0,0 +1,27 @@
+"""
+Copyright (c) 2025 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import functools
+import logging
+import math
+from types import SimpleNamespace
+from typing import Any, List, Literal, Optional, Tuple, Union, overload
+
+import torch
+
+
+def get_holistic_attention_module():
+    pass
