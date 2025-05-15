e00e8cedbfcb220f328fd36aa8f529f869b01e6b
Zihao Ye <expye@outlook.com>
2025-05-14 17:55:13 -0700

    bugfix: move `cum_m` calculation inside kernels (#1060)

--- csrc/group_gemm_groupwise_sm100.cu
+++ csrc/group_gemm_groupwise_sm100.cu
@@ -51,9 +51,8 @@ using namespace flashinfer;
 void CutlassGroupGemmGroupwiseScaledSM100(at::Tensor int_workspace_buffer,
                                           at::Tensor float_workspace_buffer, at::Tensor A,
                                           at::Tensor B, at::Tensor SFA, at::Tensor SFB,
-                                          at::Tensor C, at::Tensor m_indptr, int64_t cum_m,
-                                          int64_t n, int64_t k, int64_t scale_granularity_m,
-                                          int64_t scale_granularity_n,
+                                          at::Tensor C, at::Tensor m_indptr, int64_t n, int64_t k,
+                                          int64_t scale_granularity_m, int64_t scale_granularity_n,
                                           int64_t scale_granularity_k) {
   const c10::cuda::OptionalCUDAGuard device_guard(float_workspace_buffer.device());
   auto stream = at::cuda::getCurrentCUDAStream();
@@ -72,8 +71,8 @@ void CutlassGroupGemmGroupwiseScaledSM100(at::Tensor int_workspace_buffer,
               float_workspace_buffer.element_size() * float_workspace_buffer.size(0),
               static_cast<cutlass_t_in*>(A.data_ptr()), static_cast<cutlass_t_in*>(B.data_ptr()),
               static_cast<float*>(SFA.data_ptr()), static_cast<float*>(SFB.data_ptr()),
-              static_cast<cutlass_t_out*>(C.data_ptr()), static_cast<int*>(m_indptr.data_ptr()),
-              cum_m, n, k, batch_size, stream);
+              static_cast<cutlass_t_out*>(C.data_ptr()), static_cast<int*>(m_indptr.data_ptr()), n,
+              k, batch_size, stream);
           return true;
         });
   });
