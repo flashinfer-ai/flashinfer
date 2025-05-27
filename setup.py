9af6afe1f85d39823b9383d620ff53004a46ebae
Mehdi Amini <joker.eph@gmail.com>
2025-05-27 03:21:59 +0200

    [nvidia] Add Blackwell FMHA decode kernel from TRT-LLM (#1051)
    This patch adds support for downloading cubins on the fly and caching
    them on disk. Only the cubin actually used gets downloaded and stored
    locally, avoiding a binary size explosion for the library.
    
    This is a first version of this feature, we'll update the PR with some
    benchmark info later.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def generate_build_meta(aot_build_meta: dict) -> None:
 
 ext_modules = []
 cmdclass = {}
-install_requires = ["numpy", "torch", "ninja"]
+install_requires = ["numpy", "torch", "ninja", "requests"]
 generate_build_meta({})
 
 if enable_aot:
@@ -70,7 +70,8 @@ if enable_aot:
     cuda_version = get_cuda_version()
     torch_full_version = Version(torch.__version__)
     torch_version = f"{torch_full_version.major}.{torch_full_version.minor}"
-    install_requires = [f"torch == {torch_version}.*"]
+    install_requires = [req for req in install_requires if not req.startswith("torch ")]
+    install_requires.append(f"torch == {torch_version}.*")
 
     aot_build_meta = {}
     aot_build_meta["cuda_major"] = cuda_version.major
