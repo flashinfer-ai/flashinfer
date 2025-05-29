a4234a13167f42ba79bfd00b037cc239eae5524b
Anerudhan Gopal <agopal@nvidia.com>
2025-05-29 11:49:54 -0700

    Reference running into issues

--- tests/test_cudnn_prefill.py
+++ tests/test_cudnn_prefill.py
@@ -28,7 +28,7 @@ def test_cudnn_prefill(
     return_lse,
 ):
     print(
-        f"Running test with batch_size: {batch_size}, s_qo: {s_qo}, s_kv: {s_kv}, page_size: {page_size}, num_kv_heads: {num_kv_heads}, num_qo_heads: {num_qo_heads}, head_dim: {head_dim}, causal: {causal}, return_lse: {return_lse}"
+        f"\nRunning test with batch_size: {batch_size}, s_qo: {s_qo}, s_kv: {s_kv}, page_size: {page_size}, num_kv_heads: {num_kv_heads}, num_qo_heads: {num_qo_heads}, head_dim: {head_dim}, causal: {causal}, return_lse: {return_lse}"
     )
     # test set up basics
     seed = 0
@@ -39,10 +39,10 @@ def test_cudnn_prefill(
     shape = (batch_size, num_qo_heads, s_qo, head_dim)
     strides = (num_qo_heads * s_qo * head_dim, head_dim, num_qo_heads * head_dim, 1)
 
-    q = torch.randn(shape, device=device).half()
+    q_unstrided = torch.randn(shape, device=device).half()
 
     # Create view with desired strides [1, h*d, d, h*s*d]
-    q = q.as_strided(shape, strides)
+    q = q_unstrided.as_strided(shape, strides)
 
     # Initialize KV Cache
     num_pages_per_seq = (s_kv + page_size - 1) // page_size
@@ -74,6 +74,9 @@ def test_cudnn_prefill(
         1, s_kv + 1, (batch_size, 1, 1, 1), dtype=torch.int32
     )
 
+    print(f"actual_seq_lens_q: {actual_seq_lens_q}")
+    print(f"actual_seq_lens_kv: {actual_seq_lens_kv}")
+
     workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.int8, device=device)
 
     output, lse = flashinfer.prefill.cudnn_batch_prefill_with_kv_cache(
@@ -102,55 +105,76 @@ def test_cudnn_prefill(
         (batch_size, num_qo_heads, s_qo, head_dim),
         (num_qo_heads * s_qo * head_dim, s_qo * head_dim, head_dim, 1),
     )
-    # print(f"output: {output[2, 0:4, 4:8, 0:3]}")
-    # print(f"output: {output[2, 2:4, 0:8, 0:3]}")
-    # print(f"output.shape: {output.shape}, output.stride: {output.stride()}, output.data_ptr: {hex(output.data_ptr())}")
 
     output_np = output.cpu().numpy()
     with open("cudnn_prefill_output.csv", "w", newline="") as f:
         csv.writer(f).writerows([[float(x)] for x in output_np.flatten()])
 
-    # workspace_buffer_ref = torch.empty(128 * 1024 * 1024, dtype=torch.int8, device=device)
+    workspace_buffer_ref = torch.empty(
+        128 * 1024 * 1024, dtype=torch.int8, device=device
+    )
 
-    # wrapper = flashinfer.BatchPrefillWithPagedKVCacheWrapper(workspace_buffer_ref, "NHD")
+    wrapper = flashinfer.BatchPrefillWithPagedKVCacheWrapper(
+        workspace_buffer_ref, "NHD"
+    )
 
-    # actual_seq_lens_kv_device = actual_seq_lens_kv.to(device)
+    actual_seq_lens_q_device = actual_seq_lens_q.to(device)
+    actual_seq_lens_kv_device = actual_seq_lens_kv.to(device)
 
-    # q_flat = q.reshape(-1, q.shape[1], q.shape[3])
-    # print(f"q_flat.shape: {q_flat.shape}, q_flat.stride: {q_flat.stride()}")
+    qo_indptr = (
+        torch.cat(
+            [
+                torch.tensor([0], device=device),
+                torch.cumsum(actual_seq_lens_q_device.view(-1), dim=0),
+            ]
+        )
+        .int()
+        .to(device)
+    )
+
+    q_flat = q.reshape(-1, q.shape[1], q.shape[3])
+    print(f"q_flat.shape: {q_flat.shape}, q_flat.stride: {q_flat.stride()}")
 
     # print(f"q data: {hex(q.data_ptr())}, q_flat.data: {hex(q_flat.data_ptr())}")
 
-    # kv_indptr = (
-    #     torch.cat(
-    #         [
-    #             torch.tensor([0], device=device),
-    #             torch.cumsum(actual_seq_lens_kv_device.view(-1) // page_size, dim=0),
-    #         ]
-    #     )
-    #     .int()
-    #     .to(device)
-    # )
-    # kv_indices = torch.arange(total_num_pages, device=device).int().to(device)
-    # kv_last_page_len = (
-    #     torch.full((batch_size,), page_size, device=device).int().to(device)
-    # )
+    kv_indptr = (
+        torch.cat(
+            [
+                torch.tensor([0], device=device),
+                torch.cumsum(actual_seq_lens_kv_device.view(-1), dim=0),
+            ]
+        )
+        .int()
+        .to(device)
+    )
 
-    # wrapper.plan(
-    #     actual_seq_lens_q,
-    #     kv_indptr,
-    #     kv_indices,
-    #     kv_last_page_len,
-    #     num_qo_heads,
-    #     num_kv_heads,
-    #     head_dim,
-    #     page_size,
-    #     pos_encoding_mode="NONE",
-    #     causal=True,
-    #     q_data_type=torch.float16,
-    # )
+    print(f"qo_indptr: {qo_indptr}")
+    print(f"kv_indptr: {kv_indptr}")
 
-    # output_ref = wrapper.run(q_flat.contiguous(), kv_cache)
+    kv_indices = torch.arange(total_num_pages, device=device).int().to(device)
+    kv_last_page_len = (
+        torch.full((batch_size,), page_size, device=device).int().to(device)
+    )
+
+    wrapper.plan(
+        qo_indptr,
+        kv_indptr,
+        kv_indices,
+        kv_last_page_len,
+        num_qo_heads,
+        num_kv_heads,
+        head_dim,
+        page_size,
+        pos_encoding_mode="NONE",
+        causal=True,
+        q_data_type=torch.float16,
+    )
+
+    assert q_flat.is_contiguous()
+
+    output_ref = wrapper.run(q_flat, kv_cache)
+
+    print(f"output_ref: {output_ref.shape} @ {output_ref.stride()}")
 
     # output_ref_np = output_ref.cpu().numpy()
     # with open("cudnn_prefill_output_ref.csv", "w", newline="") as f:
