e8223cf992b2a57be1912a0eb6451a353cbbcd13
Edenzzzz <wtan45@wisc.edu>
2025-06-15 22:34:03 +0000

    fix

--- include/flashinfer/attention/persistent_template.cuh
+++ include/flashinfer/attention/persistent_template.cuh
@@ -69,7 +69,7 @@ __global__ __launch_bounds__(
   BlockPersistentRunner1::Run(params_1, &smem_storage_1);
   PROFILER_EVENT_END(profiler_closure, PersistentProfileEventType::kRunner1);
 
-  __syncthreads();
+  // __syncthreads();
 
   auto& smem_storage_2 =
       reinterpret_cast<typename BlockPersistentRunner2::KTraits::SharedStorage&>(smem);
