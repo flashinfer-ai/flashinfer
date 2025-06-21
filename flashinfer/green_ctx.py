2243d4602df8e71dfbb25b3c47adabf335ea12b2
Zihao Ye <expye@outlook.com>
2025-06-21 05:36:46 +0000

    upd

--- flashinfer/green_ctx.py
+++ flashinfer/green_ctx.py
@@ -1,3 +1,19 @@
+"""
+Copyright (c) 2025 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from typing import List, Tuple
 
 import cuda.bindings.driver as driver
@@ -92,6 +108,37 @@ def create_green_ctx_streams(
 def split_device_green_ctx(
     dev: torch.device, num_groups: int, min_count: int
 ) -> Tuple[List[torch.Stream], List[CUdevResource]]:
+    r"""
+    Split the device into multiple green contexts.
+
+    Args:
+        dev: The device to split.
+        num_groups: The number of groups to split the device into.
+        min_count: Minimum number of SMs required for each group.
+
+    Returns:
+        streams: The list of torch.Streams objects corresponding to the green contexts.
+        resources: The list of CUdevResource objects corresponding to the green contexts.
+
+    Example:
+        >>> from flashinfer.green_ctx import split_device_green_ctx
+        >>> import torch
+        >>> dev = torch.device("cuda:0")
+        >>> streams, resources = split_device_green_ctx(dev, 2, 16)
+        >>> print([r.sm.smCount for r in resources])
+        [16, 16, 100]
+        >>> with torch.cuda.stream(streams[0]):
+        ...     x = torch.randn(8192, 8192, device=dev, dtype=torch.bfloat16)
+        ...     y = torch.randn(8192, 8192, device=dev, dtype=torch.bfloat16)
+        ...     z = x @ y
+        ...     print(z.shape)
+        ...
+        torch.Size([8192, 8192])
+
+    Note:
+        The length of the returned streams and resources is ``num_groups + 1``,
+        where the last one is the remaining SMs.
+    """
     cu_dev = get_cudevice(dev)
     resource = get_device_resource(cu_dev)
     results, remaining = split_resource(resource, num_groups, min_count)
