0600341f49a2b19c9a3d030869286e5125d77c74
Yong Wu <yongcale@gmail.com>
2025-06-22 04:59:32 +0000

    Add trtllm mnnvl alltoall
    Use trtllm_alltoall.cuh instead of trtllm_alltoall.cu
    
    Upd
    
    Use pytorch_extension_utils
    
    upd
    
    upd
    
    upd
    
    compiled
    
    Fix build
    
    Register python ops
    
    fix
    
    Upd
    
    Add unittest
    
    fix
    
    fix
    
    fix
    
    Add multi-gpu test cases
    
    Add cross-gpu test
    
    Remove the invalid cross-gpu test
    
    add mnnvl (wip)
    
    comm module

---
+++ flashinfer/comm/__init__.py
@@ -0,0 +1,4 @@
+from .comm import *
+from .mapping import Mapping
+from .mnnvl import MnnvlMemory, MoEAlltoallInfo, MnnvlMoe
+from .dlpack_utils import pack_strided_memory
--- flashinfer/comm/comm.py
+++ flashinfer/comm/comm.py
@@ -24,10 +24,10 @@ import torch
 import torch.distributed as dist
 from torch.distributed import ProcessGroup
 
-from .jit import JitSpec
-from .jit import env as jit_env
-from .jit import gen_jit_spec, sm100a_nvcc_flags
-from .utils import register_custom_op
+from ..jit import JitSpec
+from ..jit import env as jit_env
+from ..jit import gen_jit_spec, sm100a_nvcc_flags
+from ..utils import register_custom_op
 
 # NOTE(Zihao): we should use cuda-python instead of ctypes cuda runtime bindings.
 # However, cuda-python's API is not stable yet, so we use ctypes bindings instead.
@@ -645,6 +645,170 @@ def get_trtllm_comm_module():
     )
 
 
+def gen_comm_alltoall_module() -> JitSpec:
+    return gen_jit_spec(
+        "comm",
+        [
+            jit_env.FLASHINFER_CSRC_DIR / "trtllm_alltoall.cu",
+        ],
+    )
+
+
+@functools.cache
+def get_comm_alltoall_module():
+    module = gen_comm_alltoall_module().build_and_load()
+
+    @register_custom_op(
+        "flashinfer::moe_comm_prepare_indices",
+        mutates_args=[],
+    )
+    def moe_comm_prepare_indices(
+        gathered_target_rank_ids: torch.Tensor,
+        real_rank_token_count_cum_sum: Optional[torch.Tensor],
+        max_token_count_per_rank: int,
+        expert_count: int,
+        top_k: int,
+        ep_rank: int,
+        ep_size: int,
+    ) -> Tuple[
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+    ]:
+        device = gathered_target_rank_ids.device
+        max_send_ranks_per_token = max(top_k, ep_size)
+        local_gather_indices = torch.empty(
+            (max_token_count_per_rank * ep_size), device=device, dtype=torch.int
+        )
+        send_rank_count_cum_sum = torch.empty(
+            (ep_size,), device=device, dtype=torch.int
+        )
+        send_rank_local_indices = torch.empty(
+            (max_token_count_per_rank * max_send_ranks_per_token),
+            device=device,
+            dtype=torch.int,
+        )
+        recv_rank_count_cum_sum = torch.empty((ep_size), device=device, dtype=torch.int)
+        recv_rank_local_indices = torch.empty(
+            (max_token_count_per_rank * ep_size), device=device, dtype=torch.int
+        )
+        backward_recv_rank_local_indice = torch.empty(
+            (max_token_count_per_rank * max_send_ranks_per_token),
+            device=device,
+            dtype=torch.int,
+        )
+        module.moe_comm_prepare_indices(
+            gathered_target_rank_ids,
+            real_rank_token_count_cum_sum,
+            local_gather_indices,
+            send_rank_count_cum_sum,
+            send_rank_local_indices,
+            recv_rank_count_cum_sum,
+            recv_rank_local_indices,
+            backward_recv_rank_local_indice,
+            max_token_count_per_rank,
+            expert_count,
+            top_k,
+            ep_rank,
+            ep_size,
+        )
+        return (
+            local_gather_indices,
+            send_rank_count_cum_sum,
+            send_rank_local_indices,
+            recv_rank_count_cum_sum,
+            recv_rank_local_indices,
+            backward_recv_rank_local_indice,
+        )
+
+    @register_custom_op(
+        "flashinfer::moe_local_gather",
+        mutates_args=["local_expert_ids", "local_scales"],
+    )
+    def moe_local_gather(
+        recv_rank_cum_sum: torch.Tensor,
+        local_gather_indices: torch.Tensor,
+        gathered_expert_ids: torch.Tensor,
+        gathered_scales: torch.Tensor,
+        local_expert_ids: torch.Tensor,
+        local_scales: torch.Tensor,
+        max_token_count_per_rank: int,
+        expert_count: int,
+        top_k: int,
+        ep_rank: int,
+        ep_size: int,
+    ) -> None:
+        module.moe_local_gather(
+            recv_rank_cum_sum,
+            local_gather_indices,
+            gathered_expert_ids,
+            gathered_scales,
+            local_expert_ids,
+            local_scales,
+            max_token_count_per_rank,
+            expert_count,
+            top_k,
+            ep_rank,
+            ep_size,
+        )
+
+    @register_custom_op(
+        "flashinfer::moe_comm",
+        mutates_args=["output"],
+    )
+    def moe_comm(
+        input: torch.Tensor,
+        send_rank_cum_sum: torch.Tensor,
+        send_indices: torch.Tensor,
+        output: torch.Tensor,
+        recv_rank_cum_sum: torch.Tensor,
+        recv_indices: torch.Tensor,
+        all_workspaces: torch.Tensor,
+        ep_rank: int,
+        ep_size: int,
+    ) -> None:
+        module.moe_comm(
+            input,
+            send_rank_cum_sum,
+            send_indices,
+            output,
+            recv_rank_cum_sum,
+            recv_indices,
+            all_workspaces,
+            ep_rank,
+            ep_size,
+        )
+
+    @register_custom_op(
+        "flashinfer::set_moe_max_usable_sm_count",
+        mutates_args=[],
+    )
+    def set_moe_max_usable_sm_count(
+        max_sm_count: int,
+    ) -> None:
+        module.set_moe_max_usable_sm_count(max_sm_count)
+
+    @register_custom_op(
+        "flashinfer::get_moe_commworkspace_size_per_rank",
+        mutates_args=[],
+    )
+    def get_moe_commworkspace_size_per_rank(
+        ep_size: int,
+    ) -> int:
+        return module.get_moe_commworkspace_size_per_rank(ep_size)
+
+    return SimpleNamespace(
+        moe_comm_prepare_indices=moe_comm_prepare_indices,
+        moe_local_gather=moe_local_gather,
+        moe_comm=moe_comm,
+        set_moe_max_usable_sm_count=set_moe_max_usable_sm_count,
+        get_moe_commworkspace_size_per_rank=get_moe_commworkspace_size_per_rank,
+    )
+
+
 def init_custom_ar(
     ipc_tensors: List[int], rank_data: torch.Tensor, rank: int, full_nvlink: bool
 ) -> int:
@@ -1157,3 +1321,89 @@ def trtllm_moe_finalize_allreduce_fusion(
         shared_expert_output=shared_expert_output,
         expert_scale_factor=expert_scale_factor,
     )
+
+
+def moe_comm_prepare_indices(
+    gathered_target_rank_ids: torch.Tensor,
+    real_rank_token_count_cum_sum: Optional[torch.Tensor],
+    max_token_count_per_rank: int,
+    expert_count: int,
+    top_k: int,
+    ep_rank: int,
+    ep_size: int,
+) -> Tuple[
+    torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor
+]:
+    return get_comm_alltoall_module().moe_comm_prepare_indices(
+        gathered_target_rank_ids,
+        real_rank_token_count_cum_sum,
+        max_token_count_per_rank,
+        expert_count,
+        top_k,
+        ep_rank,
+        ep_size,
+    )
+
+
+def moe_local_gather(
+    recv_rank_cum_sum: torch.Tensor,
+    local_gather_indices: torch.Tensor,
+    gathered_expert_ids: torch.Tensor,
+    gathered_scales: torch.Tensor,
+    local_expert_ids: torch.Tensor,
+    local_scales: torch.Tensor,
+    max_token_count_per_rank: int,
+    expert_count: int,
+    top_k: int,
+    ep_rank: int,
+    ep_size: int,
+) -> None:
+    get_comm_alltoall_module().moe_local_gather(
+        recv_rank_cum_sum,
+        local_gather_indices,
+        gathered_expert_ids,
+        gathered_scales,
+        local_expert_ids,
+        local_scales,
+        max_token_count_per_rank,
+        expert_count,
+        top_k,
+        ep_rank,
+        ep_size,
+    )
+
+
+def moe_comm(
+    input: torch.Tensor,
+    send_rank_cum_sum: torch.Tensor,
+    send_indices: torch.Tensor,
+    output: torch.Tensor,
+    recv_rank_cum_sum: torch.Tensor,
+    recv_indices: torch.Tensor,
+    all_workspaces: torch.Tensor,
+    ep_rank: int,
+    ep_size: int,
+) -> None:
+    get_comm_alltoall_module().moe_comm(
+        input,
+        send_rank_cum_sum,
+        send_indices,
+        output,
+        recv_rank_cum_sum,
+        recv_indices,
+        all_workspaces,
+        ep_rank,
+        ep_size,
+    )
+
+
+def set_moe_max_usable_sm_count(
+    max_sm_count: int,
+) -> None:
+    get_comm_alltoall_module().set_moe_max_usable_sm_count(max_sm_count)
+
+
+def get_moe_commworkspace_size_per_rank(
+    ep_size: int,
+) -> int:
+    return get_comm_alltoall_module().get_moe_commworkspace_size_per_rank(ep_size)
