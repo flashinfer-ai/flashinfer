4ec2116e58ad5c0c7e130690ad0b7b57f2698020
Yilong Zhao <74357408+happierpig@users.noreply.github.com>
2025-06-26 17:37:57 -0700

    [feat] support block sparse attention w/ variable block sizes and head-wise sparse patterns (#1177)
    <!-- .github/pull_request_template.md -->
    
    ## 📌 Description
    
    <!-- What does this PR do? Briefly describe the changes and why they’re
    needed. -->
    This PR implements a block sparse attention wrapper that calls the
    underlying FA2 and FA3 kernel implementation, which supports:
    1. Variable block size for the block sparse attention: Recent research
    progress in sparse attention algorithms in (e.g.,
    https://arxiv.org/abs/2505.18875) necessitates sparse patterns with
    variable block sizes instead of fixed ones. E.g., the following figure
    illustrates one use case.
    <img width="627" alt="flashinfer-variable-block-sparse"
    src="https://github.com/user-attachments/assets/21a88440-a6e5-4829-abe1-dabc0b8b6310"
    />
    
    
    This PR provides a straightforward API: `block_mask_map [num_kv_heads,
    MB, NB]` is boolean tensor showcasing which block is important and
    activated; `block_row_sz [num_kv_heads, MB]` provides the size of each
    row block; `block_col_sz [num_kv_heads, NB]` provides the size of column
    block.
    2. Variable sparse patterns for each attention head. Users can specify
    different block sparse maps for different `kv_head_idx` by providing
    `[num_kv_heads, ...]` metadata. Note that GQA is supported by using the
    same attention maps for the same `gqa_group_size` qo_heads.
    
    Perf Benchmarks:
    All benchmarks are conducted with `num_blocks_row=20` and
    `num_blocks_col=50` (without considering `plan` overhead). Scripts are
    available at `benchmarks/bench_block_sparse_attention.py`. Note that
    sparse FA3 achieves near-theoretical speedup with higher sparsity, while
    falling behind with low sparsity due to sparse loading overhead.
    
    <img width="682" alt="image"
    src="https://github.com/user-attachments/assets/1433d65c-1381-4742-bc09-22ed989997ef"
    />
    
    
    ## 🔍 Related Issues
    
    <!-- Link any related issues here -->
    This PR may relate to
    https://github.com/flashinfer-ai/flashinfer/issues/1091 and
    https://github.com/flashinfer-ai/flashinfer/issues/886.
    ## 🚀 Pull Request Checklist
    
    Thank you for contributing to FlashInfer! Before we review your pull
    request, please make sure the following items are complete.
    
    ### ✅ Pre-commit Checks
    
    - [x] I have installed `pre-commit` by running `pip install pre-commit`
    (or used your preferred method).
    - [x] I have installed the hooks with `pre-commit install`.
    - [x] I have run the hooks manually with `pre-commit run --all-files`
    and fixed any reported issues.
    
    > If you are unsure about how to set up `pre-commit`, see [the
    pre-commit documentation](https://pre-commit.com/).
    
    ## 🧪 Tests
    
    - [x] Tests have been added or updated as needed.
    - [x] All tests are passing (`unittest`, etc.).
    New unit tests are added to `tests/test_block_sparse.py`. All passed.
    <img width="740" alt="image"
    src="https://github.com/user-attachments/assets/3d3f3274-818b-418f-b299-0471fbda1e57"
    />
    
    ## Reviewer Notes
    cc @yzh119 @andy-yang-1
    <!-- Optional: anything you'd like reviewers to focus on, concerns, etc.
    -->
    
    ---------
    
    Co-authored-by: happierpig <zhaoyilong217@sjtu.edn.cn>
    Co-authored-by: yzh119 <zihaoy@nvidia.com>

---
+++ benchmarks/bench_block_sparse_attention.py
@@ -0,0 +1,138 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import torch
+import triton
+
+import flashinfer
+
+
+def bench_variable_block_sparse_attention(
+    num_qo_heads,
+    num_kv_heads,
+    head_dim,
+    seq_len,
+    num_blocks_row,
+    num_blocks_col,
+    block_density,
+):
+    if num_qo_heads % num_kv_heads != 0:
+        return
+    if seq_len // num_blocks_row < 1:
+        return
+    if seq_len // num_blocks_col < 1:
+        return
+
+    # synthesize uniform block sz
+    block_row_sz = torch.ones(num_blocks_row, dtype=torch.int32) * (
+        seq_len // num_blocks_row
+    )
+    block_row_sz[-1] = seq_len - (seq_len // num_blocks_row) * (num_blocks_row - 1)
+    block_row_sz = block_row_sz.unsqueeze(0).repeat(num_kv_heads, 1)
+
+    block_col_sz = torch.ones(num_blocks_col, dtype=torch.int32) * (
+        seq_len // num_blocks_col
+    )
+    block_col_sz[-1] = seq_len - (seq_len // num_blocks_col) * (num_blocks_col - 1)
+    block_col_sz = block_col_sz.unsqueeze(0).repeat(num_kv_heads, 1)
+
+    block_mask_map = (
+        torch.rand(num_kv_heads, num_blocks_row, num_blocks_col) < block_density
+    )
+
+    q = torch.randn(num_qo_heads, seq_len, head_dim, dtype=torch.half, device="cuda")
+    k = torch.randn(num_kv_heads, seq_len, head_dim, dtype=torch.half, device="cuda")
+    v = torch.randn(num_kv_heads, seq_len, head_dim, dtype=torch.half, device="cuda")
+
+    float_workspace_buffer = torch.empty(
+        128 * 1024 * 1024, dtype=torch.uint8, device="cuda:0"
+    )
+    sparse_wrapper_fa2 = flashinfer.sparse.VariableBlockSparseAttentionWrapper(
+        float_workspace_buffer, backend="fa2"
+    )
+    sparse_wrapper_fa3 = flashinfer.sparse.VariableBlockSparseAttentionWrapper(
+        float_workspace_buffer, backend="fa3"
+    )
+
+    sparse_wrapper_fa2.plan(
+        block_mask_map=block_mask_map,
+        block_row_sz=block_row_sz,
+        block_col_sz=block_col_sz,
+        num_qo_heads=num_qo_heads,
+        num_kv_heads=num_kv_heads,
+        head_dim=head_dim,
+        q_data_type=torch.half,
+    )
+    sparse_wrapper_fa3.plan(
+        block_mask_map=block_mask_map,
+        block_row_sz=block_row_sz,
+        block_col_sz=block_col_sz,
+        num_qo_heads=num_qo_heads,
+        num_kv_heads=num_kv_heads,
+        head_dim=head_dim,
+        q_data_type=torch.half,
+    )
+
+    sparse_ms_fa2 = triton.testing.do_bench(
+        lambda: sparse_wrapper_fa2.run(q, k, v),
+        warmup=100,
+        rep=1000,
+    )
+    sparse_ms_fa3 = triton.testing.do_bench(
+        lambda: sparse_wrapper_fa3.run(q, k, v),
+        warmup=100,
+        rep=1000,
+    )
+
+    q = torch.randn(seq_len, num_qo_heads, head_dim, dtype=torch.half, device="cuda")
+    k = torch.randn(seq_len, num_kv_heads, head_dim, dtype=torch.half, device="cuda")
+    v = torch.randn(seq_len, num_kv_heads, head_dim, dtype=torch.half, device="cuda")
+    dense_sm80_ms, dense_sm90_ms = (
+        triton.testing.do_bench(
+            lambda: flashinfer.single_prefill_with_kv_cache_return_lse(
+                q, k, v, causal=False, backend=backend
+            ),
+            warmup=100,
+            rep=1000,
+        )
+        for backend in ["fa2", "fa3"]
+    )
+
+    def flops(ms):
+        return seq_len * seq_len * num_qo_heads * head_dim * 4 / ms / 1e9
+
+    print(
+        f"bench_variable_block_sparse_attention (num_qo_heads={num_qo_heads}, num_kv_heads={num_kv_heads}, head_dim={head_dim}, seq_len={seq_len}, num_blocks_row={num_blocks_row}, num_blocks_col={num_blocks_col}, block_density={block_density}), sparse fa2-template: {flops(sparse_ms_fa2):.3f} TFLOPs/s, sparse fa3-template: {flops(sparse_ms_fa3):.3f} TFLOPs/s, dense fa2-template: {flops(dense_sm80_ms):.3f} TFLOPs/s, dense fa3-template: {flops(dense_sm90_ms):.3f} TFLOPs/s"
+    )
+
+
+if __name__ == "__main__":
+    for num_qo_heads in [32]:
+        for num_kv_heads in [32]:
+            for head_dim in [128]:
+                for seq_len in [8192, 16384, 32768]:
+                    for num_blocks_row in [20]:
+                        for num_blocks_col in [50]:
+                            for block_density in [0.1, 0.3, 0.5, 0.7, 0.9]:
+                                bench_variable_block_sparse_attention(
+                                    num_qo_heads,
+                                    num_kv_heads,
+                                    head_dim,
+                                    seq_len,
+                                    num_blocks_row,
+                                    num_blocks_col,
+                                    block_density,
+                                )
