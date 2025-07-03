e30c97d14f4ae6d7cab00e4023d15efc7dfa2257
Alex Yang <aleyang@nvidia.com>
2025-07-03 16:52:00 -0700

    revert sampling.cuh

--- include/flashinfer/sampling.cuh
+++ include/flashinfer/sampling.cuh
@@ -254,11 +254,11 @@ __device__ __forceinline__ std::tuple<float, float> GetMinMaxValue(float* in_dat
     }
     max_val = max(
         max_val, BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-                     .Reduce<VEC_SIZE>(in_data_, cuda::std::maximum<>{}));
+                     .Reduce<VEC_SIZE>(in_data_, cub::Max()));
     __syncthreads();
     min_val = min(
         min_val, BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-                     .Reduce<VEC_SIZE>(in_data_, cuda::std::minimum<>{}));
+                     .Reduce<VEC_SIZE>(in_data_, cub::Min()));
     __syncthreads();
   }
   if (tx == 0) {
@@ -292,7 +292,7 @@ __device__ __forceinline__ float GetMaxValue(float* in_data, uint32_t row_idx, u
     }
     max_val = max(
         max_val, BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-                     .Reduce<VEC_SIZE>(in_data_, cuda::std::maximum<>{}));
+                     .Reduce<VEC_SIZE>(in_data_, cub::Max()));
     __syncthreads();
   }
   if (tx == 0) {
@@ -653,7 +653,7 @@ __device__ __forceinline__ void DeviceSamplingFromProb(
   }
   int max_valid_index =
       BlockReduce<int, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage->block_prim.reduce_int)
-          .Reduce(valid_index, cuda::std::maximum<>{});
+          .Reduce(valid_index, cub::Max());
   if (tx == 0 && max_valid_index != -1) {
     temp_storage->last_valid_id = max_valid_index;
   }
@@ -1621,11 +1621,11 @@ __global__ void TopPRenormProbKernel(DType* probs, DType* renormed_prob, float* 
       __syncthreads();
     }
     min_gt_low = BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-                     .Reduce(min_gt_low, cuda::std::minimum<>{});
+                     .Reduce(min_gt_low, cub::Min());
     __syncthreads();
     max_le_high =
         BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-            .Reduce(max_le_high, cuda::std::maximum<>{});
+            .Reduce(max_le_high, cub::Max());
     if (tx == 0) {
       temp_storage.block_aggregate.values[0] = aggregate_gt_pivot_0;
       temp_storage.block_aggregate.values[1] = aggregate_gt_pivot_1;
@@ -1743,11 +1743,11 @@ __global__ void TopKMaskLogitsKernel(DType* logits, DType* masked_logits, IdType
       }
       min_gt_low =
           BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-              .Reduce(min_gt_low, cuda::std::minimum<>{});
+              .Reduce(min_gt_low, cub::Min());
       __syncthreads();
       max_le_high =
           BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-              .Reduce(max_le_high, cuda::std::maximum<>{});
+              .Reduce(max_le_high, cub::Max());
       if (tx == 0) {
         temp_storage.block_aggregate.counts[0] = aggregate_gt_pivot_0;
         temp_storage.block_aggregate.counts[1] = aggregate_gt_pivot_1;
@@ -1863,11 +1863,11 @@ __global__ void TopKRenormProbKernel(DType* probs, DType* renormed_prob, IdType*
       }
       min_gt_low =
           BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-              .Reduce(min_gt_low, cuda::std::minimum<>{});
+              .Reduce(min_gt_low, cub::Min());
       __syncthreads();
       max_le_high =
           BlockReduce<float, BLOCK_THREADS, REDUCE_ALGORITHM>(temp_storage.block_prim.reduce)
-              .Reduce(max_le_high, cuda::std::maximum<>{});
+              .Reduce(max_le_high, cub::Max());
       if (tx == 0) {
         temp_storage.block_aggregate.pairs[0] = aggregate_gt_pivot_0;
         temp_storage.block_aggregate.pairs[1] = aggregate_gt_pivot_1;
