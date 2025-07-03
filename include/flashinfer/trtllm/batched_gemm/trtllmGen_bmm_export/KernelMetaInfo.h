72316b4620a8968cf55b7ede5dfcb5cd6a23736c
Alex Yang <aleyang@nvidia.com>
2025-07-03 17:42:46 +0000

    confirmed pre-commit totally harmless on metadata

--- include/flashinfer/trtllm/batched_gemm/trtllmGen_bmm_export/KernelMetaInfo.h
+++ include/flashinfer/trtllm/batched_gemm/trtllmGen_bmm_export/KernelMetaInfo.h
@@ -1,32 +1,28 @@
 /*
-* SPDX-FileCopyrightText: Copyright (c) 1993-2025 NVIDIA CORPORATION &
-* AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
+ * SPDX-FileCopyrightText: Copyright (c) 1993-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #pragma once
 
 #include "BatchedGemmOptions.h"
 
-namespace batchedGemm { 
+namespace batchedGemm {
 
-
-namespace tensorrt_llm
-{
-namespace kernels
-{
+namespace tensorrt_llm {
+namespace kernels {
 // clang-format off
 
 #define TLLM_GEN_COMMIT "e84aaea"
@@ -6852,6 +6848,6 @@ static const batchedGemm::BatchedGemmConfig tllmGenBatchedGemmList[] = {
 #endif // EXCLUDE_SM_100
 };
 // clang-format on
-} // namespace kernels
-} // namespace tensorrt_llm
-}
+}  // namespace kernels
+}  // namespace tensorrt_llm
+}  // namespace batchedGemm
