fix overflow at python if

flashinfer/comm/trtllm_ar.py
@@ -495,6 +495,8 @@ def trtllm_destroy_ipc_workspace_for_all_reduce(
 
 BarrierFlagCount = 256
 
+MAX_COMM_SIZE = 2147483647  # MAX_INT32
+
 
 def trtllm_create_ipc_workspace_for_all_reduce_fusion(
     tp_rank: int,
@@ -505,6 +507,14 @@ def trtllm_create_ipc_workspace_for_all_reduce_fusion(
     group: Optional[ProcessGroup] = None,
 ) -> List[int]:
     """
+    Parameters:
+    - tp_rank: the rank of the current process.
+    - tp_size: the size of the process group.
+    - max_token_num: the maximum number of tokens in a sequence.
+    - hidden_dim: the dimension of the hidden states.
+    - use_fp32_lamport: if True, we will use fp32 datatype in allreduce fusion.
+    - group: the process group to use.
+
     Note:
     We would init 3 IPC buffers for trtllm_custom_all_reduce_fusion.
     They are sized as follows:
@@ -530,6 +540,12 @@ def trtllm_create_ipc_workspace_for_all_reduce_fusion(
         if not use_fp32_lamport
         else tp_size * max_token_num * hidden_dim * 4
     )
+    if lamport_comm_size > MAX_COMM_SIZE:
+        print(
+            f"warning: lamport_comm_size {lamport_comm_size} is greater than MAX_COMM_SIZE {MAX_COMM_SIZE}, set to MAX_COMM_SIZE"
+        )
+        lamport_comm_size = MAX_COMM_SIZE
+
     lamport_buffer_size = lamport_comm_size * 3
 
     # we should init 3 buffers for all reduce fusion:
@@ -717,13 +733,24 @@ def trtllm_allreduce_fusion(
     Regarding the `use_oneshot` parameter:
 
     It should only be enabled when:
-    (1) Preferring the one-shot strategy over the two-shot strategy.
+    (1) Force to use the one-shot strategy based on your use case.
     (2) In min-latency mode, the sequence length is less than the one-shot max token number (currently 128).
 
-    Otherwise, it should be disabled.
+    Otherwise, it should be disabled (as False).
     """
     if not use_oneshot:
         assert token_num > world_size, "sequence length should be larger than tp_size"
+
+    required_lamport_comm_size = (
+        token_num * hidden_dim * 2 * world_size
+        if allreduce_in.dtype != torch.float32
+        else token_num * hidden_dim * 4 * world_size
+    )
+
+    if required_lamport_comm_size > MAX_COMM_SIZE and use_oneshot:
+        raise ValueError(
+            f"required_lamport_comm_size {required_lamport_comm_size} is greater than MAX_COMM_SIZE {MAX_COMM_SIZE}. Cannot use oneshot in this case."
+        )
 
     get_trtllm_comm_module().trtllm_allreduce_fusion(
         allreduce_in=allreduce_in,
@@ -772,6 +799,14 @@ def trtllm_moe_allreduce_fusion(
     quant_out: Optional[torch.Tensor],
     scale_out: Optional[torch.Tensor],
 ) -> None:
+    required_lamport_comm_size = moe_reduction_token_input.numel() * 2 * world_size
+
+    # Note: only one-shot is supported for moe allreduce fusion.
+    if required_lamport_comm_size > MAX_COMM_SIZE:
+        raise ValueError(
+            f"required_lamport_comm_size {required_lamport_comm_size} is greater than MAX_COMM_SIZE {MAX_COMM_SIZE}. Cannot use oneshot in this case."
+        )
+
     get_trtllm_comm_module().trtllm_moe_allreduce_fusion(
         world_size=world_size,
         world_rank=world_rank,
@@ -811,6 +846,15 @@ def trtllm_moe_finalize_allreduce_fusion(
     shared_expert_output: Optional[torch.Tensor],
     expert_scale_factor: Optional[torch.Tensor],
 ) -> None:
+
+    required_lamport_comm_size = allreduce_in.numel() * 2 * world_size
+
+    # Note: only one-shot is supported for moe allreduce fusion.
+    if required_lamport_comm_size > MAX_COMM_SIZE:
+        raise ValueError(
+            f"required_lamport_comm_size {required_lamport_comm_size} is greater than MAX_COMM_SIZE {MAX_COMM_SIZE}. Cannot use oneshot in this case."
+        )
+
     get_trtllm_comm_module().trtllm_moe_finalize_allreduce_fusion(
         allreduce_in=allreduce_in,
         residual_in=residual_in,