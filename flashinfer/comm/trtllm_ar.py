39baa7be65db31fab48ce34bd8776225aadb3e62
Zihao Ye <expye@outlook.com>
2025-07-14 07:36:24 -0400

    upd

--- flashinfer/comm/trtllm_ar.py
+++ flashinfer/comm/trtllm_ar.py
@@ -26,7 +26,7 @@ from torch.distributed import ProcessGroup
 
 from ..jit import JitSpec
 from ..jit import env as jit_env
-from ..jit import gen_jit_spec, sm100a_nvcc_flags
+from ..jit import gen_jit_spec
 from ..utils import register_custom_op, round_up
 from .cuda_ipc import create_shared_buffer, cudart, free_shared_buffer
 
@@ -103,7 +103,6 @@ def gen_trtllm_comm_module() -> JitSpec:
             jit_env.FLASHINFER_CSRC_DIR / "trtllm_allreduce_fusion.cu",
             jit_env.FLASHINFER_CSRC_DIR / "trtllm_moe_allreduce_fusion.cu",
         ],
-        extra_cuda_cflags=sm100a_nvcc_flags,
     )
 
 
