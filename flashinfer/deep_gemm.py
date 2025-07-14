c8a13fd5f978df04356579df52cdc51fbed73c8c
Yaxing Cai <caiyaxing666@gmail.com>
2025-07-14 04:51:34 +0000

    add path prefix

--- flashinfer/deep_gemm.py
+++ flashinfer/deep_gemm.py
@@ -899,9 +899,9 @@ def load(name: str, code: str) -> SM100FP8GemmRuntime:
     if cubin_name in RUNTIME_CACHE:
         return RUNTIME_CACHE[cubin_name]
     symbol, sha256 = KERNEL_MAP[cubin_name]
+    cubin_name = f"0ffe2769c7eea90c44894abadf3eecf38801a143/deep-gemm/{cubin_name}"
     get_cubin(cubin_name, sha256)
     path = FLASHINFER_CACHE_DIR / "cubins" / f"{cubin_name}.cubin"
-    print(path)
     assert path.exists()
     RUNTIME_CACHE[cubin_name] = SM100FP8GemmRuntime(str(path), symbol)
     return RUNTIME_CACHE[cubin_name]
