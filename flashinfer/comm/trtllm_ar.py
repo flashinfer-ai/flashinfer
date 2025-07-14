9c7774d155bd2a68ab94c5f62c3eea695a0d1079
Zihao Ye <expye@outlook.com>
2025-07-14 11:05:13 -0700

    fix: Remove sm100+ requirment for trtllm allreduce kernels (#1249)
    <!-- .github/pull_request_template.md -->
    
    ## 📌 Description
    
    The trtllm allreduce kernels requires sm100 arch, because when fused
    with fp4 quant, we rely on `cvt.rn{.relu}.f16x2.{e2m1x2/e2m3x2/e3m2x2}`
    instructions which is only supported for sm100+.
    
    This PR adds fallback software implementation for ealier architectures.
    
    ## 🚀 Pull Request Checklist
    
    Thank you for contributing to FlashInfer! Before we review your pull
    request, please make sure the following items are complete.
    
    ### ✅ Pre-commit Checks
    
    - [ ] I have installed `pre-commit` by running `pip install pre-commit`
    (or used your preferred method).
    - [ ] I have installed the hooks with `pre-commit install`.
    - [ ] I have run the hooks manually with `pre-commit run --all-files`
    and fixed any reported issues.
    
    > If you are unsure about how to set up `pre-commit`, see [the
    pre-commit documentation](https://pre-commit.com/).
    
    ## 🧪 Tests
    
    - [ ] Tests have been added or updated as needed.
    - [ ] All tests are passing (`unittest`, etc.).
    
    ## Reviewer Notes
    
    <!-- Optional: anything you'd like reviewers to focus on, concerns, etc.
    -->
    
    ---------
    
    Co-authored-by: averyhuang <averyh@nvidia.com>
    Co-authored-by: yingyihuang <yingyihuang2000@outlook.com>

--- flashinfer/comm/trtllm_ar.py
+++ flashinfer/comm/trtllm_ar.py
@@ -96,6 +96,7 @@ class FP4QuantizationSFLayout:
 
 
 def gen_trtllm_comm_module() -> JitSpec:
+    major, minor = torch.cuda.get_device_capability()
     return gen_jit_spec(
         "trtllm_comm",
         [
@@ -103,7 +104,7 @@ def gen_trtllm_comm_module() -> JitSpec:
             jit_env.FLASHINFER_CSRC_DIR / "trtllm_allreduce_fusion.cu",
             jit_env.FLASHINFER_CSRC_DIR / "trtllm_moe_allreduce_fusion.cu",
         ],
-        extra_cuda_cflags=sm100a_nvcc_flags,
+        extra_cuda_cflags=sm100a_nvcc_flags if major >= 10 and minor >= 0 else [],
     )
 
 
