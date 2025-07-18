b57d1d706f07f2a97a98da977bcb93a5f72deb7f
Vincent Huang <vincenth@nvidia.com>
2025-07-18 06:29:03 +0000

    add mm_fp4 cutlass backend
    support a/b input type e2m1, block quant type e4m3 with block size 16
    output bfloat16 and fp16, kernels ported from trtllm.
    
    Signed-off-by: Vincent Huang <vincenth@nvidia.com>

--- flashinfer/gemm.py
+++ flashinfer/gemm.py
@@ -156,6 +156,30 @@ def get_gemm_module():
     return _gemm_module
 
 
+def gen_gemm_sm100_module_cutlass_fp4() -> JitSpec:
+    gen_directory = jit_env.FLASHINFER_GEN_SRC_DIR / "gen_gemm_sm100_cutlass_fp4"
+    os.makedirs(gen_directory, exist_ok=True)
+    source_paths = [
+        jit_env.FLASHINFER_CSRC_DIR / "fp4_gemm_bf16.cu",
+        jit_env.FLASHINFER_CSRC_DIR / "fp4_gemm_fp16.cu",
+        jit_env.FLASHINFER_CSRC_DIR / "fp4Gemm.cpp",
+    ]
+
+    return gen_jit_spec(
+        "fp4_gemm",
+        source_paths,
+        extra_cuda_cflags=sm100a_nvcc_flags
+        + [
+            "-DENABLE_BF16",
+            "-DENABLE_FP4",
+        ],
+        extra_cflags=[
+            "-DFAST_BUILD",
+        ],
+        extra_ldflags=["-lcuda"],
+    )
+
+
 def gen_gemm_sm100_module() -> JitSpec:
     gen_directory = jit_env.FLASHINFER_GEN_SRC_DIR / "gen_gemm_sm100"
     os.makedirs(gen_directory, exist_ok=True)
@@ -234,6 +258,13 @@ def gen_gemm_sm100_module() -> JitSpec:
 @functools.cache
 def get_gemm_sm100_module():
     module = gen_gemm_sm100_module().build_and_load()
+
+    return module
+
+
+@functools.cache
+def get_gemm_sm100_module_cutlass_fp4():
+    module = gen_gemm_sm100_module_cutlass_fp4().build_and_load()
 
     return module
 
@@ -1108,6 +1139,7 @@ def mm_fp4(
     out_dtype: torch.dtype,
     out: Optional[torch.Tensor] = None,
     block_size: int = 16,
+    backend: Literal["cudnn", "cutlass"] = "cudnn",
 ) -> torch.Tensor:
     r"""MM FP4
 
@@ -1136,6 +1168,9 @@ def mm_fp4(
 
     block_size: int
         Block size for FP4 quantization, only 16 is supported.
+
+    backend: Literal["cudnn", "cutlass"]
+        The backend to use for the operation. Defaults to ``"cudnn"``.
 
     Returns
     -------
@@ -1213,25 +1248,29 @@ def mm_fp4(
         _expand_block_scale_tensor_shape(b_descale, batch)
     )
 
-    # build the fp4 cudnn graph
-    graph = build_cudnn_gemm_block_scale_dequantize_graph(
-        real_a_shape,
-        real_a_stride,
-        real_b_shape,
-        real_b_stride,
-        expanded_a_descale_shape,
-        expanded_a_descale_stride,
-        expanded_b_descale_shape,
-        expanded_b_descale_stride,
-        _get_native_fp4_dtype(),
-        torch.float8_e4m3fn,
-        _torch_data_type_to_cudnn_data_type(out_dtype),
-        block_size,
-        a.device,
-    )
+    if backend == "cudnn":
+        # build the fp4 cudnn graph
+        graph = build_cudnn_gemm_block_scale_dequantize_graph(
+            real_a_shape,
+            real_a_stride,
+            real_b_shape,
+            real_b_stride,
+            expanded_a_descale_shape,
+            expanded_a_descale_stride,
+            expanded_b_descale_shape,
+            expanded_b_descale_stride,
+            _get_native_fp4_dtype(),
+            torch.float8_e4m3fn,
+            _torch_data_type_to_cudnn_data_type(out_dtype),
+            block_size,
+        )
 
-    # execute the fp4 cudnn graph
-    execute_cudnn_gemm_fp4_graph(graph, a, b, a_descale, b_descale, alpha, out)
+        # execute the fp4 cudnn graph
+        execute_cudnn_gemm_fp4_graph(graph, a, b, a_descale, b_descale, alpha, out)
+    elif backend == "cutlass":
+        get_gemm_sm100_module_cutlass_fp4().fp4_gemm.default(
+            a, b.T, a_descale, b_descale.T, alpha, out
+        )
     return out
 
 
