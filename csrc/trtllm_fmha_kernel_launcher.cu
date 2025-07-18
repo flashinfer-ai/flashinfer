3d1b5fadfadc781673b71fdd8c78ff8611f7f43d
Zihao Ye <expye@outlook.com>
2025-07-18 14:10:23 -0400

    unify decode & prefill launcher

--- csrc/trtllm_fmha_kernel_launcher.cu
+++ csrc/trtllm_fmha_kernel_launcher.cu
@@ -33,14 +33,20 @@
 #include "pytorch_extension_utils.h"
 
 namespace flashinfer {
-template <typename DTypeQ, typename DTypeKV, typename DTypeO>
-void trtllm_paged_attention_decode_launcher(
+
+enum class TllmPagedAttentionMode {
+  Context,
+  ForGen,
+};
+
+template <typename DTypeQ, typename DTypeKV, typename DTypeO, TllmPagedAttentionMode mode>
+void trtllm_paged_attention_launcher(
     DTypeO* out, DTypeQ* query, DTypeKV* key_value_cache, void* workspace_buffer,
     KVCachePageIndex* block_tables, int* seq_lens, int64_t batch_size, int64_t max_seq_len,
     int64_t num_pages, int64_t num_qo_heads, int64_t num_kv_heads, int64_t head_dim,
     int64_t page_size, int64_t max_num_blocks_per_seq, double bmm1_scale, double bmm2_scale,
     int64_t window_left, int64_t sum_seq_q, int64_t sum_seq_kv, int64_t sm_count,
-    cudaStream_t stream) {
+    cudaStream_t stream, int* cum_seq_lens_q = nullptr, int* cum_seq_lens_kv = nullptr) {
   if (num_qo_heads % num_kv_heads != 0) {
     std::ostringstream err_msg;
     err_msg << "num_qo_heads must be a multiple of num_kv_heads, got num_kv_heads: " << num_kv_heads
@@ -51,51 +57,15 @@ void trtllm_paged_attention_decode_launcher(
   auto q_data_type = TypeToDataType<DTypeQ>::value;
   auto kv_data_type = TypeToDataType<DTypeKV>::value;
   auto o_data_type = TypeToDataType<DTypeO>::value;
-
-  bool use_multi_block = true;
   static auto fmha_runner = TllmGenFmhaRunner(q_data_type, kv_data_type, o_data_type);
 
   TllmGenFmhaRunnerParams runner_params;
 
-  runner_params.mMaskType = TrtllmGenAttentionMaskType::Dense;
-  runner_params.mKernelType = FmhaKernelType::Generation;
-  runner_params.mTileScheduler =
-      use_multi_block ? TileScheduler::Static : TileScheduler::Persistent;
-  runner_params.mMultiCtasKvMode = use_multi_block;
-
+  // Common params
   runner_params.qPtr = query;
-
-  runner_params.mQkvLayout = QkvLayout::PagedKv;
   runner_params.kvPtr = key_value_cache;
   runner_params.kvPageIdxPtr = block_tables;
-  runner_params.mMaxNumPagesPerSeqKv = max_num_blocks_per_seq;
-  runner_params.mNumTokensPerPage = page_size;
-
-  runner_params.mSumOfSeqLensQ = 1;
-  runner_params.mSumOfSeqLensKv = sum_seq_kv;
-  // runner_params.cumSeqLensQPtr = cum_seq_lens_q.data_ptr<int>();
-  // runner_params.cumSeqLensKvPtr = cum_seq_lens_kv.data_ptr<int>();
-
-  // num_kv_heads should be enough, but num_heads for safty at long seq len.
-  // Round up num_semaphores to a multiple of 8 to satisfy the 16B alignment requirement for
-  // multiCtasKvScratchPtr.
-  size_t num_semaphores = round_up(batch_size * num_qo_heads, 8);
-
-  runner_params.multiCtasKvScratchPtr = reinterpret_cast<void*>(
-      static_cast<char*>(workspace_buffer) + num_semaphores * sizeof(uint32_t));
-  runner_params.multiCtasKvCounterPtr = reinterpret_cast<int32_t*>(workspace_buffer);
-
-  zero_gmem_semaphore_launcher(runner_params.multiCtasKvCounterPtr, num_semaphores,
-                               /*enable_pdl=*/true, stream);
-
-  if (head_dim != 64 && head_dim != 128 && head_dim != 192 && head_dim != 256) {
-    std::ostringstream err_msg;
-    err_msg << "head_dim " << head_dim << " is not supported!";
-    FLASHINFER_ERROR(err_msg.str());
-  }
-
   runner_params.seqLensKvPtr = seq_lens;
-
   runner_params.oPtr = out;
   runner_params.mHeadDimQk = head_dim;
   runner_params.mHeadDimV = head_dim;
@@ -103,31 +73,56 @@ void trtllm_paged_attention_decode_launcher(
   runner_params.mNumHeadsKv = num_kv_heads;
   runner_params.mNumHeadsQPerKv = num_qo_heads / num_kv_heads;
   runner_params.mBatchSize = batch_size;
-  runner_params.mMaxSeqLenQ = 1;
   runner_params.mMaxSeqLenKv = max_seq_len;
-  runner_params.mSumOfSeqLensQ = int(batch_size * runner_params.mMaxSeqLenQ);
-  runner_params.mScaleQ = 1.0;
-
-  // Set the chunked attention size and sliding window size to INT_MAX to disable them when checking
-  // if the kernel is supported.
-  runner_params.mChunkedAttentionSize = INT_MAX;
-  runner_params.mAttentionWindowSize = window_left == -1 ? INT_MAX : window_left + 1;
-  auto [foundKernels, kinfo] = fmha_runner.isSupportedWithInfo(runner_params);
-  if (!foundKernels) {
-    std::ostringstream err_msg;
-    err_msg << "Missing TRTLLM-GEN decode kernel:" << kinfo;
-    FLASHINFER_ERROR(err_msg.str());
-  }
-  // else {
-  //   std::cout << "Found TRTLLM-GEN decode kernel" << kinfo << std::endl;
-  // }
-
+  runner_params.mMaxNumPagesPerSeqKv = max_num_blocks_per_seq;
+  runner_params.mNumTokensPerPage = page_size;
+  runner_params.mQkvLayout = QkvLayout::PagedKv;
   runner_params.mMultiProcessorCount = sm_count;
-  int max_head_dim_kv = head_dim;
   runner_params.mNumPagesInMemPool = num_pages * 2;
   runner_params.stream = stream;
   runner_params.outputScale = bmm2_scale;
   runner_params.scaleSoftmaxLog2 = bmm1_scale * M_LOG2E;
+  runner_params.mScaleQ = 1.0;
+  runner_params.mChunkedAttentionSize = INT_MAX;
+  runner_params.mAttentionWindowSize = window_left == -1 ? INT_MAX : window_left + 1;
+
+  if constexpr (mode == TllmPagedAttentionMode::Context) {
+    runner_params.mMaskType = TrtllmGenAttentionMaskType::Causal;
+    runner_params.mKernelType = FmhaKernelType::Context;
+    runner_params.mTileScheduler = TileScheduler::Persistent;
+    runner_params.mMultiCtasKvMode = false;
+
+    runner_params.mMaxSeqLenQ = sum_seq_q;
+    runner_params.mSumOfSeqLensQ = sum_seq_q;
+    runner_params.mSumOfSeqLensKv = sum_seq_kv;
+    runner_params.cumSeqLensQPtr = cum_seq_lens_q;
+    runner_params.cumSeqLensKvPtr = cum_seq_lens_kv;
+  } else {
+    // Decode
+    runner_params.mMaskType = TrtllmGenAttentionMaskType::Dense;
+    runner_params.mKernelType = FmhaKernelType::Generation;
+    runner_params.mTileScheduler = TileScheduler::Static;
+    runner_params.mMultiCtasKvMode = true;
+
+    runner_params.mMaxSeqLenQ = 1;
+    runner_params.mSumOfSeqLensQ = batch_size;
+    runner_params.mSumOfSeqLensKv = sum_seq_kv;
+
+    size_t num_semaphores = round_up(batch_size * num_qo_heads, 8);
+    runner_params.multiCtasKvScratchPtr = reinterpret_cast<void*>(
+        static_cast<char*>(workspace_buffer) + num_semaphores * sizeof(uint32_t));
+    runner_params.multiCtasKvCounterPtr = reinterpret_cast<int32_t*>(workspace_buffer);
+    zero_gmem_semaphore_launcher(runner_params.multiCtasKvCounterPtr, num_semaphores,
+                                 /*enable_pdl=*/true, stream);
+  }
+
+  auto [foundKernels, kinfo] = fmha_runner.isSupportedWithInfo(runner_params);
+  if (!foundKernels) {
+    std::ostringstream err_msg;
+    err_msg << "Missing TRTLLM-GEN kernel ("
+            << (mode == TllmPagedAttentionMode::Context ? "context" : "decode") << "): " << kinfo;
+    FLASHINFER_ERROR(err_msg.str());
+  }
 
   fmha_runner.run(runner_params);
 }
@@ -153,7 +148,7 @@ void trtllm_paged_attention_decode(at::Tensor& out, at::Tensor& query, at::Tenso
     auto device = query.device();
     const auto stream = at::cuda::getCurrentCUDAStream(device.index());
 
-    trtllm_paged_attention_decode_launcher<DTypeQ, DTypeKV, DTypeO>(
+    trtllm_paged_attention_launcher<DTypeQ, DTypeKV, DTypeO, TllmPagedAttentionMode::ForGen>(
         static_cast<DTypeO*>(out.data_ptr()), static_cast<DTypeQ*>(query.data_ptr()),
         static_cast<DTypeKV*>(key_value_cache.data_ptr()), workspace_buffer.data_ptr(),
         static_cast<KVCachePageIndex*>(block_tables.data_ptr()),
@@ -162,79 +157,6 @@ void trtllm_paged_attention_decode(at::Tensor& out, at::Tensor& query, at::Tenso
         window_left, sum_seq_q, sum_seq_kv, sm_count, stream);
     return true;
   });
-}
-
-template <typename DTypeQ, typename DTypeKV, typename DTypeO>
-void trtllm_paged_attention_context_launcher(
-    DTypeO* out, DTypeQ* query, DTypeKV* key_value_cache, void* workspace_buffer,
-    KVCachePageIndex* block_tables, int* seq_lens, int64_t batch_size, int64_t max_seq_len,
-    int64_t num_pages, int64_t num_qo_heads, int64_t num_kv_heads, int64_t head_dim,
-    int64_t page_size, int64_t max_num_blocks_per_seq, double bmm1_scale, double bmm2_scale,
-    int64_t window_left, int64_t sum_seq_q, int64_t sum_seq_kv, int64_t sm_count,
-    cudaStream_t stream, int* cum_seq_lens_q = nullptr, int* cum_seq_lens_kv = nullptr) {
-  auto q_data_type = TypeToDataType<DTypeQ>::value;
-  auto kv_data_type = TypeToDataType<DTypeKV>::value;
-  auto o_data_type = TypeToDataType<DTypeO>::value;
-
-  static auto fmha_runner = TllmGenFmhaRunner(q_data_type, kv_data_type, o_data_type);
-
-  TllmGenFmhaRunnerParams runner_params;
-
-  runner_params.mMaskType = TrtllmGenAttentionMaskType::Causal;
-  runner_params.mKernelType = FmhaKernelType::Context;
-
-  // Always use persistent scheduler for better performance.
-  runner_params.mTileScheduler = TileScheduler::Persistent;
-  runner_params.mMultiCtasKvMode = false;
-
-  runner_params.qPtr = query;
-
-  runner_params.mQkvLayout = QkvLayout::PagedKv;
-  runner_params.kvPtr = key_value_cache;
-  runner_params.kvPageIdxPtr = block_tables;
-  runner_params.mMaxNumPagesPerSeqKv = max_num_blocks_per_seq;
-  runner_params.mNumTokensPerPage = page_size;
-
-  runner_params.seqLensKvPtr = seq_lens;
-
-  runner_params.oPtr = out;
-  runner_params.mHeadDimQk = head_dim;
-  runner_params.mHeadDimV = head_dim;
-  runner_params.mNumHeadsQ = num_qo_heads;
-  runner_params.mNumHeadsKv = num_kv_heads;
-  runner_params.mNumHeadsQPerKv = num_qo_heads / num_kv_heads;
-  runner_params.mBatchSize = batch_size;
-  // NOTE(Zihao): need more discussions on the value of these two
-  runner_params.mMaxSeqLenQ = sum_seq_q;
-  runner_params.mMaxSeqLenKv = max_seq_len;
-
-  runner_params.mSumOfSeqLensQ = sum_seq_q;
-  runner_params.mSumOfSeqLensKv = sum_seq_kv;
-  runner_params.cumSeqLensQPtr = cum_seq_lens_q;
-  runner_params.cumSeqLensKvPtr = cum_seq_lens_kv;
-
-  runner_params.mScaleQ = 1.0;
-  // Set the chunked attention size and sliding window size to INT_MAX to disable them when checking
-  // if
-  // the kernel is supported.
-  runner_params.mChunkedAttentionSize = INT_MAX;
-  runner_params.mAttentionWindowSize = window_left == -1 ? INT_MAX : window_left + 1;
-
-  runner_params.mMultiProcessorCount = sm_count;
-  auto [foundKernels, kinfo] = fmha_runner.isSupportedWithInfo(runner_params);
-  if (!foundKernels) {
-    std::ostringstream err_msg;
-    TllmGenSelectKernelParams select_kernel_params{runner_params};
-    err_msg << "Missing TRTLLM-GEN context kernel:" << kinfo;
-    FLASHINFER_ERROR(err_msg.str());
-  }
-  int max_head_dim_kv = head_dim;
-
-  runner_params.mNumPagesInMemPool = num_pages * 2;
-  runner_params.stream = stream;
-  runner_params.outputScale = bmm2_scale;
-  runner_params.scaleSoftmaxLog2 = bmm1_scale * M_LOG2E;
-  fmha_runner.run(runner_params);
 }
 
 void trtllm_paged_attention_context(at::Tensor& out, at::Tensor& query, at::Tensor& key_value_cache,
@@ -259,7 +181,7 @@ void trtllm_paged_attention_context(at::Tensor& out, at::Tensor& query, at::Tens
     auto device = query.device();
     const auto stream = at::cuda::getCurrentCUDAStream(device.index());
 
-    trtllm_paged_attention_context_launcher<DTypeQ, DTypeKV, DTypeO>(
+    trtllm_paged_attention_launcher<DTypeQ, DTypeKV, DTypeO, TllmPagedAttentionMode::Context>(
         static_cast<DTypeO*>(out.data_ptr()), static_cast<DTypeQ*>(query.data_ptr()),
         static_cast<DTypeKV*>(key_value_cache.data_ptr()), workspace_buffer.data_ptr(),
         static_cast<KVCachePageIndex*>(block_tables.data_ptr()),
