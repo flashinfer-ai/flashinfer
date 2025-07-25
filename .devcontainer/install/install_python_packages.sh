cb2d49052ba939ca52c95cd005a1f56b61154214
Yingyi Huang <yingyihuang2000@outlook.com>
2025-07-25 05:53:49 +0000

    upd

--- .devcontainer/install/install_python_packages.sh
+++ .devcontainer/install/install_python_packages.sh
@@ -19,6 +19,7 @@
 set -e
 set -u
 
+pip3 install nvidia-cutlass-dsl
 pip3 install ninja pytest numpy scipy build cuda-python pytest
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
 pip3 install pre-commit
