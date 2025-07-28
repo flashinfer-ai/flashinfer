67e77fd5ab13297199372760b92b5541d0e465bb
Ilia Sergachev <1894984+sergachev@users.noreply.github.com>
2025-07-28 08:54:01 +0200

    feat: Add blockwise-scaled FP8 GEMM via TRTLLM-Gen. (#1320)
    ## 📌 Description
    
    Add fast implementation of DeepSeek-style block-scaled FP8 GEMM.
    
    ## 🚀 Pull Request Checklist
    
    Thank you for contributing to FlashInfer! Before we review your pull
    request, please make sure the following items are complete.
    
    ### ✅ Pre-commit Checks
    
    - [x] I have installed `pre-commit` by running `pip install pre-commit`
    (or used your preferred method).
    - [x] I have installed the hooks with `pre-commit install`.
    - [x] I have run the hooks manually with `pre-commit run --all-files`
    and fixed any reported issues.
    
    > If you are unsure about how to set up `pre-commit`, see [the
    pre-commit documentation](https://pre-commit.com/).
    
    ## 🧪 Tests
    
    - [x] Tests have been added or updated as needed.
    - [x] All tests are passing (`unittest`, etc.).

--- flashinfer/gemm.py
+++ flashinfer/gemm.py
@@ -42,6 +42,7 @@ except OSError as e:
 from .jit import JitSpec
 from .jit import env as jit_env
 from .jit import gen_jit_spec, sm90a_nvcc_flags, sm100a_nvcc_flags
+from .jit.cubin_loader import setup_cubin_loader
 from .jit.utils import dtype_cutlass_map, filename_safe_dtype_map, write_if_different
 from .utils import (
     _get_cache_buf,
@@ -51,6 +52,8 @@ from .utils import (
     register_custom_op,
     register_fake_op,
 )
+
+DEFAULT_WORKSPACE_SIZE = 32 * 1024 * 1024
 
 
 def gen_gemm_module() -> JitSpec:
@@ -236,6 +239,29 @@ def get_gemm_sm100_module():
     module = gen_gemm_sm100_module().build_and_load()
 
     return module
+
+
+def trtllm_gemm_gen_module() -> JitSpec:
+    return gen_jit_spec(
+        "trtllm_gemm",
+        [
+            jit_env.FLASHINFER_CSRC_DIR / "trtllm_gemm_runner.cu",
+        ],
+        extra_cuda_cflags=[
+            "-DTLLM_GEN_EXPORT_INTERFACE",
+            "-DTLLM_ENABLE_CUDA",
+        ]
+        + sm100a_nvcc_flags,
+        extra_ldflags=["-lcuda"],
+    )
+
+
+@functools.cache
+def get_trtllm_gemm_module():
+    mod = trtllm_gemm_gen_module()
+    op = mod.build_and_load()
+    setup_cubin_loader(mod.get_library_path())
+    return op
 
 
 def gen_gemm_sm90_module() -> JitSpec:
@@ -944,7 +970,6 @@ def build_cudnn_gemm_with_per_tensor_q_graph(
 
     stream = torch.cuda.current_stream(device)
     with cudnn.graph(_get_cudnn_handle(stream)) as (graph, _):
-
         a_cudnn_tensor = graph.tensor(
             name="a", dim=a_shape, stride=a_stride, data_type=a_type
         )
@@ -1311,7 +1336,7 @@ def bmm_fp8(
         return _cudnn_gemm_fp8(A, B, A_scale * B_scale, out, dtype)
     elif backend == "cublas":
         workspace_buffer = _get_cache_buf(
-            "bmm_fp8_workspace", 32 * 1024 * 1024, A.device
+            "bmm_fp8_workspace", DEFAULT_WORKSPACE_SIZE, A.device
         )
         get_gemm_module().bmm_fp8(workspace_buffer, A, B, out, A_scale, B_scale)
     return out
@@ -1327,6 +1352,7 @@ def gemm_fp8_nt_groupwise(
     scale_granularity_mnk: Tuple[int, int, int] = (1, 128, 128),
     out: Optional[torch.Tensor] = None,
     out_dtype: Optional[torch.dtype] = None,
+    backend: Literal["cutlass", "trtllm"] = "cutlass",
 ) -> torch.Tensor:
     r"""Performs matrix multiplication with FP8 data types using groupwise scaling.
 
@@ -1369,6 +1395,9 @@ def gemm_fp8_nt_groupwise(
         If out is not specified, we will create an output tensor with this dtype.
         Defaults to ``torch.bfloat16``.
 
+    backend: Literal["cutlass", "trtllm"]
+        The backend to use for the operation. Defaults to ``"cutlass"``.
+
     Returns
     -------
     out: torch.Tensor
@@ -1379,7 +1408,7 @@ def gemm_fp8_nt_groupwise(
     The ``m`` should be padded to a multiple of 4 before calling this function, to accommodate the kernel's requirement.
     """
     workspace_buffer = _get_cache_buf(
-        "gemm_fp8_nt_groupwise_workspace", 32 * 1024 * 1024, a.device
+        "gemm_fp8_nt_groupwise_workspace", DEFAULT_WORKSPACE_SIZE, a.device
     )
     if a.ndim != 2 or b.ndim != 2:
         raise ValueError(f"Shape mismatch. a.shape = {a.shape}, b.shape = {b.shape}")
@@ -1410,17 +1439,31 @@ def gemm_fp8_nt_groupwise(
             dtype=out_dtype,
         )
 
-    get_gemm_sm100_module().gemm_fp8_nt_groupwise.default(
-        workspace_buffer,
-        a,
-        b,
-        a_scale,
-        b_scale,
-        out,
-        *scale_granularity_mnk,
-        scale_major_mode,
-        mma_sm,
-    )
+    if backend == "cutlass":
+        get_gemm_sm100_module().gemm_fp8_nt_groupwise.default(
+            workspace_buffer,
+            a,
+            b,
+            a_scale,
+            b_scale,
+            out,
+            *scale_granularity_mnk,
+            scale_major_mode,
+            mma_sm,
+        )
+    elif backend == "trtllm":
+        assert scale_granularity_mnk == (1, 128, 128)
+        assert scale_major_mode == "MN"
+        assert a.shape[1] >= 256
+        # mma_sm is ignored
+        get_trtllm_gemm_module().trtllm_gemm(
+            workspace_buffer,
+            a,
+            b,
+            a_scale.t(),
+            b_scale.t().contiguous().t(),
+            out,
+        )
 
     return out
 
@@ -1518,10 +1561,10 @@ def group_gemm_fp8_nt_groupwise(
     to accommodate the kernel's requirement.
     """
     int_workspace_buffer = _get_cache_buf(
-        "group_gemm_fp8_nt_groupwise_int_workspace", 32 * 1024 * 1024, a.device
+        "group_gemm_fp8_nt_groupwise_int_workspace", DEFAULT_WORKSPACE_SIZE, a.device
     )
     float_workspace_buffer = _get_cache_buf(
-        "group_gemm_fp8_nt_groupwise_float_workspace", 32 * 1024 * 1024, a.device
+        "group_gemm_fp8_nt_groupwise_float_workspace", DEFAULT_WORKSPACE_SIZE, a.device
     )
 
     assert a.dtype in [torch.float8_e4m3fn, torch.float8_e5m2]
@@ -1645,10 +1688,12 @@ def group_gemm_mxfp4_nt_groupwise(
     to accommodate the kernel's requirement.
     """
     int_workspace_buffer = _get_cache_buf(
-        "group_gemm_mxfp4_nt_groupwise_int_workspace", 32 * 1024 * 1024, a.device
+        "group_gemm_mxfp4_nt_groupwise_int_workspace", DEFAULT_WORKSPACE_SIZE, a.device
     )
     float_workspace_buffer = _get_cache_buf(
-        "group_gemm_mxfp4_nt_groupwise_float_workspace", 32 * 1024 * 1024, a.device
+        "group_gemm_mxfp4_nt_groupwise_float_workspace",
+        DEFAULT_WORKSPACE_SIZE,
+        a.device,
     )
 
     assert a.dtype in [torch.float8_e4m3fn, torch.float8_e5m2]
