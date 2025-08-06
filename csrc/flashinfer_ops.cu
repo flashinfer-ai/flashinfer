99acf4ddcf7fdd9bb934b1576d7be5117c323d0c
Shiyan Deng <dsy842974287@meta.com>
2025-08-06 16:57:20 -0700

    fix sig

--- csrc/flashinfer_ops.cu
+++ csrc/flashinfer_ops.cu
@@ -112,15 +112,15 @@ void BatchPrefillWithRaggedKVCacheRun(at::Tensor float_workspace_buffer,
                                       at::Tensor q, at::Tensor k, at::Tensor v,
                                       at::Tensor qo_indptr, at::Tensor kv_indptr, at::Tensor o,
                                       std::optional<at::Tensor> maybe_lse, int64_t mask_mode_code,
-                                      int64_t layout,
-                                      int64_t window_left BATCH_PREFILL_ADDITIONAL_FUNC_PARAMS);
+                                      int64_t layout, int64_t window_left,
+                                      bool enable_pdl BATCH_PREFILL_ADDITIONAL_FUNC_PARAMS);
 
 void BatchPrefillWithPagedKVCacheRun(
     at::Tensor float_workspace_buffer, at::Tensor int_workspace_buffer, at::Tensor plan_info_vec,
     at::Tensor q, at::Tensor paged_k_cache, at::Tensor paged_v_cache, at::Tensor qo_indptr,
     at::Tensor paged_kv_indptr, at::Tensor paged_kv_indices, at::Tensor paged_kv_last_page_len,
     at::Tensor o, std::optional<at::Tensor> maybe_lse, int64_t mask_mode_code, int64_t layout,
-    int64_t window_left BATCH_PREFILL_ADDITIONAL_FUNC_PARAMS);
+    int64_t window_left, bool enable_pdl BATCH_PREFILL_ADDITIONAL_FUNC_PARAMS);
 
 //========== pod-attention =========
 void pod_with_kv_cache_tensor(
