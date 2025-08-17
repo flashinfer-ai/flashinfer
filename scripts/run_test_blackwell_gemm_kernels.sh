59d0733e1413b6ba0aeb7ba05b73966b7a99fe3d
Avery Yingyi Huang <yingyihuang2000@outlook.com>
2025-08-17 14:36:06 -0400

    add to ci

--- scripts/run_test_blackwell_gemm_kernels.sh
+++ scripts/run_test_blackwell_gemm_kernels.sh
@@ -7,3 +7,4 @@ pytest -s tests/test_mm_fp4.py
 pytest -s tests/test_groupwise_scaled_gemm_fp8.py
 pytest -s tests/test_groupwise_scaled_gemm_mxfp4.py
 pytest -s tests/test_cute_dsl_blockscaled_gemm.py
+pytest -s tests/test_cute_dsl_blockscaled_gemm.py
