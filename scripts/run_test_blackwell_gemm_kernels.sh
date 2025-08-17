2e278c1edb454725b13c7b1201a6e7b94dfe9fc2
Avery Yingyi Huang <yingyihuang2000@outlook.com>
2025-08-17 14:39:07 -0400

    Revert "add to ci"
    This reverts commit 59d0733e1413b6ba0aeb7ba05b73966b7a99fe3d.

--- scripts/run_test_blackwell_gemm_kernels.sh
+++ scripts/run_test_blackwell_gemm_kernels.sh
@@ -7,4 +7,3 @@ pytest -s tests/test_mm_fp4.py
 pytest -s tests/test_groupwise_scaled_gemm_fp8.py
 pytest -s tests/test_groupwise_scaled_gemm_mxfp4.py
 pytest -s tests/test_cute_dsl_blockscaled_gemm.py
-pytest -s tests/test_cute_dsl_blockscaled_gemm.py
