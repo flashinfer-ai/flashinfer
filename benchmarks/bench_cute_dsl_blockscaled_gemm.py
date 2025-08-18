3b72d21064189fa6bb66bada633dab4e029e486d
fzyzcjy <ch271828n@outlook.com>
2025-08-18 10:29:01 +0800

    more

--- benchmarks/bench_cute_dsl_blockscaled_gemm.py
+++ benchmarks/bench_cute_dsl_blockscaled_gemm.py
@@ -6,9 +6,10 @@ def enumerate_m_grouped_masked():
     max_m = 4096
     for num_groups, m in (
         # GB200 cases
-        (6, 1024),
+        (6, 1024), (6, 512),
         # DeepGEMM default cases
         (1, 1024), (2, 512), (4, 256),
+        # TODO scan more cases
     ):
         for n, k in ((4096, 7168), (7168, 2048),):
             yield dict(num_groups=num_groups, max_m=max_m, m=m, n=n, k=k)
