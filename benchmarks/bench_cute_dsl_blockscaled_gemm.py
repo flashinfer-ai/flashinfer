7414fb623f416330d58be41de71d463219858e14
fzyzcjy <ch271828n@outlook.com>
2025-08-18 10:27:11 +0800

    more

--- benchmarks/bench_cute_dsl_blockscaled_gemm.py
+++ benchmarks/bench_cute_dsl_blockscaled_gemm.py
@@ -1,10 +1,19 @@
 import torch
 
 
-def bench_one():
+# ref: DeepGEMM
+def enumerate_m_grouped_masked():
+    max_m = 4096
+    for num_groups, m in ((1, 1024), (2, 512), (4, 256)):
+        for n, k in ((4096, 7168), (7168, 2048),):
+            yield dict(num_groups=num_groups, max_m=max_m, m=m, n=n, k=k)
+
+
+def bench_one(num_groups, max_m, m, n, k):
     TODO
+
 
 if __name__ == "__main__":
     torch.manual_seed(42)
-    for TODO in TODO:
-        bench_one()
+    for config in enumerate_m_grouped_masked():
+        bench_one(**config)
