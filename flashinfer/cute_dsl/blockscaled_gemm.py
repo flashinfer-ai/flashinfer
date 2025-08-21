bf179e4b7565c0cbf6af71cdc9e51abb370829de
fzyzcjy <ch271828n@outlook.com>
2025-08-21 13:22:01 +0800

    ci

--- flashinfer/cute_dsl/blockscaled_gemm.py
+++ flashinfer/cute_dsl/blockscaled_gemm.py
@@ -26,7 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from typing import Optional, Tuple, Type, Union, Any, Dict
+from typing import Optional, Tuple, Type, Union, Any
 
 import cuda.bindings.driver as cuda
 import cutlass
