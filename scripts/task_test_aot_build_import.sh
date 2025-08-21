2973f55c5bbb77f9f42ac8ccadf4313d8cfcd662
Yong Wu <yowu@nvidia.com>
2025-08-20 21:17:34 -0700

    test: Enable all modules in AOT build test

--- scripts/task_test_aot_build_import.sh
+++ scripts/task_test_aot_build_import.sh
@@ -4,10 +4,10 @@ set -eo pipefail
 set -x
 : ${MAX_JOBS:=$(nproc)}
 : ${CUDA_VISIBLE_DEVICES:=""}
-export TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0+PTX"
+export TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0+PTX 10.0 12.0"
 
 python -c "import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI)"
-python -m flashinfer.aot
+python -m flashinfer.aot --add-comm True --add-moe True
 python -m build --wheel
 pip install dist/*.whl
 
