64a3a7967b6baf92f0e76ceef4435ae19bf0e5fb
Yong Wu <yowu@nvidia.com>
2025-08-20 21:41:47 -0700

    upd

--- scripts/task_test_aot_build_import.sh
+++ scripts/task_test_aot_build_import.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 set -x
 : ${MAX_JOBS:=$(nproc)}
 : ${CUDA_VISIBLE_DEVICES:=""}
-export TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0+PTX 10.0 12.0"
+export TORCH_CUDA_ARCH_LIST="7.5 8.0 8.9 9.0+PTX 10.0+PTX 12.0+PTX"
 
 python -c "import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI)"
 python -m flashinfer.aot --add-comm True --add-moe True
