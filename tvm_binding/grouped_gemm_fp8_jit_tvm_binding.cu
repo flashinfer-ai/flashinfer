49a1dd1ae700d04c4377655d022cdfd6aba51033
Anrui Liu <anruil@catalyst-fleet1.cs.cmu.edu>
2025-08-26 15:29:55 -0400

    add flashinfer-jit with tvm_binding, resolve conflicts
    resolve conflict

---
+++ tvm_binding/grouped_gemm_fp8_jit_tvm_binding.cu
@@ -0,0 +1,25 @@
+#include "tvm_binding_utils.h"
+
+// Function declarations (implementations in grouped_gemm.cu)
+IntTuple GroupedGemmGetWorkspaceSize(int64_t batch_size, int64_t max_m, int64_t max_n, int64_t max_k);
+
+int GroupedGemmFp8Run(
+    DLTensor* int_workspace_buffer,     
+    DLTensor* float_workspace_buffer,   
+    DLTensor* A,                        
+    DLTensor* B,                        
+    DLTensor* SFA,                      
+    DLTensor* SFB,                     
+    DLTensor* D,                        
+    DLTensor* m_indptr,                 
+    int64_t n, int64_t k,               
+    int64_t scale_granularity_m,       
+    int64_t scale_granularity_n,     
+    int64_t scale_granularity_k,        
+    int64_t scale_major_mode,           
+    int64_t mma_sm,                  
+    TVMStreamHandle cuda_stream         
+);
+
+// TVM_FFI_DLL_EXPORT_TYPED_FUNC(grouped_gemm_fp8_run, GroupedGemmFp8Run);
+// TVM_FFI_DLL_EXPORT_TYPED_FUNC(grouped_gemm_fp16_fp16_fp16_run, GroupedGemmFp16Fp16Fp16Run);
