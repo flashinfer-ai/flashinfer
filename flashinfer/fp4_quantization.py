2400e99ec23d17787fe56e62474e4ee86dee76e4
Yong Wu <yowu@nvidia.com>
2025-08-30 02:26:23 +0000

    Feature: Bringup SM120 & SM121

--- flashinfer/fp4_quantization.py
+++ flashinfer/fp4_quantization.py
@@ -23,7 +23,15 @@ import torch
 
 from .jit import JitSpec
 from .jit import env as jit_env
-from .jit import gen_jit_spec, sm100a_nvcc_flags, sm90a_nvcc_flags
+from .jit import (
+    gen_jit_spec,
+    sm120a_nvcc_flags,
+    sm121a_nvcc_flags,
+    sm110a_nvcc_flags,
+    sm103a_nvcc_flags,
+    sm100a_nvcc_flags,
+    sm90a_nvcc_flags,
+)
 from .jit.cpp_ext import is_cuda_version_at_least
 from .utils import (
     device_support_pdl,
@@ -71,6 +79,18 @@ def gen_fp4_quantization_sm90_module() -> JitSpec:
     return gen_fp4_quantization_module(sm90a_nvcc_flags, "90")
 
 
+def gen_fp4_quantization_sm110_module() -> JitSpec:
+    return gen_fp4_quantization_module(sm110a_nvcc_flags, "110")
+
+
+def gen_fp4_quantization_sm120_module() -> JitSpec:
+    return gen_fp4_quantization_module(sm120a_nvcc_flags, "120")
+
+
+def gen_fp4_quantization_sm121_module() -> JitSpec:
+    return gen_fp4_quantization_module(sm121a_nvcc_flags, "121")
+
+
 def gen_fp4_quantization_module(nvcc_flags: List[str], device_arch: str) -> JitSpec:
     return gen_jit_spec(
         f"fp4_quantization_{device_arch}",
@@ -104,7 +124,13 @@ def gen_fp4_quantization_module(nvcc_flags: List[str], device_arch: str) -> JitS
 
 @functools.cache
 def get_fp4_quantization_module(backend: str = "100"):
-    if backend == "100":
+    if backend == "121":
+        module = gen_fp4_quantization_sm121_module().build_and_load()
+    elif backend == "120":
+        module = gen_fp4_quantization_sm120_module().build_and_load()
+    elif backend == "110":
+        module = gen_fp4_quantization_sm110_module().build_and_load()
+    elif backend == "100":
         module = gen_fp4_quantization_sm100_module().build_and_load()
     elif backend == "90":
         module = gen_fp4_quantization_sm90_module().build_and_load()
