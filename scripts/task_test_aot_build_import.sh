2946e0454b300265bed2e9cb0982c0daf21098d4
Yong Wu <yowu@nvidia.com>
2025-08-31 08:03:51 -0700

    ci: limit aot parallel build jobs based on available memory (#1612)

--- scripts/task_test_aot_build_import.sh
+++ scripts/task_test_aot_build_import.sh
@@ -2,7 +2,17 @@
 
 set -eo pipefail
 set -x
-: ${MAX_JOBS:=$(nproc)}
+
+# MAX_JOBS = min(nproc, max(1, MemAvailable_GB/4))
+MEM_AVAILABLE_GB=$(free -g | awk '/^Mem:/ {print $7}')
+NPROC=$(nproc)
+MAX_JOBS=$(( MEM_AVAILABLE_GB / 4 ))
+if (( MAX_JOBS < 1 )); then
+  MAX_JOBS=1
+elif (( NPROC < MAX_JOBS )); then
+  MAX_JOBS=$NPROC
+fi
+
 : ${CUDA_VISIBLE_DEVICES:=""}
 export TORCH_CUDA_ARCH_LIST=$(python3 -c '
 import torch
