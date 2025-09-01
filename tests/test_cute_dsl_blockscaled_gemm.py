6efaa24973d906f09160dc9401ddfe6ba42640da
fzyzcjy <ch271828n@outlook.com>
2025-09-01 11:15:21 +0800

    more

--- tests/test_cute_dsl_blockscaled_gemm.py
+++ tests/test_cute_dsl_blockscaled_gemm.py
@@ -198,7 +198,9 @@ def test_blockscaled_gemm_python_interface(
         )
 
         if enable_dst_signals:
-            assert torch.all(dst_signals == sm_count), f"{dst_signals=} {masked_m_tensor=}"
+            expect_dst_signals = torch.zeros_like(dst_signals)
+            expect_dst_signals[:ceil_div(masked_m_tensor, mma_tiler_mn[0]), :] = ceil_div(k, mma_tiler_mn[1])
+            assert torch.all(dst_signals == expect_dst_signals), f"{dst_signals=} {masked_m_tensor=}"
 
     # compute ref output
     if not fuse_alpha:
