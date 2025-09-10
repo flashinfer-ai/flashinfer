fd647484611c391331fa4031a4e99526d7b6da7b
fzyzcjy <ch271828n@outlook.com>
2025-09-10 10:02:41 +0800

    more

--- benchmarks/bench_cutlass_fused_moe.py
+++ benchmarks/bench_cutlass_fused_moe.py
@@ -39,8 +39,8 @@ test_configs = [
     # },
     {
         "hidden_size": 7168,
-        # "num_experts": 288,  # 1gpu
-        "num_experts": 288 // 4,  # 4gpu
+        # "num_experts": 256,  # 1gpu
+        "num_experts": 256 // 4,  # 4gpu
         "top_k": 8,
         "intermediate_size": 2048,
     },
