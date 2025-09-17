74a9d9a62dfe6462adc121ee68cce246ef3200e7
Maximilien Breughe <50598321+nvmbreughe@users.noreply.github.com>
2025-09-17 18:53:11 -0500

    CI: Updated test lists and addressed some failing tests (#1708)
    <!-- .github/pull_request_template.md -->
    
    ## 📌 Description
    * The testslists are updated to include everything under tests/
    * Note that this is part of a 2 phase approach. Next MR will reorganize
    the test directory, get rid of the test lists and just run everything
    under tests/.
    * Test fixes:
      * Update to Dockerfile for failing Triton tests
      * skips Hopper directed tests when not running on Hopper
    
    <!-- What does this PR do? Briefly describe the changes and why they’re
    needed. -->
    
    ## 🔍 Related Issues
    
    <!-- Link any related issues here -->
    
    ## 🚀 Pull Request Checklist
    
    Thank you for contributing to FlashInfer! Before we review your pull
    request, please make sure the following items are complete.
    
    ### ✅ Pre-commit Checks
    
    - [x] I have installed `pre-commit` by running `pip install pre-commit`
    (or used your preferred method).
    - [x] I have installed the hooks with `pre-commit install`.
    - [x] I have run the hooks manually with `pre-commit run --all-files`
    and fixed any reported issues.
    
    > If you are unsure about how to set up `pre-commit`, see [the
    pre-commit documentation](https://pre-commit.com/).
    
    ## 🧪 Tests
    
    - [x] Tests have been added or updated as needed.
    - [ ] All tests are passing (`unittest`, etc.).
    
    ## Reviewer Notes
    
    <!-- Optional: anything you'd like reviewers to focus on, concerns, etc.
    -->

--- scripts/run_test_blackwell_attention_kernels.sh
+++ scripts/run_test_blackwell_attention_kernels.sh
@@ -3,17 +3,47 @@
 set -eo pipefail
 set -x
 : ${JUNIT_DIR:=$(realpath ./junit)}
+: ${MAX_JOBS:=$(nproc)}
+: ${CUDA_VISIBLE_DEVICES:=0}
+
+pip install -e . -v
+pip install --upgrade nvidia-cudnn-cu12
+pip install --upgrade cuda-python==12.*
 
 EXIT_CODE=0
 
 test_scripts=(
-  "test_blackwell_fmha.py"
-  "test_deepseek_mla.py"
-  "test_trtllm_gen_attention.py"
-  "test_trtllm_gen_mla.py"
-  "test_cudnn_decode.py"
-  "test_cudnn_prefill.py"
-  "test_cudnn_prefill_deepseek.py"
+    "test_batch_decode_kernels.py"
+    "test_batch_prefill_kernels.py"
+    "test_batch_prefill.py"
+    "test_cudnn_decode.py"
+    "test_cudnn_prefill_deepseek.py"
+    "test_cudnn_prefill.py"
+    "test_decode_fp8_calibration_scale.py"
+    "test_decode_prefill_lse.py"
+    "test_fp8_prefill.py"
+    "test_mla_decode_kernel.py"
+    "test_non_contiguous_decode.py"
+    "test_non_contiguous_prefill.py"
+    "test_single_prefill.py"
+    "test_tensor_cores_decode.py"
+    "test_trtllm_gen_attention.py"
+    "test_alibi.py"
+    "test_attention_sink_blackwell.py"
+    "test_attention_sink.py"
+    "test_batch_attention.py"
+    "test_hopper_fp8_attention.py"
+    "test_blackwell_fmha.py"
+    "test_deepseek_mla.py"
+    "test_mla_page.py"
+    "test_page.py"
+    "test_rope.py"
+    "test_sliding_window.py"
+    "test_trtllm_gen_mla.py"
+    "test_hopper.py"
+    "test_xqa.py"
+    "test_shared_prefix_kernels.py"
+    "test_logits_cap.py"
 )
 
 for test_file in "${test_scripts[@]}"; do
--- scripts/run_test_blackwell_moe_kernels.sh
+++ scripts/run_test_blackwell_moe_kernels.sh
@@ -3,6 +3,12 @@
 set -eo pipefail
 set -x
 : ${JUNIT_DIR:=$(realpath ./junit)}
+: ${MAX_JOBS:=$(nproc)}
+: ${CUDA_VISIBLE_DEVICES:=0}
+
+pip install -e . -v
+pip install --upgrade nvidia-cudnn-cu12
+pip install --upgrade cuda-python==12.*
 
 EXIT_CODE=0
 
