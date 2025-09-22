58cd2d3933dee130da9344110a2dc06eba8510ec
Avery Yingyi Huang <yingyihuang2000@outlook.com>
2025-09-21 23:12:34 -0400

    tmp

--- tests/test_fast_plan_decode.py
+++ tests/test_fast_plan_decode.py
@@ -21,21 +21,21 @@ import flashinfer
 
 MAX_BATCH_SIZE = 128
 
-global_override_indptr_cpu = None
-
 
 @pytest.mark.parametrize("batch_size", [12, 17, 128])
-@pytest.mark.parametrize("kv_len", [54, 2048, 16384])
+@pytest.mark.parametrize("kv_len", [54, 97, 512, 2048, 16384])
 @pytest.mark.parametrize("page_size", [1, 8, 16])
 @pytest.mark.parametrize("num_kv_heads", [4])
 @pytest.mark.parametrize("num_qo_heads", [4, 32])
 @pytest.mark.parametrize("head_dim", [128, 256])
 @pytest.mark.parametrize("kv_layout", ["NHD"])
 @pytest.mark.parametrize("pos_encoding_mode", ["NONE", "ROPE_LLAMA"])
+@pytest.mark.parametrize("logits_soft_cap", [0.0])
+@pytest.mark.parametrize("return_lse", [True])
 @pytest.mark.parametrize("q_dtype", [torch.float16])
 @pytest.mark.parametrize("kv_dtype", [torch.float16, torch.float8_e4m3fn])
 @pytest.mark.parametrize("contiguous_kv", [True])
-def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
+def test_batch_decode_with_paged_kv_cache_with_fast_plan(
     batch_size,
     kv_len,
     page_size,
@@ -44,15 +44,17 @@ def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
     head_dim,
     kv_layout,
     pos_encoding_mode,
+    logits_soft_cap,
+    return_lse,
     q_dtype,
     kv_dtype,
     contiguous_kv,
 ):
-    global global_override_indptr_cpu
-
     q = torch.randn(batch_size, num_qo_heads, head_dim, device="cuda:0", dtype=q_dtype)
     num_pages_per_seq = (kv_len + page_size - 1) // page_size
+    total_num_pages = num_pages_per_seq * batch_size
 
+    global global_override_indptr_cpu
     if global_override_indptr_cpu is None:
         global_override_indptr_cpu = torch.empty(MAX_BATCH_SIZE + 1, device="cpu")
 
@@ -60,7 +62,6 @@ def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
         torch.arange(0, batch_size + 1).int() * num_pages_per_seq
     )
 
-    total_num_pages = num_pages_per_seq * batch_size
     if kv_layout == "HND":
         kv_shape = [total_num_pages, 2, num_kv_heads, page_size, head_dim]
     else:
@@ -73,8 +74,8 @@ def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
         kv_shape = tmp
         kv_data_fp32 = torch.randn(*kv_shape, dtype=torch.float32, device="cuda:0")
         kv_data = kv_data_fp32.to(kv_dtype)
-        kv_data_fp32 = kv_data_fp32[:, 1, :, 1, :, 1, :, 1, :]
         kv_data = kv_data[:, 1, :, 1, :, 1, :, 1, :]
+        kv_data_fp32 = kv_data_fp32[:, 1, :, 1, :, 1, :, 1, :]
         # actual data is stored in non-contiguous memory
         assert (
             kv_data.stride(-4)
@@ -83,122 +84,52 @@ def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
     else:
         kv_data_fp32 = torch.randn(*kv_shape, dtype=torch.float32, device="cuda:0")
         kv_data = kv_data_fp32.to(kv_dtype)
-    kv_indptr_host_warmup = torch.arange(
-        0, batch_size + 1, device="cuda:0", dtype=torch.int32
+    kv_indptr = (
+        torch.arange(0, batch_size + 1, device="cuda:0", dtype=torch.int32)
+        * num_pages_per_seq
     )
-    kv_indices_host_warmup = torch.arange(
-        0, batch_size, device="cuda:0", dtype=torch.int32
-    )
-    kv_last_page_len_host_warmup = torch.full(
-        (batch_size,), page_size, dtype=torch.int32
-    )
-
-    # NOTE(Zihao): allocate more space than needed for testing
-    kv_indptr_device_buffer = torch.empty(
-        batch_size + 1, device="cuda:0", dtype=torch.int32
-    )
-    kv_indices_device_buffer = torch.empty(
-        total_num_pages, device="cuda:0", dtype=torch.int32
-    )
-    kv_last_page_device_buffer = torch.empty(
-        batch_size, device="cuda:0", dtype=torch.int32
+    kv_indices = torch.arange(0, total_num_pages, device="cuda:0", dtype=torch.int32)
+    kv_last_page_len = torch.full(
+        (batch_size,), (kv_len - 1) % page_size + 1, dtype=torch.int32, device="cuda:0"
     )
 
-    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.int8, device="cuda:0")
-    wrapper = flashinfer.decode.CUDAGraphBatchDecodeWithPagedKVCacheWrapper(
-        workspace_buffer,
-        kv_indptr_device_buffer,
-        kv_indices_device_buffer,
-        kv_last_page_device_buffer,
-        kv_layout,
+    workspace_buffer = torch.empty(32 * 1024 * 1024, dtype=torch.int8, device="cuda:0")
+    wrapper = flashinfer.decode.BatchDecodeWithPagedKVCacheWrapper(
+        workspace_buffer, kv_layout
     )
-
-    # First call regular plan to initialize _cached_module
     wrapper.plan(
-        kv_indptr_host_warmup,
-        kv_indices_host_warmup,
-        kv_last_page_len_host_warmup,
+        kv_indptr,
+        kv_indices,
+        kv_last_page_len,
         num_qo_heads,
         num_kv_heads,
         head_dim,
         page_size,
-        data_type=kv_dtype,
+        logits_soft_cap=logits_soft_cap,
         pos_encoding_mode=pos_encoding_mode,
+        data_type=kv_dtype,
         q_data_type=q_dtype,
     )
-
-    # Then replace plan with fast_decode_plan for subsequent calls
-    wrapper.plan = partial(flashinfer.decode.fast_decode_plan, wrapper)
-
+    wrapper.plan = partial(flashinfer.fast_decode_plan, wrapper)
     wrapper.plan(
-        kv_indptr_host_warmup,
-        kv_indices_host_warmup,
-        kv_last_page_len_host_warmup,
+        kv_indptr,
+        kv_indices,
+        kv_last_page_len,
         num_qo_heads,
         num_kv_heads,
         head_dim,
         page_size,
-        data_type=kv_dtype,
+        logits_soft_cap=logits_soft_cap,
         pos_encoding_mode=pos_encoding_mode,
+        data_type=kv_dtype,
         q_data_type=q_dtype,
+        non_blocking=True,
     )
-    # warmup
-    s = torch.cuda.Stream()
-    s.wait_stream(torch.cuda.current_stream())
-    with torch.cuda.stream(s):
-        for _ in range(3):
-            o = wrapper.run(q, kv_data)
-    torch.cuda.current_stream().wait_stream(s)
-
-    # capture
-    g = torch.cuda.CUDAGraph()
-    with torch.cuda.graph(g):
+    if return_lse:
+        o, _ = wrapper.run(q, kv_data, return_lse=True)
+    else:
         o = wrapper.run(q, kv_data)
 
-    # replay multiple times
-    for i in range(1, min(4, num_pages_per_seq)):
-        kv_indptr_host = torch.arange(0, batch_size + 1).int() * i
-        kv_indices_host = torch.arange(0, i * batch_size).int()
-        kv_last_page_len_host = torch.full((batch_size,), page_size, dtype=torch.int32)
-
-        wrapper.plan(
-            kv_indptr_host,
-            kv_indices_host,
-            kv_last_page_len_host,
-            num_qo_heads,
-            num_kv_heads,
-            head_dim,
-            page_size,
-            data_type=kv_dtype,
-            pos_encoding_mode=pos_encoding_mode,
-            q_data_type=q_dtype,
-        )
-        g.replay()
-
-    # replay again
-    kv_indptr_host = torch.arange(0, batch_size + 1).int() * num_pages_per_seq
-    kv_indices_host = torch.arange(0, total_num_pages).int()
-    kv_last_page_len_host = torch.full(
-        (batch_size,), (kv_len - 1) % page_size + 1, dtype=torch.int32
-    )
-
-    wrapper.plan(
-        kv_indptr_host,
-        kv_indices_host,
-        kv_last_page_len_host,
-        num_qo_heads,
-        num_kv_heads,
-        head_dim,
-        page_size,
-        data_type=kv_dtype,
-        pos_encoding_mode=pos_encoding_mode,
-        q_data_type=q_dtype,
-    )
-    g.replay()
-
-    # compute ground truth and compare
-    kv_indptr = kv_indptr_host.to(0)
-    kv_last_page_len = kv_last_page_len_host.to(0)
     for i in range(batch_size):
         perm_dims = [0, 2, 1, 3] if kv_layout == "HND" else [0, 1, 2, 3]
         perm_dims_last = [1, 0, 2] if kv_layout == "HND" else [0, 1, 2]
@@ -234,12 +165,33 @@ def test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
             dim=0,
         ).to(kv_dtype)
         o_ref_i = flashinfer.decode.single_decode_with_kv_cache(
-            qi, ki, vi, pos_encoding_mode=pos_encoding_mode
+            qi,
+            ki,
+            vi,
+            pos_encoding_mode=pos_encoding_mode,
+            logits_soft_cap=logits_soft_cap,
         )
         torch.testing.assert_close(o[i], o_ref_i, rtol=1e-3, atol=1e-3)
 
+    # test user-allocated output
+    o_buffer = torch.empty_like(o)
+    wrapper.run(q, kv_data, out=o_buffer)
+    torch.testing.assert_close(o, o_buffer, rtol=1e-3, atol=1e-3)
+
 
 if __name__ == "__main__":
-    test_cuda_graph_batch_fast_decode_with_paged_kv_cache(
-        12, 54, 8, 8, 8, 128, "HND", "NONE", torch.float16, torch.float8_e5m2, True
+    test_batch_decode_with_paged_kv_cache_with_fast_plan(
+        12,
+        54,
+        8,
+        4,
+        4,
+        128,
+        "NHD",
+        "NONE",
+        0.0,
+        True,
+        torch.float16,
+        torch.float8_e4m3fn,
+        True,
     )
