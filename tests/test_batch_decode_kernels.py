f7d55709361a96d45667eb4ec400fbb1e8a31e1a
Yaxing Cai <caiyaxing666@gmail.com>
2025-09-23 05:11:12 +0000

    fix

--- tests/test_batch_decode_kernels.py
+++ tests/test_batch_decode_kernels.py
@@ -22,35 +22,35 @@ import flashinfer
 from tvm_ffi import use_torch_stream
 
 
-@pytest.fixture(autouse=True, scope="module")
-def warmup_jit():
-    flashinfer.jit.build_jit_specs(
-        gen_decode_attention_modules(
-            [torch.float16],  # q_dtypes
-            [
-                torch.float16,
-                torch.float8_e4m3fn,
-            ],  # kv_dtypes
-            [128, 256],  # head_dims
-            [0, 1],  # pos_encoding_modes
-            [False],  # use_sliding_windows
-            [False],  # use_logits_soft_caps
-        )
-        + gen_prefill_attention_modules(
-            [torch.float16],  # q_dtypes
-            [
-                torch.float16,
-                torch.float8_e4m3fn,
-            ],  # kv_dtypes
-            [128, 256],  # head_dims
-            [0, 1],  # pos_encoding_modes
-            [False],  # use_sliding_windows
-            [False],  # use_logits_soft_caps
-            [False],  # use_fp16_qk_reductions
-        ),
-        verbose=False,
-    )
-    yield
+# @pytest.fixture(autouse=True, scope="module")
+# def warmup_jit():
+#     flashinfer.jit.build_jit_specs(
+#         gen_decode_attention_modules(
+#             [torch.float16],  # q_dtypes
+#             [
+#                 torch.float16,
+#                 torch.float8_e4m3fn,
+#             ],  # kv_dtypes
+#             [128, 256],  # head_dims
+#             [0, 1],  # pos_encoding_modes
+#             [False],  # use_sliding_windows
+#             [False],  # use_logits_soft_caps
+#         )
+#         + gen_prefill_attention_modules(
+#             [torch.float16],  # q_dtypes
+#             [
+#                 torch.float16,
+#                 torch.float8_e4m3fn,
+#             ],  # kv_dtypes
+#             [128, 256],  # head_dims
+#             [0, 1],  # pos_encoding_modes
+#             [False],  # use_sliding_windows
+#             [False],  # use_logits_soft_caps
+#             [False],  # use_fp16_qk_reductions
+#         ),
+#         verbose=False,
+#     )
+#     yield
 
 
 @pytest.mark.parametrize("batch_size", [12, 17, 128])
