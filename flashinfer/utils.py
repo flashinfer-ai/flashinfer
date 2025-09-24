2ea52205006023980e927e09e09871d31b641f97
Maximilien Breughe <mbreughe@nvidia.com>
2025-09-24 15:46:08 -0700

    Simplified new exceptions

--- flashinfer/utils.py
+++ flashinfer/utils.py
@@ -52,27 +52,15 @@ log2e = 1.44269504088896340736
 
 
 class GPUArchitectureError(Exception):
-    def __init__(self, msg: str):
-        self.msg = msg
-        super().__init__(self.msg)
+    """Custom exception for GPU architecture-related errors."""
 
-    def __str__(self):
-        return self.msg
-
-    def __repr__(self):
-        return self.msg
+    pass
 
 
 class LibraryError(Exception):
-    def __init__(self, msg: str):
-        self.msg = msg
-        super().__init__(self.msg)
+    """Custom exception for library-related errors."""
 
-    def __str__(self):
-        return self.msg
-
-    def __repr__(self):
-        return self.msg
+    pass
 
 
 def _expand_5d(x: torch.Tensor, kv_layout: str) -> torch.Tensor:
