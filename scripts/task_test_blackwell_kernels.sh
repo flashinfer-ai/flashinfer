8e4bf25319591d04aacde604443bd8865da855a8
Maximilien Breughe <mbreughe@nvidia.com>
2025-09-23 20:03:28 -0700

    Script update

--- scripts/task_test_blackwell_kernels.sh
+++ scripts/task_test_blackwell_kernels.sh
@@ -6,18 +6,7 @@ set -x
 
 
 EXIT_CODE=0
-scripts_to_run=(
-  "run_test_blackwell_utils_kernels.sh"
-  "run_test_blackwell_attention_kernels.sh"
-  "run_test_blackwell_gemm_kernels.sh"
-  "run_test_blackwell_moe_kernels.sh"
-  "run_test_blackwell_comm_kernels.sh"
-)
-for script in "${scripts_to_run[@]}"; do
-  bash "scripts/$script" || EXIT_CODE=1
-  if [[ -z "${RUN_TO_COMPLETION}" && $EXIT_CODE -ne 0 ]]; then
-    exit $EXIT_CODE
-  fi
-done
+
+pytest tests/ || EXIT_CODE=1
 
 exit $EXIT_CODE
