9989453ce71138e7cd55785b03f01f7b8e2adbec
Yaxing Cai <caiyaxing666@gmail.com>
2025-09-25 18:28:09 +0000

    fix

--- csrc/batch_prefill_fp8_sm90.cu
+++ csrc/batch_prefill_fp8_sm90.cu
@@ -37,11 +37,11 @@ using tvm::ffi::Array;
 using tvm::ffi::Optional;
 
 Array<int64_t> BatchPrefillWithKVCacheSM90Plan(
-    Tensor float_workspace_buffer, Tensor int_workspace_buffer,
-    Tensor page_locked_int_workspace_buffer, Tensor qo_indptr, Tensor kv_indptr, Tensor kv_len_arr,
-    int64_t total_num_rows, int64_t batch_size, int64_t num_qo_heads, int64_t num_kv_heads,
-    int64_t page_size, bool enable_cuda_graph, int64_t head_dim_qk, int64_t head_dim_vo,
-    bool causal, int64_t window_left) {
+    ffi::Tensor float_workspace_buffer, ffi::Tensor int_workspace_buffer,
+    ffi::Tensor page_locked_int_workspace_buffer, ffi::Tensor qo_indptr, ffi::Tensor kv_indptr,
+    ffi::Tensor kv_len_arr, int64_t total_num_rows, int64_t batch_size, int64_t num_qo_heads,
+    int64_t num_kv_heads, int64_t page_size, bool enable_cuda_graph, int64_t head_dim_qk,
+    int64_t head_dim_vo, bool causal, int64_t window_left) {
   size_t float_workspace_size_in_bytes =
       float_workspace_buffer->shape[0] * get_element_size(float_workspace_buffer);
   size_t int_workspace_size_in_bytes =
@@ -66,30 +66,32 @@ Array<int64_t> BatchPrefillWithKVCacheSM90Plan(
   return Array(plan_info.ToVector());
 }
 
-void BatchPrefillWithRaggedKVCacheSM90Run(Tensor float_workspace_buffer,
-                                          Tensor int_workspace_buffer, Array<int64_t> plan_info_vec,
-                                          Tensor q, Tensor k, Tensor v, Tensor qo_indptr,
-                                          Tensor kv_indptr, Tensor o, Optional<Tensor> maybe_lse,
-                                          int64_t mask_mode_code, int64_t layout,
-                                          int64_t window_left,
+void BatchPrefillWithRaggedKVCacheSM90Run(ffi::Tensor float_workspace_buffer,
+                                          ffi::Tensor int_workspace_buffer,
+                                          Array<int64_t> plan_info_vec, ffi::Tensor q,
+                                          ffi::Tensor k, ffi::Tensor v, ffi::Tensor qo_indptr,
+                                          ffi::Tensor kv_indptr, ffi::Tensor o,
+                                          Optional<ffi::Tensor> maybe_lse, int64_t mask_mode_code,
+                                          int64_t layout, int64_t window_left,
                                           bool enable_pdl  // placeholder
                                               ADDITIONAL_FUNC_PARAMS) {
   return;  // TODO: Implement this function
 }
 
 void BatchPrefillWithPagedKVCacheSM90Run(
-    Tensor float_workspace_buffer, Tensor int_workspace_buffer, Array<int64_t> plan_info_vec,
-    Tensor q, Tensor paged_k_cache, Tensor paged_v_cache, Tensor qo_indptr, Tensor paged_kv_indptr,
-    Tensor paged_kv_indices, Tensor paged_kv_last_page_len, Tensor o, Optional<Tensor> maybe_lse,
-    int64_t mask_mode_code, int64_t layout, int64_t window_left,
+    ffi::Tensor float_workspace_buffer, ffi::Tensor int_workspace_buffer,
+    Array<int64_t> plan_info_vec, ffi::Tensor q, ffi::Tensor paged_k_cache,
+    ffi::Tensor paged_v_cache, ffi::Tensor qo_indptr, ffi::Tensor paged_kv_indptr,
+    ffi::Tensor paged_kv_indices, ffi::Tensor paged_kv_last_page_len, ffi::Tensor o,
+    Optional<ffi::Tensor> maybe_lse, int64_t mask_mode_code, int64_t layout, int64_t window_left,
     bool enable_pdl ADDITIONAL_FUNC_PARAMS) {
   PrefillPlanSM90Info plan_info;
   plan_info.FromVector(std::vector<int64_t>(plan_info_vec.begin(), plan_info_vec.end()));
 
-  if (maybe_lse) {
-    const auto& lse = *maybe_lse;
-    TORCH_CHECK(lse->shape[0] == q->shape[0], lse->shape[0], q->shape[0]);
-    TORCH_CHECK(lse->shape[1] == q->shape[1], lse->shape[1], q->shape[1]);
+  if (maybe_lse.has_value()) {
+    const auto& lse = maybe_lse.value();
+    TVM_FFI_ICHECK_EQ(lse->shape[0], q->shape[0]);
+    TVM_FFI_ICHECK_EQ(lse->shape[1], q->shape[1]);
   }
   QKVLayout kv_layout = static_cast<QKVLayout>(layout);
   int64_t num_kv_heads, page_size;
