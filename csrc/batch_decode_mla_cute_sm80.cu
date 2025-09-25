8baa1224cdea29577e471ebed0e7ff8cd8776d55
Yaxing Cai <caiyaxing666@gmail.com>
2025-09-25 03:47:58 +0000

    fix

--- csrc/batch_decode_mla_cute_sm80.cu
+++ csrc/batch_decode_mla_cute_sm80.cu
@@ -11,10 +11,10 @@ using namespace flashinfer;
 using tvm::ffi::Array;
 using tvm::ffi::Optional;
 
-Array<int64_t> BatchDecodeWithPagedKVCachePlanMLA(Tensor float_workspace_buffer,
-                                                  Tensor int_workspace_buffer,
-                                                  Tensor page_locked_int_workspace_buffer,
-                                                  Tensor indptr, int64_t batch_size,
+Array<int64_t> BatchDecodeWithPagedKVCachePlanMLA(ffi::Tensor float_workspace_buffer,
+                                                  ffi::Tensor int_workspace_buffer,
+                                                  ffi::Tensor page_locked_int_workspace_buffer,
+                                                  ffi::Tensor indptr, int64_t batch_size,
                                                   int64_t num_qo_heads, int64_t page_size,
                                                   bool enable_cuda_graph) {
   size_t float_workspace_size_in_bytes =
@@ -43,11 +43,11 @@ Array<int64_t> BatchDecodeWithPagedKVCachePlanMLA(Tensor float_workspace_buffer,
 }
 
 void BatchDecodeWithPagedKVCacheRunMLA(
-    Tensor float_workspace_buffer, Tensor int_workspace_buffer, Array<int64_t> plan_info_vec,
-    Tensor q_nope, Tensor q_pe, Tensor paged_ckv_cache, Tensor paged_kpe_cache,
-    Tensor paged_kv_indptr, Tensor paged_kv_indices, Tensor paged_kv_last_page_len, Tensor o,
-    double sm_scale, int64_t window_left, double logits_soft_cap, double rope_scale,
-    double rope_theta, Optional<Tensor> maybe_lse,
+    ffi::Tensor float_workspace_buffer, ffi::Tensor int_workspace_buffer,
+    Array<int64_t> plan_info_vec, ffi::Tensor q_nope, ffi::Tensor q_pe, ffi::Tensor paged_ckv_cache,
+    ffi::Tensor paged_kpe_cache, ffi::Tensor paged_kv_indptr, ffi::Tensor paged_kv_indices,
+    ffi::Tensor paged_kv_last_page_len, ffi::Tensor o, double sm_scale, int64_t window_left,
+    double logits_soft_cap, double rope_scale, double rope_theta, Optional<ffi::Tensor> maybe_lse,
     bool enable_pdl  // fake placeholder, sm80 does not support pdl
 ) {
   DecodePlanInfo plan_info;
