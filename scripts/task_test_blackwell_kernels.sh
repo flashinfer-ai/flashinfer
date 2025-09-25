5c777905a99628adcfc69961273e6291a0225cf6
Maximilien Breughe <mbreughe@nvidia.com>
2025-09-25 12:35:59 -0700

    Updated setup script

--- scripts/task_test_blackwell_kernels.sh
+++ scripts/task_test_blackwell_kernels.sh
@@ -2,8 +2,14 @@
 
 set -eo pipefail
 set -x
-: ${JUNIT_DIR:=$(realpath ./junit)}
 
+: ${JUNIT_DIR:=$(realpath ./junit)}
+: ${MAX_JOBS:=$(nproc)}
+: ${CUDA_VISIBLE_DEVICES:=0}
+
+pip install -e . -v
+pip install --upgrade nvidia-cudnn-cu12
+pip install --upgrade cuda-python==12.*
 
 EXIT_CODE=0
 
