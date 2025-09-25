e84678940844fc8e72fe38348aee5f77f5be3715
kahyunnam <knam@nvidia.com>
2025-09-25 12:49:28 -0700

    skip using decorator for catching GPUArchitectureError exception

--- tests/test_mla_decode_kernel.py
+++ tests/test_mla_decode_kernel.py
@@ -7,7 +7,7 @@ from torch import nn
 
 import flashinfer
 from rope_reference import apply_rotary_emb, precompute_freqs_cis
-from flashinfer.utils import get_compute_capability
+from conftest import skip_on_gpu_arch_error
 
 
 def wmape(target: torch.Tensor, preds: torch.Tensor):
@@ -396,17 +396,11 @@ class DeepseekV2AttentionMatAbsorbDecode(nn.Module):
         return output
 
 
+@skip_on_gpu_arch_error
 @pytest.mark.parametrize("bsz", [6])
 @pytest.mark.parametrize("kv_len", [640])
 @pytest.mark.parametrize("page_size", [16])
 def test_mla_decode_kernel(bsz, kv_len, page_size):
-    major, minor = get_compute_capability(torch.device("cuda"))
-    device_arch = major * 10 + minor
-    if device_arch != 80:
-        pytest.skip(
-            "MLA decode kernel is not supported on this GPU (SM{device_arch}). Supported architecture: SM80."
-        )
-
     dev_id = 0
 
     torch.manual_seed(666)
