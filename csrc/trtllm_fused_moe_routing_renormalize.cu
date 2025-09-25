eb849b3b6fa8008421e58b8f15e6404cb81da742
Yaxing Cai <caiyaxing666@gmail.com>
2025-09-25 03:02:34 +0000

    fix

--- csrc/trtllm_fused_moe_routing_renormalize.cu
+++ csrc/trtllm_fused_moe_routing_renormalize.cu
@@ -299,7 +299,7 @@ void run(Data const& data, void* stream) {
   TVM_FFI_ICHECK(data.mPtrPermutedIdxSize != nullptr && data.mPtrCtaIdxXyToBatchIdx != nullptr &&
                  data.mPtrCtaIdxXyToMnLimit != nullptr && data.mPtrNumNonExitingCtas != nullptr)
       << "Llama4 routing kernel expects permuted idx and grouped Gemm launch config buffers";
-  TVM_FFI_ICHECK_LE(data.mTopK <= MaxNumTopExperts)
+  TVM_FFI_ICHECK_LE(data.mTopK, MaxNumTopExperts)
       << "Routing kernel expects topK experts <= " << MaxNumTopExperts << ", got " << data.mTopK;
   TVM_FFI_ICHECK_LT(data.mPaddingLog2, 8)
       << "Routing kernel expects padding log2 < 8, got " << data.mPaddingLog2;
