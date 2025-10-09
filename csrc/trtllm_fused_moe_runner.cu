33acfe2c5e192cd4c62d2ae5bf3e5e845795948c
Christina Zhang <83400082+ChristinaZ@users.noreply.github.com>
2025-10-09 05:52:41 -0700

    Add support for FP32 bias in the trtllm routing kernel for DeepSeek mode.
    Signed-off-by: Christina Zhang <83400082+ChristinaZ@users.noreply.github.com>

--- csrc/trtllm_fused_moe_runner.cu
+++ csrc/trtllm_fused_moe_runner.cu
@@ -62,7 +62,11 @@ void Runner::run(void* routingLogits, void* routingBias, int32_t numTokens, int3
     FLASHINFER_CHECK(topK <= 8, "For DeepSeek routing method, must have topK <= 8");
     FLASHINFER_CHECK(topkGroup <= 4, "For DeepSeek routing method, must have topkGroup <= 4");
     moe::dev::routing::routingDeepSeek::Data routingData;
-    routingData.mDtypeExpW = btg::Dtype::Bfloat16; 
+    routingData.mDtypeExpW =
+        btg::Dtype::Bfloat16;            // for DeepSeek, the expW is currently always bfloat16
+    routingData.mDtypeBias = dtypeBias;  // for DeepSeek, the bias can be bfloat16 or fp32
+
+    routingData.mDtypeScore = btg::Dtype::Fp32;  // for DeepSeek, the score is currently always fp32
     routingData.mUsePdl = true;
 
     // output:
