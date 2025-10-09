2b2b422ebda88ea10bc2ee3142ee637ab625fb88
Brian Ryu <bryu@nvidia.com>
2025-10-09 22:04:54 +0000

    Add print statements

--- include/flashinfer/trtllm/fmha/fmhaKernels.cuh
+++ include/flashinfer/trtllm/fmha/fmhaKernels.cuh
@@ -191,6 +191,8 @@ class TllmGenFmhaKernel {
       // Prepare the kernel parameters.
       auto kernelParams =
           KernelParams::setKernelParams(params, kernelMeta, maxNumCtasQ, maxNumCtasKv);
+      std::cout << "params.ptrCumSeqLensQ:" << kernelParams.ptrCumSeqLensQ << std::endl;
+      std::cout << "params.ptrCumSeqLensKv:" << kernelParams.ptrCumSeqLensKv << std::endl;
 
       // Prepare kernel parameters list for cuLaunchKernelEx.
       void* kernelParamsList[] = {&kernelParams};
