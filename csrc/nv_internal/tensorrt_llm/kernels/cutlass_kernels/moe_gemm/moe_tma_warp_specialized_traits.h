19785b9e9135b883079879af6c9fdb8439d0942f
Amir Klein <203507526+amirkl94@users.noreply.github.com>
2025-10-28 10:19:25 +0200

    Fine grain ifdefs
    Signed-off-by: Amir Klein <203507526+amirkl94@users.noreply.github.com>

--- csrc/nv_internal/tensorrt_llm/kernels/cutlass_kernels/moe_gemm/moe_tma_warp_specialized_traits.h
+++ csrc/nv_internal/tensorrt_llm/kernels/cutlass_kernels/moe_gemm/moe_tma_warp_specialized_traits.h
@@ -48,12 +48,13 @@ template <typename T, typename WeightType,
           TmaWarpSpecializedGroupedGemmInput::EpilogueFusion Fusion =
               TmaWarpSpecializedGroupedGemmInput::EpilogueFusion::NONE>
 constexpr bool isValidBlackwellMOESpecialisation() {
-#if defined(CUTLASS_ARCH_MMA_SM100_SUPPORTED) && \
-    defined(ENABLE_FP4)  // TODO Is there a better choice
-  return (cutlass::platform::is_same<T, WeightType>::value ||
-          (cutlass::platform::is_same<T, __nv_fp8_e4m3>::value &&
-           cutlass::platform::is_same<WeightType, __nv_fp4_e2m1>::value)) &&
-         cutlass::platform::is_same<EpilogueTag, cutlass_extensions::EpilogueOpDefault>::value &&
+#if defined(CUTLASS_ARCH_MMA_SM100_SUPPORTED)  // TODO Is there a better choice
+  return (cutlass::platform::is_same<T, WeightType>::value
+#if defined(ENABLE_FP4)
+          || (cutlass::platform::is_same<T, __nv_fp8_e4m3>::value &&
+              cutlass::platform::is_same<WeightType, __nv_fp4_e2m1>::value))
+#endif
+         && cutlass::platform::is_same<EpilogueTag, cutlass_extensions::EpilogueOpDefault>::value &&
          Fusion == TmaWarpSpecializedGroupedGemmInput::EpilogueFusion::NONE;
 #else
   return false;  // CUTLASS_ARCH_MMA_SM100_SUPPORTED is set when Blackwell kernels are enabled
