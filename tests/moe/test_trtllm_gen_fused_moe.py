f8a75fffd591ff2c0b225b2731c97f1d818edfb7
yzh119 <zihaoy@nvidia.com>
2025-10-28 21:24:08 -0700

    pre-commit

--- tests/moe/test_trtllm_gen_fused_moe.py
+++ tests/moe/test_trtllm_gen_fused_moe.py
@@ -2306,7 +2306,7 @@ def test_renormalize_routing(
     "moe_impl",
     [
         pytest.param(FP4Moe(quant_mode=QuantMode.FP4_NVFP4_NVFP4), id="NvFP4xNvFP4"),
-        pytest.param(FP4Moe(quant_mode=QuantMode.FP4_MXFP4_MXFP8), id="MxFP4xMxFP8")
+        pytest.param(FP4Moe(quant_mode=QuantMode.FP4_MXFP4_MXFP8), id="MxFP4xMxFP8"),
     ],
 )
 @pytest.mark.parametrize(
