6686135807f807b527cb90f5a62110b602a53e09
Maximilien Breughe <mbreughe@nvidia.com>
2025-10-30 15:17:23 -0700

    Added python checks for the API

--- tests/model_optimizations/test_dsv3_router_gemm.py
+++ tests/model_optimizations/test_dsv3_router_gemm.py
@@ -1,14 +1,15 @@
 import torch
-
+import pytest
 import flashinfer.model_optimizations.dsv3.routergemm as dsv3_router_gemm
 import torch.nn.functional as F
 
 
 # TODO: Add negative tests
-def test_dsv3_router_gemm_op():
-    num_tokens = 16
-    num_experts = 256
-    hidden_dim = 7168
+# Positive tests
+@pytest.mark.parametrize("num_tokens", [1, 2, 3, 5, 8, 13, 16])
+@pytest.mark.parametrize("num_experts", [256])
+@pytest.mark.parametrize("hidden_dim", [7168])
+def test_dsv3_router_gemm_op(num_tokens, num_experts, hidden_dim):
     mat_a = torch.randn(num_tokens, hidden_dim, device="cuda", dtype=torch.bfloat16)
     mat_b = torch.randn(
         num_experts, hidden_dim, device="cuda", dtype=torch.bfloat16
@@ -20,7 +21,3 @@ def test_dsv3_router_gemm_op():
 
     cos_sim = F.cosine_similarity(ref.reshape(-1), out.reshape(-1), dim=0)
     assert cos_sim > 0.99
-
-
-if __name__ == "__main__":
-    test_dsv3_router_gemm_op()
