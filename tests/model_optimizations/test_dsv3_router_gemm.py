b1f00e9609f0709518dfb559d85ca19c8c6ae327
Maximilien Breughe <mbreughe@nvidia.com>
2025-11-03 09:51:53 -0800

    Moved implementation and added alias

--- tests/model_optimizations/test_dsv3_router_gemm.py
+++ tests/model_optimizations/test_dsv3_router_gemm.py
@@ -1,6 +1,6 @@
 import torch
 import pytest
-import flashinfer.model_optimizations.dsv3.routergemm as dsv3_router_gemm
+import flashinfer.dsv3_ops.routergemm as dsv3_router_gemm
 import torch.nn.functional as F
 
 
@@ -14,7 +14,6 @@ def test_dsv3_router_gemm_op(num_tokens, num_experts, hidden_dim):
         num_experts, hidden_dim, device="cuda", dtype=torch.bfloat16
     ).t()  # column major
     out = torch.randn(num_tokens, num_experts, device="cuda", dtype=torch.float32)
-    # TODO: We shouldn't need to pass bias here, but just for testing
     dsv3_router_gemm.dsv3_router_gemm_op(mat_a, mat_b, out, False)
     ref = mat_a @ mat_b
 
