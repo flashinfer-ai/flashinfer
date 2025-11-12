639f4470f836684bb5e5688dd64cefade7135f0e
Aditya K Kamath <akamath1997@gmail.com>
2025-11-12 06:58:44 +0000

    Clamp lowerbound of max_grid_size to 0, to prevent possible underflow of uint32_t

--- include/flashinfer/attention/scheduler.cuh
+++ include/flashinfer/attention/scheduler.cuh
@@ -715,7 +715,9 @@ inline cudaError_t PrefillPlan(void* float_buffer, size_t float_workspace_size_i
   FLASHINFER_CUDA_CALL(cudaGetDevice(&dev_id));
   FLASHINFER_CUDA_CALL(cudaDeviceGetAttribute(&num_sm, cudaDevAttrMultiProcessorCount, dev_id));
   int num_blocks_per_sm = 2;
-  int max_grid_size = num_blocks_per_sm * num_sm - num_colocated_ctas;
+  int64_t available_ctas =
+      static_cast<int64_t>(num_blocks_per_sm) * num_sm - num_colocated_ctas;
+  int max_grid_size = static_cast<int>(std::max<int64_t>(0, available_ctas));
   uint32_t max_batch_size_if_split = max_grid_size / num_kv_heads;
 
   // step 2: determine kv_chunk_size
