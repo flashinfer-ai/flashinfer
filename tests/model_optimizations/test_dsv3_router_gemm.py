1e5bcaaaf9149552d13018ded6fc0fc58d5a5a70
Dimitrios Bariamis <12195802+dbari@users.noreply.github.com>
2025-11-27 10:25:22 -0800

    Extend DSv3 router GEMM to support Mistral Large 3 types and sizes
    Signed-off-by: Dimitrios Bariamis <12195802+dbari@users.noreply.github.com>

--- tests/model_optimizations/test_dsv3_router_gemm.py
+++ tests/model_optimizations/test_dsv3_router_gemm.py
@@ -1,16 +1,24 @@
 import torch
 import pytest
-from flashinfer.dsv3_ops import mm_M1_16_K7168_N256
+from flashinfer.dsv3_ops import mm_M1_16_K7168_N128, mm_M1_16_K7168_N256
 import torch.nn.functional as F
 from flashinfer.utils import get_compute_capability
 
 
 # Positive tests
 @pytest.mark.parametrize("num_tokens", [1, 2, 3, 5, 8, 13, 16])
-@pytest.mark.parametrize("num_experts", [256])
+@pytest.mark.parametrize(
+    "num_experts,output_dtype,fn_to_test",
+    (
+        [256, torch.float32, mm_M1_16_K7168_N256],
+        [128, torch.bfloat16, mm_M1_16_K7168_N128],
+    ),
+)
 @pytest.mark.parametrize("hidden_dim", [7168])
 @pytest.mark.parametrize("launch_with_pdl", [True, False])
-def test_dsv3_router_gemm_op(num_tokens, num_experts, hidden_dim, launch_with_pdl):
+def test_dsv3_router_gemm_op(
+    num_tokens, num_experts, hidden_dim, launch_with_pdl, output_dtype, fn_to_test
+):
     compute_capability = get_compute_capability(torch.device("cuda"))
     compute_capability_number = compute_capability[0] * 10 + compute_capability[1]
     if compute_capability_number != 100:
@@ -20,8 +28,8 @@ def test_dsv3_router_gemm_op(num_tokens, num_experts, hidden_dim, launch_with_pd
     mat_b = torch.randn(
         num_experts, hidden_dim, device="cuda", dtype=torch.bfloat16
     ).t()  # column major
-    out = torch.randn(num_tokens, num_experts, device="cuda", dtype=torch.float32)
-    mm_M1_16_K7168_N256(mat_a, mat_b, out, launch_with_pdl=launch_with_pdl)
+    out = torch.empty(num_tokens, num_experts, device="cuda", dtype=output_dtype)
+    fn_to_test(mat_a, mat_b, out, launch_with_pdl=launch_with_pdl)
     ref = mat_a @ mat_b
 
     cos_sim = F.cosine_similarity(ref.reshape(-1), out.reshape(-1), dim=0)
