1d37b6ff420b147d7b06fb7d447a64bf769bf18f
Raayan Dhar raayan.dhar@gmail.com <raayan.dhar@gmail.com>
2025-12-02 10:31:22 -0800

    address comments
    Signed-off-by: Raayan Dhar raayan.dhar@gmail.com <raayan.dhar@gmail.com>

--- csrc/renorm.cu
+++ csrc/renorm.cu
@@ -21,26 +21,6 @@ using namespace flashinfer;
 
 using tvm::ffi::Optional;
 
-// Validate sampling parameters
-inline void check_tensor_param(const Optional<TensorView>& maybe_param, const TensorView& tensor) {
-  if (maybe_param.has_value()) {
-    const TensorView& param = maybe_param.value();
-    if (param.ndim() == 0) {
-      TVM_FFI_ICHECK(false)
-          << "Expected a 1D tensor of shape (batch_size,) or scalar for the sampling parameter, "
-          << "but got a 0-dimensional tensor with shape " << param.size(0) << ". ";
-    } else if (param.ndim() > 1) {
-      TVM_FFI_ICHECK(false) << "Expected a 1D tensor or scalar for the sampling parameter, "
-                            << "but got a " << param.ndim() << "D tensor. ";
-    } else if (param.size(0) != tensor.size(0)) {
-      TVM_FFI_ICHECK(false) << "Sampling parameter tensor batch size mismatch: "
-                            << "expected length " << tensor.size(0)
-                            << " to match the reference tensor batch size, "
-                            << "but got length " << param.size(0) << ". ";
-    }
-  }
-}
-
 void top_p_renorm_probs(TensorView probs, TensorView renorm_probs,
                         Optional<TensorView> maybe_top_p_arr, double top_p_val) {
   CHECK_INPUT(probs);
