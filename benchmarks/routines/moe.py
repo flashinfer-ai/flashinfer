47ffaa78b2038f3f0303f31d5bc34a3c49006ba8
Amit Zuker <203509407+amitz-nv@users.noreply.github.com>
2026-01-07 20:24:55 +0200

    Minor fixes
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- benchmarks/routines/moe.py
+++ benchmarks/routines/moe.py
@@ -246,7 +246,6 @@ def parse_moe_args(line, parser):
     }
     args.routing_method_type = routing_method_name_to_type[args.routing_method]
 
-    args.activation_type = args.activation_type
     if args.verbose >= 1:
         print(f"[INFO] {args = }")
     return args
@@ -1606,7 +1605,7 @@ def testTrtllmFp8PerTensorScaleMoe(args):
             output1_scales_gate_scalar,
             gemm2_weights_fp8,
             output2_scales_scalar,
-            args.activation_type
+            args.activation_type,
         ),
     )
 
