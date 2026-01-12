d99923c231cb95f04843b12acfde8267523216c8
Igor Shovkun <igshov@gmail.com>
2026-01-12 15:24:53 -0800

    fix obsoleted bf16 ifdefs

--- include/flashinfer/mamba/conversion.cuh
+++ include/flashinfer/mamba/conversion.cuh
@@ -1,8 +1,9 @@
 #pragma once
 #include <cuda.h>
-#include <cuda_bf16.h>
 #include <cuda_fp16.h>
-
+#ifdef FLASHINFER_ENABLE_BF16
+#include <cuda_bf16.h>
+#endif
 #ifdef ENABLE_FP8
 #include <cuda_fp8.h>
 #endif
@@ -12,7 +13,8 @@ namespace flashinfer::mamba::conversion {
 inline __device__ float toFloat(float f) { return f; }
 
 inline __device__ float toFloat(__half h) { return __half2float(h); }
-#ifdef ENABLE_BF16
+
+#ifdef FLASHINFER_ENABLE_BF16
 inline __device__ float toFloat(__nv_bfloat16 val) { return __bfloat162float(val); }
 #endif
 
@@ -22,7 +24,7 @@ inline __device__ void convertAndStore(__half* output, float input) {
   *output = __float2half(input);
 }
 
-#ifdef ENABLE_BF16
+#ifdef FLASHINFER_ENABLE_BF16
 inline __device__ void convertAndStore(__nv_bfloat16* output, float input) {
   *output = __float2bfloat16(input);
 }
