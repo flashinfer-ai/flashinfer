f7c2df52ec6b0476855a6bc43b95a1869757d793
amitz-nv <203509407+amitz-nv@users.noreply.github.com>
2026-02-02 14:14:27 +0000

    Remove bad num experts check in csrc/trtllm_fused_moe_routing_deepseek.cu
    Signed-off-by: amitz-nv <203509407+amitz-nv@users.noreply.github.com>

--- csrc/trtllm_fused_moe_routing_deepseek.cu
+++ csrc/trtllm_fused_moe_routing_deepseek.cu
@@ -565,9 +565,6 @@ void runImpl(Data& data, void* stream) {
   FLASHINFER_CHECK(data.mTopK * data.mNumLimitedGroups <= WarpSize,
                    "Routing kernel expects top K * top groups <= warp size (for now), got %d * %d",
                    data.mTopK, data.mNumLimitedGroups);
-  FLASHINFER_CHECK(data.mNumExperts >= MaxSupportedTopExperts,
-                   "Routing kernel expects %d to be at most #experts %d", MaxSupportedTopExperts,
-                   data.mNumExperts);
   FLASHINFER_CHECK(data.mNumExperts <= MaxSupportedExpertCount,
                    "Routing kernel expects #experts %d  <= #threads %d", data.mNumExperts,
                    MaxSupportedExpertCount);
