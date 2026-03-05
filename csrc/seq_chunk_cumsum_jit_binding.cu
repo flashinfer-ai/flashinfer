1146f1fe54a8d25f295e5a7c9788325b52017b2c
Igor Shovkun <igshov@gmail.com>
2026-03-04 21:39:19 -0800

    Add missing seq_chunk_cumsum source files

---
+++ csrc/seq_chunk_cumsum_jit_binding.cu
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025 by FlashInfer team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "tvm_ffi_utils.h"
+
+void seq_chunk_cumsum(TensorView seq_idx, TensorView chunk_indices, TensorView chunk_offsets,
+                      TensorView output, int64_t chunk_size, int64_t num_logical_chunks,
+                      int64_t num_seqs);
+
+TVM_FFI_DLL_EXPORT_TYPED_FUNC(seq_chunk_cumsum, seq_chunk_cumsum);
