c82573092164e1d748562569d4320123ba0915e9
jimmzhou <jimmzhou@nvidia.com>
2026-03-20 19:21:27 +0000

    init

--- benchmarks/routines/attention.py
+++ benchmarks/routines/attention.py
@@ -697,7 +697,11 @@ def testBatchDecodeWithPagedKVCacheWrapper(args):
             repeat_iters=args.num_iters,
             sleep_after_run=False,
             enable_cupti=args.use_cupti,
-            use_cuda_graph=(is_cuda_graph_compatible and cur_backend != "fa2"),
+            use_cuda_graph=(
+                is_cuda_graph_compatible
+                and cur_backend
+                not in ("fa2", "trtllm-fmha-v2", "trtllm-native", "cudnn-native")
+            ),
             cold_l2_cache=True,
             input_args=(
                 cur_backend,
@@ -1388,7 +1392,7 @@ def testBatchPrefillWithPagedKVCacheWrapper(args):
             repeat_iters=args.num_iters,
             sleep_after_run=False,
             enable_cupti=args.use_cupti,
-            use_cuda_graph=(is_cuda_graph_compatible and cur_backend != "fa2"),
+            use_cuda_graph=(is_cuda_graph_compatible and cur_backend not in ("fa2")),
             cold_l2_cache=True,
             input_args=(
                 cur_backend,
@@ -2002,7 +2006,11 @@ def testBatchPrefillWithRaggedKVCacheWrapper(args):
             repeat_iters=args.num_iters,
             sleep_after_run=True,
             enable_cupti=args.use_cupti,
-            use_cuda_graph=(is_cuda_graph_compatible and cur_backend != "fa2"),
+            use_cuda_graph=(
+                is_cuda_graph_compatible
+                and cur_backend
+                not in ("fa2", "trtllm-fmha-v2", "trtllm-native", "cudnn-native")
+            ),
             cold_l2_cache=True,
             input_args=(
                 cur_backend,
@@ -2464,7 +2472,11 @@ def testBatchMLAPagedAttentionWrapper(args):
             repeat_iters=args.num_iters,
             sleep_after_run=False,
             enable_cupti=args.use_cupti,
-            use_cuda_graph=(is_cuda_graph_compatible and cur_backend != "fa2"),
+            use_cuda_graph=(
+                is_cuda_graph_compatible
+                and cur_backend
+                not in ("fa2", "trtllm-fmha-v2", "trtllm-native", "cudnn-native")
+            ),
             cold_l2_cache=True,
             input_args=(
                 cur_backend,
