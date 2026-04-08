0fdd9223f979ee739761095547699876cd47131c
jimmzhou <jimmzhou@nvidia.com>
2026-04-08 18:54:25 +0000

    alibi test init

--- flashinfer/jit/core.py
+++ flashinfer/jit/core.py
@@ -313,8 +313,9 @@ class JitSpec:
         # where another process is building the library and removes the .so file.
         with FileLock(self.lock_path, thread_local=False):
             so_path = self.jit_library_path
-            verbose = os.environ.get("FLASHINFER_JIT_VERBOSE", "0") == "1"
-            self.build(verbose, need_lock=False)
+            if not so_path.exists():
+                verbose = os.environ.get("FLASHINFER_JIT_VERBOSE", "0") == "1"
+                self.build(verbose, need_lock=False)
             result = self.load(so_path)
 
         return result
