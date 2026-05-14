715eb8b4f3ccd39849e223fc2f95e201256df709
djns99 <40156487+djns99@users.noreply.github.com>
2026-05-15 09:43:07 +1200

    Add support for MXFP8 and Relu2

--- flashinfer/artifacts.py
+++ flashinfer/artifacts.py
@@ -137,7 +137,7 @@ class ArtifactPath:
 
     TRTLLM_GEN_FMHA: str = "158f6fa11ef139a098cfddcdddce73ca99d164ad/fmha/trtllm-gen/"
     TRTLLM_GEN_BMM: str = (
-        "bfce2df0fcd9a538b40bade61c993c2aa76b5afe/batched_gemm-920d640-fd198aa/"
+        "d2c5915bf45ae64308a947824c14966c2718a292/batched_gemm-91e0ba0-896b90b/"
     )
     TRTLLM_GEN_GEMM: str = (
         "10f64528a1172dae8e29601a3b99ab9dc78d37be/gemm-91e0ba0-2710384/"
@@ -160,7 +160,7 @@ class CheckSumHash:
         "c2d9399b2537be785882354a4f9902ed6c03136c0ea341e201eac40c3923e1dc"
     )
     TRTLLM_GEN_BMM: str = (
-        "a0a9ac117d6e5975adb866e3b407d78f4b6e2b4215f8c615d3d5b8e32ef4eb66"
+        "ffc4f52df6ff901000ab4cd80fa1ae24dcf5fe5739c6e5c0b3bb9b8e4802084e"
     )
     DEEPGEMM: str = "1a2a166839042dbd2a57f48051c82cd1ad032815927c753db269a4ed10d0ffbf"
     TRTLLM_GEN_GEMM: str = (
