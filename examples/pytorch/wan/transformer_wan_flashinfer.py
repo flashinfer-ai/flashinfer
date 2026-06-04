20dd19d3c6858c8bf8ee83a98aa52e72511bfca0
Forrest Lin <forrestl@nvidia.com>
2026-06-04 02:01:09 -0700

    [wan] add license

--- examples/pytorch/wan/transformer_wan_flashinfer.py
+++ examples/pytorch/wan/transformer_wan_flashinfer.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2026 by FlashInfer team.
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# This file is adapted from the diffusers WanTransformer3DModel implementation:
+#   https://github.com/huggingface/diffusers/blob/main/src/diffusers/models/transformers/transformer_wan.py
+# The original code is licensed under the Apache License, Version 2.0.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 FlashInfer-optimized implementation of WanTransformer3DModel.
 
