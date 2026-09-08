988c36cf86318c0a82c7537299b4bf555c1a2147
Yingyi Huang <averyh@nvidia.com>
2026-09-08 16:13:50 -0700

    Accept cross-platform CUDA tensor-map alignment

--- csrc/fused_moe/warp_decode/generated/sm_103a/cake_warp_decode_bc3ec42355f5d8526d04_kernel.cu
+++ csrc/fused_moe/warp_decode/generated/sm_103a/cake_warp_decode_bc3ec42355f5d8526d04_kernel.cu
@@ -38,7 +38,7 @@ typedef struct __align__(128) { uint64_t opaque[16]; } CUtensorMap;
 #endif
 
 static_assert(sizeof(CUtensorMap) == 128, "CUtensorMap CUDA ABI must be 128 bytes");
-static_assert(alignof(CUtensorMap) == 128, "CUtensorMap CUDA ABI must be 128-byte aligned");
+static_assert(alignof(CUtensorMap) >= 64, "CUtensorMap CUDA ABI must be at least 64-byte aligned");
 #include <cuda_bf16.h>
 #include <cuda_fp8.h>
 
